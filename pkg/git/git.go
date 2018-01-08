a22d719b9890a96c8897a79c385f2dadc00375f2
James Strachan <james.strachan@gmail.com>
2018-01-08 18:43:36 +0000

    parse the .git/config to find the default remote url

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -6,20 +6,20 @@ import (
 )
 
 // FindGitConfigDir tries to find the `.git` directory either in the current directory or in parent directories
-func FindGitConfigDir(dir string) (string, error) {
+func FindGitConfigDir(dir string) (string, string, error) {
 	d := dir
 	for {
 		gitDir := filepath.Join(d, ".git/config")
 		f, err := util.FileExists(gitDir)
 		if err != nil {
-			return "", err
+			return "", "", err
 		}
 		if f {
-			return d, nil
+			return d, gitDir, nil
 		}
 		p, _ := filepath.Split(d)
 		if d == "/" || p == d {
-			return "", nil
+			return "", "", nil
 		}
 	}
 
--- pkg/jx/cmd/import.go
+++ pkg/jx/cmd/import.go
@@ -9,11 +9,12 @@ import (
 
 	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
 	"github.com/jenkins-x/jx/pkg/git"
+	gitcfg "gopkg.in/src-d/go-git.v4/config"
+	"io/ioutil"
 )
 
 type ImportOptions struct {
 	CommonOptions
-
 }
 
 func NewCmdImport(f cmdutil.Factory, out io.Writer, errOut io.Writer) *cobra.Command {
@@ -52,7 +53,7 @@ func (o *ImportOptions) Run() error {
 	if err != nil {
 		return err
 	}
-	root, err := git.FindGitConfigDir(dir)
+	root, gitConf, err := git.FindGitConfigDir(dir)
 	if err != nil {
 		return err
 	}
@@ -60,7 +61,52 @@ func (o *ImportOptions) Run() error {
 		return fmt.Errorf("TODO support non-cloned git repos!")
 	}
 	out := o.Out
-	fmt.Fprintf(out, "Root directory of project is %s\n", root)
+
+	cfg := gitcfg.NewConfig()
+	data, err := ioutil.ReadFile(gitConf)
+	if err != nil {
+		return fmt.Errorf("Failed to load %s due to %s", gitConf, err)
+	}
+
+	err = cfg.Unmarshal(data)
+	if err != nil {
+		return fmt.Errorf("Failed to unmarshal %s due to %s", gitConf, err)
+	}
+	remotes := cfg.Remotes
+	if len(remotes) == 0 {
+		return fmt.Errorf("Could not find any git remotes in the local %s so please specify a git repository on the command line\n", gitConf)
+	}
+	url := getRemoteUrl(cfg, "upstream")
+	if url == "" {
+		url = getRemoteUrl(cfg, "origin")
+		if url == "" {
+			if len(remotes) == 1 {
+				for _, r := range remotes {
+					u := firstRemoteUrl(r)
+					if u != "" {
+						url = u
+						break
+					}
+				}
+			}
+		}
+	}
+	fmt.Fprintf(out, "Git remote URL: %s\n", url)
 	fmt.Fprintf(out, "Has %d jobs\n", len(jobs))
 	return nil
+}
+func firstRemoteUrl(remote *gitcfg.RemoteConfig) string {
+	if remote != nil {
+		urls := remote.URLs
+		if urls != nil && len(urls) > 0 {
+			return urls[0]
+		}
+	}
+	return ""
+}
+func getRemoteUrl(config *gitcfg.Config, name string) string {
+	if config.Remotes != nil {
+		return firstRemoteUrl(config.Remotes[name])
+	}
+	return ""
 }
