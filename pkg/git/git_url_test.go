c404917fab63e1224f4012733db0dd89cd804460
James Strachan <james.strachan@gmail.com>
2018-01-09 08:45:09 +0000

    add support for parsing git urls to extract host/org/name

---
+++ pkg/git/git_url_test.go
@@ -0,0 +1,56 @@
+package git
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+type parseGitUrlData struct {
+	url          string
+	host         string
+	organisation string
+	name         string
+}
+
+func TestParseGitURL(t *testing.T) {
+	testCases := []parseGitUrlData{
+		{
+			"git://host.xz/org/repo", "host.xz", "org", "repo",
+		},
+		{
+			"git://host.xz/org/repo.git", "host.xz", "org", "repo",
+		},
+		{
+			"git://host.xz/org/repo.git/", "host.xz", "org", "repo",
+		},
+		{
+			"git://github.com/jstrachan/npm-pipeline-test-project.git", "github.com", "jstrachan", "npm-pipeline-test-project",
+		},
+		{
+			"https://github.com/fabric8io/foo.git", "github.com", "fabric8io", "foo",
+		},
+		{
+			"https://github.com/fabric8io/foo", "github.com", "fabric8io", "foo",
+		},
+		{
+			"git@github.com:jstrachan/npm-pipeline-test-project.git", "github.com", "jstrachan", "npm-pipeline-test-project",
+		},
+		{
+			"git@github.com:bar/foo.git", "github.com", "bar", "foo",
+		},
+		{
+			"git@github.com:bar/foo", "github.com", "bar", "foo",
+		},
+		{
+			"bar/foo", "github.com", "bar", "foo",
+		},
+	}
+	for _, data := range testCases {
+		info, err := ParseGitURL(data.url)
+		assert.Nil(t, err)
+		assert.NotNil(t, info)
+		assert.Equal(t, data.host, info.Host, "Host does not match for input %s", data.url)
+		assert.Equal(t, data.organisation, info.Organisation, "Organisation does not match for input %s", data.url)
+		assert.Equal(t, data.name, info.Name, "Name does not match for input %s", data.url)
+	}
+}
