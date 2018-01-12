248b86638ee5cbfd65396c6bb8c1521bdb75399d
James Strachan <james.strachan@gmail.com>
2018-01-12 23:10:49 +0000

    add a `jx get env foo` to list apps and versions

--- pkg/jx/cmd/get_env.go
+++ pkg/jx/cmd/get_env.go
@@ -1,13 +1,16 @@
 package cmd
 
 import (
+	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/spf13/cobra"
 
 	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
-	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
 	"github.com/jenkins-x/jx/pkg/kube"
+	"github.com/jenkins-x/jx/pkg/util"
+	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -70,20 +73,66 @@ func (o *GetEnvOptions) Run() error {
 	if err != nil {
 		return err
 	}
-	envs, err := client.JenkinsV1().Environments(ns).List(metav1.ListOptions{})
-	if err != nil {
-		return err
-	}
-	if len(envs.Items) == 0 {
-		return outputEmptyListWarning(o.Out)
-	}
+	args := o.Args
+	if len(args) > 0 {
+		e := args[0]
+		env, err := client.JenkinsV1().Environments(ns).Get(e, metav1.GetOptions{})
+		if err != nil {
+			envNames, err := kube.GetEnvironmentNames(client, ns)
+			if err != nil {
+				return err
+			}
+			return util.InvalidArg(e, envNames)
+		}
 
-	table := o.CreateTable()
-	table.AddRow("NAME", "LABEL", "PROMOTE", "NAMESPACE", "CLUSTER", "SOURCE URL", "REF")
+		// lets output one environment
+		spec := &env.Spec
 
-	for _, env := range envs.Items {
-		table.AddRow(env.Name, env.Spec.Label, string(env.Spec.PromotionStrategy), env.Spec.Namespace, env.Spec.Cluster, env.Spec.Source.URL, env.Spec.Source.Ref)
+		table := o.CreateTable()
+		table.AddRow("ENV", "LABEL", "NAMESPACE", "SOURCE", "REF")
+		table.AddRow(e, spec.Label, spec.Namespace, spec.Source.URL, spec.Source.Ref)
+		table.Render()
+		o.Printf("\n")
+
+		ens := env.Spec.Namespace
+		if ens != "" {
+			deps, err := kubeClient.AppsV1beta2().Deployments(ens).List(metav1.ListOptions{})
+			if err != nil {
+				return fmt.Errorf("Could not find deployments in namespace %s: %s", ens, err)
+			}
+
+			table = o.CreateTable()
+			table.AddRow("APP", "VERSION", "DESIRED", "CURRENT", "UP-TO-DATE", "AVAILABLE", "AGE")
+			for _, d := range deps.Items {
+				replicas := ""
+				if d.Spec.Replicas != nil {
+					replicas = formatInt32(*d.Spec.Replicas)
+				}
+				table.AddRow(d.Name, kube.GetVersion(&d.ObjectMeta), replicas,
+					formatInt32(d.Status.ReadyReplicas), formatInt32(d.Status.UpdatedReplicas), formatInt32(d.Status.AvailableReplicas), "")
+			}
+			table.Render()
+		}
+	} else {
+		envs, err := client.JenkinsV1().Environments(ns).List(metav1.ListOptions{})
+		if err != nil {
+			return err
+		}
+		if len(envs.Items) == 0 {
+			return outputEmptyListWarning(o.Out)
+		}
+
+		table := o.CreateTable()
+		table.AddRow("NAME", "LABEL", "PROMOTE", "NAMESPACE", "CLUSTER", "SOURCE URL", "REF")
+
+		for _, env := range envs.Items {
+			table.AddRow(env.Name, env.Spec.Label, string(env.Spec.PromotionStrategy), env.Spec.Namespace, env.Spec.Cluster, env.Spec.Source.URL, env.Spec.Source.Ref)
+		}
+		table.Render()
 	}
-	table.Render()
 	return nil
+}
+
+func formatInt32(n int32) string {
+    return strconv.FormatInt(int64(n), 10)
 }
