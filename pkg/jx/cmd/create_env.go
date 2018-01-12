c1e310b4b21ce1b246e322609c27b98f35e82b80
James Strachan <james.strachan@gmail.com>
2018-01-12 15:49:31 +0000

    minor refactor

--- pkg/jx/cmd/create_env.go
+++ pkg/jx/cmd/create_env.go
@@ -99,23 +99,9 @@ func (o *CreateEnvOptions) Run() error {
 	}
 	o.Printf("Created environment %s\n", env.Name)
 
-	// lets create the namespace if we are on the same cluster
-	spec := &env.Spec
-	if spec.Cluster == "" && spec.Namespace != "" {
-		labels := map[string]string{
-			"team": ns,
-		}
-		annotations := map[string]string{}
-
-		kubeClient, _, err := o.Factory.CreateClient()
-		if err != nil {
-			return err
-		}
-
-		err = kube.EnsureNamespaceCreated(kubeClient, spec.Namespace, labels, annotations)
-		if err != nil {
-			return err
-		}
+	kubeClient, _, err := o.Factory.CreateClient()
+	if err != nil {
+		return err
 	}
-	return nil
+	return kube.EnsureEnvironmentNamespaceSetup(kubeClient, &env, ns)
 }
