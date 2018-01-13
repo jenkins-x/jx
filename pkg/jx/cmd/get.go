96c628e80c31e732b305ab2f3772a5353e96f8af
James Strachan <james.strachan@gmail.com>
2018-01-13 17:03:38 +0000

    add `jx get version`

--- pkg/jx/cmd/get.go
+++ pkg/jx/cmd/get.go
@@ -61,6 +61,7 @@ func NewCmdGet(f cmdutil.Factory, out io.Writer, errOut io.Writer) *cobra.Comman
 	cmd.AddCommand(NewCmdGetEnv(f, out, errOut))
 	cmd.AddCommand(NewCmdGetPipeline(f, out, errOut))
 	cmd.AddCommand(NewCmdGetURL(f, out, errOut))
+	cmd.AddCommand(NewCmdGetVersion(f, out, errOut))
 	return cmd
 }
 
