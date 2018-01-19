c3196e4c25215854568940cbca19976091726f7e
Rob Davies <rajdavies@gmail.com>
2018-01-19 12:43:11 +0000

    create cluster on aks; add metrics cmd (#143)
    * first cut at metrics
    
    * 2nd cut at metrics
    
    * add metrics command
    
    * first cut on azure support
    
    * allocate resource group
    
    * updated description

--- pkg/jx/cmd/create_cluster_aks.go
+++ pkg/jx/cmd/create_cluster_aks.go
@@ -4,13 +4,15 @@ import (
 	"io"
 	"os"
 
-	"fmt"
 
 	"github.com/jenkins-x/jx/pkg/jx/cmd/log"
 	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
 	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
 	"github.com/spf13/cobra"
+	"gopkg.in/AlecAivazis/survey.v1"
 )
+
+
 
 // CreateClusterOptions the flags for running crest cluster
 type CreateClusterAKSOptions struct {
@@ -20,6 +22,12 @@ type CreateClusterAKSOptions struct {
 }
 
 type CreateClusterAKSFlags struct {
+	Name              	   string
+	ResourceGroup		   string
+	Location			   string
+	NodeCount			   string
+	KubeVersion            string
+	PathToPublicKey        string
 }
 
 var (
@@ -32,6 +40,12 @@ var (
 		ongoing operations and maintenance by provisioning, upgrading, and scaling resources on demand, without taking
 		your applications offline.
 
+		Please use a location local to you: you can retrieve this from the Azure portal or by 
+		running "az provider list" in your terminal.
+
+		Important: You will need an account on azure, with a storage account (https://portal.azure.com/#create/Microsoft.StorageAccount-ARM)
+        and network (https://portal.azure.com/#create/Microsoft.VirtualNetwork-ARM) - both linked to the resource group you use
+		to create the cluster in.
 `)
 
 	createClusterAKSExample = templates.Examples(`
@@ -71,8 +85,12 @@ func NewCmdCreateClusterAKS(f cmdutil.Factory, out io.Writer, errOut io.Writer) 
 
 	options.addCreateClusterFlags(cmd)
 
-	//cmd.Flags().StringVarP(&options.Flags.Memory, "memory", "m", "4096", "Amount of RAM allocated to the minikube VM in MB")
-	//cmd.Flags().StringVarP(&options.Flags.CPU, "cpu", "c", "3", "Number of CPUs allocated to the minikube VM")
+	cmd.Flags().StringVarP(&options.Flags.Name, "name", "n", "jenkins-x-cluster", "Name of the cluster")
+	cmd.Flags().StringVarP(&options.Flags.ResourceGroup, "resource group", "g", "jenkins-x", "Name of the resource group")
+	cmd.Flags().StringVarP(&options.Flags.Location, "location", "l", "West Europe", "location to run cluster in")
+	cmd.Flags().StringVarP(&options.Flags.NodeCount, "nodes", "o", "1", "node count")
+	cmd.Flags().StringVarP(&options.Flags.KubeVersion, "K8Version", "v", "1.8.2", "kubernetes version")
+	cmd.Flags().StringVarP(&options.Flags.PathToPublicKey, "PathToPublicRSAKey", "p", "", "pathToPublicRSAKey")
 
 	return cmd
 }
@@ -101,6 +119,107 @@ func (o *CreateClusterAKSOptions) Run() error {
 
 func (o *CreateClusterAKSOptions) createClusterAKS() error {
 
-	// TODO
-	return fmt.Errorf("Create %s cluster not yet implemented", o.Provider)
+	name := o.Flags.Name
+	prompt := &survey.Input{
+		Message: "name",
+		Default: name,
+		Help:    "name of the cluster",
+	}
+	survey.AskOne(prompt, &name, nil)
+
+	location := o.Flags.Location
+	prompt = &survey.Input{
+		Message: "location",
+		Default: location,
+		Help:    "location to run cluster",
+	}
+	survey.AskOne(prompt, &location, nil)
+
+	resourceGroup := o.Flags.ResourceGroup
+	prompt = &survey.Input{
+		Message: "resourceGroup",
+		Default: resourceGroup,
+		Help:    "resource group",
+	}
+	survey.AskOne(prompt, &resourceGroup, nil)
+
+	nodeCount := o.Flags.NodeCount
+	prompt = &survey.Input{
+		Message: "nodes",
+		Default: nodeCount,
+		Help:    "number of nodes",
+	}
+	survey.AskOne(prompt,&nodeCount,nil)
+
+	kubeVersion := o.Flags.KubeVersion
+	prompt = &survey.Input{
+		Message: "k8version",
+		Default: kubeVersion,
+		Help:    "k8 version",
+	}
+	survey.AskOne(prompt,&kubeVersion,nil)
+
+	pathToPublicKey := o.Flags.PathToPublicKey
+	prompt = &survey.Input{
+		Message: "PathToPublicKey",
+		Default: "",
+		Help:    "path to public RSA key",
+	}
+	survey.AskOne(prompt,&pathToPublicKey,nil)
+
+
+
+	//First login
+
+	err := o.runCommand("az", "login")
+	if err != nil{
+		return err
+	}
+
+	//create a resource group
+
+
+	createGroup := []string{"group","create", "-l",location,"-n",resourceGroup}
+
+	err = o.runCommand("az",createGroup...)
+
+	if err != nil{
+		return err
+	}
+
+
+	createCluster := []string{"aks", "create", "-g", resourceGroup,"-n", name,"-k",kubeVersion,"--node-count", nodeCount}
+
+	if pathToPublicKey != ""{
+		createCluster = append(createCluster,"--ssh-key-value",pathToPublicKey)
+	}else {
+		createCluster = append(createCluster,"--generate-ssh-keys")
+	}
+
+	err = o.runCommand("az", createCluster...)
+	if err != nil {
+		return err
+	}
+
+	// call jx init
+	initOpts := &InitOptions{
+		CommonOptions: o.CommonOptions,
+	}
+	err = initOpts.Run()
+	if err != nil {
+		return err
+	}
+
+	// call jx install
+	installOpts := &InstallOptions{
+		CommonOptions:      o.CommonOptions,
+		CloudEnvRepository: DEFAULT_CLOUD_ENVIRONMENTS_URL,
+		KubernetesProvider: AKS, // TODO replace with context, maybe get from ~/.jx/gitAuth.yaml?
+	}
+	err = installOpts.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
---
+++ pkg/jx/cmd/metrics.go
@@ -0,0 +1,108 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
+	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
+	"github.com/jenkins-x/jx/pkg/kube"
+	"github.com/spf13/cobra"
+	"io"
+)
+
+type MetricsOptions struct {
+	CommonOptions
+	Namespace string
+}
+
+var (
+	MetricsLong = templates.LongDesc(`
+		Gets the metrics of the newest pod for a Deployment.
+
+`)
+
+	MetricsExample = templates.Examples(`
+		# displays metrics of the latest pod in deployment myapp
+		jx metrics myapp
+
+		# Tails the metrics of the container foo in the latest pod in deployment myapp
+		jx metrics myapp -c foo
+`)
+)
+
+func NewCmdMetrics(f cmdutil.Factory, out io.Writer, errOut io.Writer) *cobra.Command {
+	options := &MetricsOptions{
+		CommonOptions: CommonOptions{
+			Factory: f,
+			Out:     out,
+			Err:     errOut,
+		},
+	}
+	cmd := &cobra.Command{
+		Use:     "metrics [deployment]",
+		Short:   "Gets the metrics of the latest pod for a deployment",
+		Long:    MetricsLong,
+		Example: MetricsExample,
+		Aliases: []string{"metrics"},
+		Run: func(cmd *cobra.Command, args []string) {
+			options.Cmd = cmd
+			options.Args = args
+			err := options.Run()
+			cmdutil.CheckErr(err)
+		},
+	}
+
+	cmd.Flags().StringVarP(&options.Namespace, "namespace", "n", "", "the namespace to look for the Deployment. Defaults to the current namespace")
+	return cmd
+}
+
+func (o *MetricsOptions) Run() error {
+	args := o.Args
+
+	client, curNs, err := o.Factory.CreateClient()
+	if err != nil {
+		return err
+	}
+
+	ns := o.Namespace
+	if ns == "" {
+		ns = curNs
+	}
+	name := ""
+	if len(args) == 0 {
+		names, err := GetDeploymentNames(client, ns)
+		if err != nil {
+			return err
+		}
+		if len(names) == 0 {
+			return fmt.Errorf("There are no Deployments running")
+		}
+		n, err := kube.PickName(names, "Pick Deployment:")
+		if err != nil {
+			return err
+		}
+		name = n
+	} else {
+		name = args[0]
+	}
+
+	pod, err := waitForReadyPodForDeployment(client, ns, name)
+
+	if err != nil {
+		return err
+	}
+
+	if pod == "" {
+		return fmt.Errorf("No pod found for namespace %s with name %s", ns, name)
+	}
+
+	namespaceVar := "--namespace="+ns
+
+	args = []string{"top", "pod", pod,namespaceVar}
+
+	err = o.runCommand("kubectl", args...)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
