0462415cc90c3faf8a56051b69dcece04ba77651
James Strachan <james.strachan@gmail.com>
2018-01-20 10:20:08 +0000

    formatted

--- pkg/kube/services.go
+++ pkg/kube/services.go
@@ -1,12 +1,12 @@
 package kube
 
 import (
+	"fmt"
 	v1 "k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"fmt"
-	"strings"
 	"sort"
+	"strings"
 )
 
 const (
