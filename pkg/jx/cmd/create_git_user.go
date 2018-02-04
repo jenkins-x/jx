edf7fe2b921d00a917800080c93ad99ea9653e5c
James Strachan <james.strachan@gmail.com>
2018-02-04 12:26:18 +0000

    added support for generating a gitea API token using browser automation

--- pkg/jx/cmd/create_git_user.go
+++ pkg/jx/cmd/create_git_user.go
@@ -1,14 +1,24 @@
 package cmd
 
 import (
-	"io"
-
+	"bufio"
+	"context"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+
 	"github.com/jenkins-x/jx/pkg/gits"
 	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
-	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
 	"github.com/jenkins-x/jx/pkg/util"
 	"github.com/spf13/cobra"
+	"github.com/jenkins-x/jx/pkg/auth"
+	"github.com/chromedp/chromedp"
+	"github.com/chromedp/cdproto/cdp"
+	"k8s.io/apimachinery/pkg/util/uuid"
+	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
+	"bytes"
+	"strings"
 )
 
 var (
@@ -60,7 +70,8 @@ func NewCmdCreateGitUser(f cmdutil.Factory, out io.Writer, errOut io.Writer) *co
 	}
 	options.addCommonFlags(cmd)
 	options.GitServerFlags.addGitServerFlags(cmd)
-	cmd.Flags().StringVarP(&options.ApiToken, "--api-token", "t", "", "The API Token for the user")
+	cmd.Flags().StringVarP(&options.ApiToken, "api-token", "t", "", "The API Token for the user")
+	cmd.Flags().StringVarP(&options.Password, "password", "p", "", "The User password to try automatically create a new API Token")
 
 	return cmd
 }
@@ -95,9 +106,16 @@ func (o *CreateGitUserOptions) Run() error {
 		userAuth.ApiToken = o.ApiToken
 	}
 
-	if userAuth.IsInvalid() {
-		tokenUrl := gits.ProviderAccessTokenURL(server.Kind, server.URL)
+	tokenUrl := gits.ProviderAccessTokenURL(server.Kind, server.URL)
 
+	if userAuth.IsInvalid() && o.Password != "" {
+		err := o.tryFindAPITokenFromBrowser(tokenUrl, userAuth)
+		if err != nil {
+			return err
+		}
+	}
+
+	if userAuth.IsInvalid() {
 		o.Printf("Please generate an API Token for server %s\n", server.Label())
 		o.Printf("Click this URL %s\n\n", util.ColorInfo(tokenUrl))
 		o.Printf("Then COPY the token and enter in into the form below:\n\n")
@@ -118,4 +136,108 @@ func (o *CreateGitUserOptions) Run() error {
 	o.Printf("Created user %s API Token for git server %s at %s\n",
 		util.ColorInfo(o.Username), util.ColorInfo(server.Name), util.ColorInfo(server.URL))
 	return nil
+}
+
+// lets try use the users browser to find the API token
+func (o *CreateGitUserOptions) tryFindAPITokenFromBrowser(tokenUrl string, userAuth *auth.UserAuth) error {
+	var err error
+
+	ctxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	file, err := ioutil.TempFile("", "jx-browser")
+	if err != nil {
+		return err
+	}
+	writer := bufio.NewWriter(file)
+	o.Printf("Chrome debugging logs written to: %s\n", util.ColorInfo(file.Name()))
+
+	logger := func(message string, args ...interface{}) {
+		fmt.Fprintf(writer, message+"\n", args...)
+	}
+	c, err := chromedp.New(ctxt, chromedp.WithLog(logger))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.Run(ctxt, chromedp.Tasks{
+		chromedp.Navigate(tokenUrl),
+	})
+	if err != nil {
+		return err
+	}
+
+	nodes := []*cdp.Node{}
+	err = c.Run(ctxt, chromedp.Nodes("//input", &nodes))
+	if err != nil {
+		return err
+	}
+
+	login := false
+	for _, node := range nodes {
+		name := node.AttributeValue("name")
+		if name == "user_name" {
+			login = true
+		}
+	}
+
+	if login {
+		o.Printf("logging in\n")
+		err = c.Run(ctxt, chromedp.Tasks{
+			chromedp.WaitVisible("user_name", chromedp.ByID),
+			chromedp.SendKeys("user_name", userAuth.Username, chromedp.ByID),
+			chromedp.SendKeys("password", o.Password+"\n", chromedp.ByID),
+		})
+		if err != nil {
+			return err
+		}
+	}
+	o.Printf("Generating new token\n")
+
+	tokenId := "jx-" + string(uuid.NewUUID())
+	generateNewTokenButtonSelector := "//div[normalize-space(text())='Generate New Token']"
+
+	tokenResultDivSelector := "//div[@class='ui info message']/p"
+	err = c.Run(ctxt, chromedp.Tasks{
+		chromedp.WaitVisible(generateNewTokenButtonSelector),
+		chromedp.Click(generateNewTokenButtonSelector),
+		chromedp.WaitVisible("name", chromedp.ByID),
+		chromedp.SendKeys("name", tokenId + "\n", chromedp.ByID),
+		chromedp.WaitVisible(tokenResultDivSelector),
+		chromedp.Nodes(tokenResultDivSelector, &nodes),
+	})
+	if err != nil {
+		return err
+	}
+	token := ""
+	for _, node := range nodes {
+		text := nodeText(node)
+		if text != "" && token == "" {
+			token = text
+			break
+		}
+	}
+	o.Printf("Found API Token %s\n", util.ColorInfo(token))
+	if token != "" {
+		userAuth.ApiToken = token
+	}
+
+	err = c.Shutdown(ctxt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func nodeText(node *cdp.Node) string {
+	var buffer bytes.Buffer
+	for _, node := range node.Children {
+		switch node.NodeType {
+		case cdp.NodeTypeText:
+			buffer.WriteString(node.NodeValue)
+		case cdp.NodeTypeElement:
+			buffer.WriteString(nodeText(node))
+		}
+	}
+	return strings.TrimSpace(buffer.String())
 }
