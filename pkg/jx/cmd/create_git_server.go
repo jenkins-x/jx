c7dce038cb193ef100388acebd78d6d01f5e9ac5
James Strachan <james.strachan@gmail.com>
2018-02-07 09:06:44 +0100

    lets make name optional to simplify the CLI
    also lets allow the URL to be discovered for known kinds of hosted git server

--- pkg/jx/cmd/create_git_server.go
+++ pkg/jx/cmd/create_git_server.go
@@ -13,18 +13,23 @@ import (
 var (
 	create_git_server_long = templates.LongDesc(`
 		Adds a new Git Server URL
-        ` + env_description + `
 `)
 
 	create_git_server_example = templates.Examples(`
 		# Add a new git server URL
-		jx create git server gitea mythingy https://my.server.com/
+		jx create git server gitea
 	`)
+
+	gitKindToServiceName = map[string]string{
+		"gitea": "gitea-gitea",
+	}
 )
 
 // CreateGitServerOptions the options for the create spring command
 type CreateGitServerOptions struct {
 	CreateOptions
+
+	Name string
 }
 
 // NewCmdCreateGitServer creates a command object for the "create" command
@@ -40,9 +45,9 @@ func NewCmdCreateGitServer(f cmdutil.Factory, out io.Writer, errOut io.Writer) *
 	}
 
 	cmd := &cobra.Command{
-		Use:     "server [kind] [name] [url]",
-		Short:   "Adds a new git server URL",
-		Aliases: []string{"gitserver"},
+		Use:     "server kind [url]",
+		Short:   "Creates a new git server URL",
+		Aliases: []string{"provider"},
 		Long:    create_git_server_long,
 		Example: create_git_server_example,
 		Run: func(cmd *cobra.Command, args []string) {
@@ -52,20 +57,40 @@ func NewCmdCreateGitServer(f cmdutil.Factory, out io.Writer, errOut io.Writer) *
 			cmdutil.CheckErr(err)
 		},
 	}
-	//addCreateAppFlags(cmd, &options.CreateOptions)
 
+	cmd.Flags().StringVarP(&options.Name, "name", "n", "", "The name for the git server being created")
 	return cmd
 }
 
 // Run implements the command
 func (o *CreateGitServerOptions) Run() error {
 	args := o.Args
-	if len(args) < 3 {
-		return fmt.Errorf("Missing git server URL arguments. Usage: jx create git server kind name url")
+	if len(args) < 1 {
+		return missingArguments()
 	}
 	kind := args[0]
-	name := args[1]
-	gitUrl := args[2]
+	name := o.Name
+	if name == "" {
+		name = kind
+	}
+	gitUrl := ""
+	if len(args) > 1 {
+		gitUrl = args[1]
+	} else {
+		// lets try find the git URL based on the provider
+		serviceName := gitKindToServiceName[kind]
+		if serviceName != "" {
+			url, err := o.findService(serviceName)
+			if err != nil {
+				return fmt.Errorf("Failed to find %s git serivce %s: %s", kind, serviceName, err)
+			}
+			gitUrl = url
+		}
+	}
+
+	if gitUrl == "" {
+		return missingArguments()
+	}
 	authConfigSvc, err := o.Factory.CreateGitAuthConfigService()
 	if err != nil {
 		return err
@@ -79,4 +104,8 @@ func (o *CreateGitServerOptions) Run() error {
 	}
 	o.Printf("Added git server %s for URL %s\n", util.ColorInfo(name), util.ColorInfo(gitUrl))
 	return nil
+}
+
+func missingArguments() error {
+	return fmt.Errorf("Missing git server URL arguments. Usage: jx create git server kind [url]")
 }
