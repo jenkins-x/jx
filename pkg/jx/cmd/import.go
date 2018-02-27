b0eac13210a4e996f35e78911827feef46c4eb63
rawlingsj <rawlingsj80@gmail.com>
2018-02-27 10:43:29 +0000

    feat: add CI pipeline to the default Jenkinsfile we add to repos
    Note: this does not yet deploy to a preview env yet fixes #282

--- pkg/jx/cmd/import.go
+++ pkg/jx/cmd/import.go
@@ -49,66 +49,98 @@ CMD ["java", "-jar", "app.jar"]
 	// TODO replace with the jx-pipelines-plugin version when its available
 	defaultJenkinsfile = `
 pipeline {
-  agent {
-    label "jenkins-maven"
-  }
-
-  environment {
-    ORG 		= 'jenkinsx'
-    APP_NAME    = '%s'
-    GIT_CREDS = credentials('jenkins-x-git')
-    CHARTMUSEUM_CREDS = credentials('jenkins-x-chartmuseum')
-  }
-
-  stages {
-
-    stage('Build Release') {
-      steps {
-        container('maven') {
-          // ensure we're not on a detached head
-          sh "git checkout master"
-
-          // until we switch to the new kubernetes / jenkins credential implementation use git credentials store
-          sh "git config --global credential.helper store"
-
-          // so we can retrieve the version in later steps
-          sh "echo \$(jx-release-version) > VERSION"
-          sh "mvn versions:set -DnewVersion=\$(jx-release-version)"
-        }
-
-        dir ('./charts/%s') {
-          container('maven') {
-            sh "make tag"
-          }
-        }
-
-        container('maven') {
-          sh 'mvn clean deploy'
-
-          sh "docker build -f Dockerfile.release -t $JENKINS_X_DOCKER_REGISTRY_SERVICE_HOST:$JENKINS_X_DOCKER_REGISTRY_SERVICE_PORT/$ORG/$APP_NAME:\$(cat VERSION) ."
-          sh "docker push $JENKINS_X_DOCKER_REGISTRY_SERVICE_HOST:$JENKINS_X_DOCKER_REGISTRY_SERVICE_PORT/$ORG/$APP_NAME:\$(cat VERSION)"
-
-          sh "jx step changelog"
-        }
-      }
+    agent {
+      label "jenkins-maven"
     }
 
-    stage('Promote to Environments') {
-      steps {
-        dir ('./charts/%s') {
+    environment {
+      ORG 		= 'jenkinsx'
+      APP_NAME    = '%s'
+      GIT_CREDS = credentials('jenkins-x-git')
+      CHARTMUSEUM_CREDS = credentials('jenkins-x-chartmuseum')
+    }
+
+    stages {
+      stage('CI Build and push snapshpt') {
+        when {
+          branch 'PR-*'
+        }
+        environment {
+          PREVIEW_VERSION = "0.0.0-SNAPSHOT-$BRANCH_NAME-$BUILD_NUMBER"
+          PREVIEW_NAMESPACE = "$APP_NAME-$BRANCH_NAME".toLowerCase()
+          HELM_RELEASE = "$PREVIEW_NAMESPACE".toLowerCase()
+        }
+        steps {
           container('maven') {
+            sh "mvn versions:set -DnewVersion=$PREVIEW_VERSION"
+            sh "mvn install"
+            sh "docker build -f Dockerfile.release -t $JENKINS_X_DOCKER_REGISTRY_SERVICE_HOST:$JENKINS_X_DOCKER_REGISTRY_SERVICE_PORT/$ORG/$APP_NAME:$PREVIEW_VERSION ."
+            sh "docker push $JENKINS_X_DOCKER_REGISTRY_SERVICE_HOST:$JENKINS_X_DOCKER_REGISTRY_SERVICE_PORT/$ORG/$APP_NAME:$PREVIEW_VERSION"
+          }
 
-			// release the helm chart
-            sh 'make release'
+		  // comment out until draft pack includes preview environment charts
+          //dir ('./charts/preview') {
+          //  container('maven') {
+          //    sh "make preview"
+          //  }
+          //}
+        }
+      }
 
-			// promote through all 'Auto' promotion Environments 
-            sh 'GIT_USERNAME=$GIT_CREDS_USR GIT_API_TOKEN=$GIT_CREDS_PSW jx promote -b --all-auto --timeout 1h --version \$(cat ../../VERSION)'
+      stage('Build Release') {
+        when {
+          branch 'master'
+        }
+        steps {
+          container('maven') {
+            // ensure we're not on a detached head
+            sh "git checkout master"
+
+            // until we switch to the new kubernetes / jenkins credential implementation use git credentials store
+            sh "git config --global credential.helper store"
+
+            // so we can retrieve the version in later steps
+            sh "echo \$(jx-release-version) > VERSION"
+            sh "mvn versions:set -DnewVersion=\$(cat VERSION)"
+          }
+
+          dir ('./charts/%s') {
+            container('maven') {
+              sh "make tag"
+            }
+          }
+
+          container('maven') {
+            sh 'mvn clean deploy'
+            sh "docker build -f Dockerfile.release -t $JENKINS_X_DOCKER_REGISTRY_SERVICE_HOST:$JENKINS_X_DOCKER_REGISTRY_SERVICE_PORT/$ORG/$APP_NAME:\$(cat VERSION) ."
+            sh "docker push $JENKINS_X_DOCKER_REGISTRY_SERVICE_HOST:$JENKINS_X_DOCKER_REGISTRY_SERVICE_PORT/$ORG/$APP_NAME:\$(cat VERSION)"
           }
         }
       }
+
+      stage('Promote to Environments') {
+        environment {
+          GIT_USERNAME = "$GIT_CREDS_USR"
+          GIT_API_TOKEN = "$GIT_CREDS_PSW"
+        }
+        when {
+          branch 'master'
+        }
+        steps {
+          dir ('./charts/%s') {
+            container('maven') {
+              // release the helm chart
+              sh 'make release'
+
+              // promote through all 'Auto' promotion Environments
+              sh 'jx promote -b --all-auto --timeout 1h --version \$(cat ../../VERSION)'
+            }
+          }
+        }
+      }
+
     }
   }
-}
 `
 )
 
