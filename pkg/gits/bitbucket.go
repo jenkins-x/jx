72c248e9616428ace09f060b3cb670af1cd42062
Will Refvem <wrefvem@cloudbees.com>
2018-03-21 23:57:41 -0400

    Implementation of ListOrganisations for Bitbucket GitProvider

--- pkg/gits/bitbucket.go
+++ pkg/gits/bitbucket.go
@@ -1,11 +1,72 @@
 package gits
 
+import (
+	"context"
+
+	"github.com/jenkins-x/jx/pkg/auth"
+	"github.com/wbrefvem/go-bitbucket"
+)
+
+// BitbucketProvider mplements GitProvider interface for bitbucket.org
 type BitbucketProvider struct {
-	Client *APIClient
+	Client   *bitbucket.APIClient
+	Username string
+	Context  context.Context
+
+	Server auth.AuthServer
+	User   auth.UserAuth
+}
+
+// NewBitbucketProvider is a constructor for type BitbucketProvider
+func NewBitbucketProvider(server *auth.AuthServer, user *auth.UserAuth) (GitProvider, error) {
+	ctx := context.Background()
+
+	basicAuthContext := context.WithValue(
+		ctx,
+		bitbucket.ContextBasicAuth,
+		bitbucket.BasicAuth{
+			UserName: user.Username,
+			// App Password, equivalent to GH personal access token
+			Password: user.ApiToken,
+		},
+	)
+
+	provider := BitbucketProvider{
+		Server:   *server,
+		User:     *user,
+		Username: user.Username,
+		Context:  basicAuthContext,
+	}
+
+	cfg := bitbucket.NewConfiguration()
+	// FIXME: don't use DefaultClient
+	provider.Client = bitbucket.NewAPIClient(cfg)
+
+	return &provider, nil
 }
 
 func (b *BitbucketProvider) ListOrganisations() ([]GitOrganisation, error) {
-	return nil, nil
+
+	// Organizations i
+	teams, response, err := b.Client.TeamsApi.TeamsGet(b.Context, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Some said his numbers were magical. Others said they were int-significant.
+	// TODO: process 404 "not a team account" responses
+	if response.StatusCode == 404 {
+		return nil, err
+	}
+
+	bTeams := []GitOrganisation{}
+
+	for _, team := range teams.Values {
+		bTeams = append(bTeams, GitOrganisation{Login: team.Username})
+	}
+
+	return bTeams, nil
 }
 
 func (b *BitbucketProvider) ListRepositories(org string) ([]*GitRepository, error) {
@@ -13,7 +74,7 @@ func (b *BitbucketProvider) ListRepositories(org string) ([]*GitRepository, erro
 }
 
 func (b *BitbucketProvider) CreateRepository(org string, name string, private bool) (*GitRepository, error) {
-	return nil, nil, nil
+	return nil, nil
 }
 
 func (b *BitbucketProvider) GetRepository(org string, name string) (*GitRepository, error) {
@@ -45,7 +106,7 @@ func (b *BitbucketProvider) UpdatePullRequestStatus(pr *GitPullRequest) error {
 }
 
 func (b *BitbucketProvider) PullRequestLastCommitStatus(pr *GitPullRequest) (string, error) {
-	return nil, nil
+	return "", nil
 }
 
 func (b *BitbucketProvider) ListCommitStatus(org string, repo string, sha string) ([]*GitRepoStatus, error) {
@@ -76,7 +137,7 @@ func (b *BitbucketProvider) CreateIssueComment(owner string, repo string, number
 	return nil
 }
 
-func (b *BibucketProvider) HasIssues() {
+func (b *BitbucketProvider) HasIssues() bool {
 	return true
 }
 
@@ -84,10 +145,22 @@ func (b *BitbucketProvider) IsGitHub() bool {
 	return false
 }
 
-func (b *BitbuckProvider) IsGitea() bool {
+func (b *BitbucketProvider) IsGitea() bool {
 	return false
 }
 
 func (b *BitbucketProvider) Kind() string {
 	return "bitbucket"
+}
+
+func (b *BitbucketProvider) JenkinsWebHookPath(gitURL string, secret string) string {
+	return ""
+}
+
+func (b *BitbucketProvider) Label() string {
+	return ""
+}
+
+func (b *BitbucketProvider) UpdateRelease(owner string, repo string, tag string, releaseInfo *GitRelease) error {
+	return nil
 }
