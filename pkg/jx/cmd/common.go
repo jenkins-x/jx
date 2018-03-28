1dd445114f8677f18c31414561d3ccbb8b324bc1
James Strachan <james.strachan@gmail.com>
2018-03-28 09:09:46 +0100

    fix: bigger default timeout for addons

--- pkg/jx/cmd/common.go
+++ pkg/jx/cmd/common.go
@@ -485,7 +485,8 @@ func (o *CommonOptions) installChart(releaseName string, chart string, version s
 			return err
 		}
 	}
-	args := []string{"upgrade", "--install"}
+	timeout := fmt.Sprintf("--timeout=%s", defaultInstallTimeout)
+	args := []string{"upgrade", "--install", timeout}
 	if version != "" {
 		args = append(args, "--version", version)
 	}
