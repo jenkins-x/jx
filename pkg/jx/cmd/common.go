de3d2af0358bcf58866622c709d4ae22e21aff08
James Strachan <james.strachan@gmail.com>
2018-04-09 07:21:28 +0100

    chore: refactor methods into separate file

--- pkg/jx/cmd/common.go
+++ pkg/jx/cmd/common.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"net/url"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
@@ -71,64 +70,6 @@ func (c *CommonOptions) CreateTable() table.Table {
 
 func (c *CommonOptions) Printf(format string, a ...interface{}) (n int, err error) {
 	return fmt.Fprintf(c.Out, format, a...)
-}
-
-func (o *CommonOptions) runCommandFromDir(dir, name string, args ...string) error {
-	e := exec.Command(name, args...)
-	if dir != "" {
-		e.Dir = dir
-	}
-	e.Stdout = o.Out
-	e.Stderr = o.Err
-	err := e.Run()
-	if err != nil {
-		o.Printf("Error: Command failed  %s %s\n", name, strings.Join(args, " "))
-	}
-	return err
-}
-
-func (o *CommonOptions) runCommand(name string, args ...string) error {
-	e := exec.Command(name, args...)
-	e.Stdout = o.Out
-	e.Stderr = o.Err
-	err := e.Run()
-	if err != nil {
-		o.Printf("Error: Command failed  %s %s\n", name, strings.Join(args, " "))
-	}
-	return err
-}
-
-func (o *CommonOptions) runCommandQuietly(name string, args ...string) error {
-	e := exec.Command(name, args...)
-	e.Stdout = o.Out
-	e.Stderr = o.Err
-	return e.Run()
-}
-
-func (o *CommonOptions) runCommandInteractive(interactive bool, name string, args ...string) error {
-	e := exec.Command(name, args...)
-	e.Stdout = o.Out
-	e.Stderr = o.Err
-	if interactive {
-		e.Stdin = os.Stdin
-	}
-	err := e.Run()
-	if err != nil {
-		o.Printf("Error: Command failed  %s %s\n", name, strings.Join(args, " "))
-	}
-	return err
-}
-
-// getCommandOutput evaluates the given command and returns the trimmed output
-func (o *CommonOptions) getCommandOutput(dir string, name string, args ...string) (string, error) {
-	e := exec.Command(name, args...)
-	if dir != "" {
-		e.Dir = dir
-	}
-	data, err := e.CombinedOutput()
-	text := string(data)
-	text = strings.TrimSpace(text)
-	return text, err
 }
 
 func (options *CommonOptions) addCommonFlags(cmd *cobra.Command) {
