27245e435d542cebc7347f547952d81246502f17
James Strachan <james.strachan@gmail.com>
2018-04-23 04:23:44 +0100

    fix:(minikube) disable docker-machine-driver-hyperkit installer
    which doesn't seem to work any more

--- pkg/jx/cmd/common_install.go
+++ pkg/jx/cmd/common_install.go
@@ -187,36 +187,37 @@ func (o *CommonOptions) getLatestVersionFromKubernetesReleaseUrl() (sem semver.V
 }
 
 func (o *CommonOptions) installHyperkit() error {
-	info, err := o.getCommandOutput("", "docker-machine-driver-hyperkit")
-	if strings.Contains(info, "Docker") {
-		o.Printf("docker-machine-driver-hyperkit is already installed\n")
-		return nil
-	}
-	o.Printf("Result: %s and %v\n", info, err)
-/*	err = o.runCommand("curl", "-LO", "https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-hyperkit")
-	if err != nil {
-		return err
-	}
+	/*
+		info, err := o.getCommandOutput("", "docker-machine-driver-hyperkit")
+		if strings.Contains(info, "Docker") {
+			o.Printf("docker-machine-driver-hyperkit is already installed\n")
+			return nil
+		}
+		o.Printf("Result: %s and %v\n", info, err)
+		err = o.runCommand("curl", "-LO", "https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-hyperkit")
+		if err != nil {
+			return err
+		}
 
-	err = o.runCommand("chmod", "+x", "docker-machine-driver-hyperkit")
-	if err != nil {
-		return err
-	}
+		err = o.runCommand("chmod", "+x", "docker-machine-driver-hyperkit")
+		if err != nil {
+			return err
+		}
 
-	log.Warn("Installing hyperkit does require sudo to perform some actions, for more details see https://github.com/kubernetes/minikube/blob/master/docs/drivers.md#hyperkit-driver")
+		log.Warn("Installing hyperkit does require sudo to perform some actions, for more details see https://github.com/kubernetes/minikube/blob/master/docs/drivers.md#hyperkit-driver")
 
-	err = o.runCommand("sudo", "mv", "docker-machine-driver-hyperkit", "/usr/local/bin/")
-	if err != nil {
-		return err
-	}
+		err = o.runCommand("sudo", "mv", "docker-machine-driver-hyperkit", "/usr/local/bin/")
+		if err != nil {
+			return err
+		}
 
-	err = o.runCommand("sudo", "chown", "root:wheel", "/usr/local/bin/docker-machine-driver-hyperkit")
-	if err != nil {
-		return err
-	}
+		err = o.runCommand("sudo", "chown", "root:wheel", "/usr/local/bin/docker-machine-driver-hyperkit")
+		if err != nil {
+			return err
+		}
 
-	return o.runCommand("sudo", "chmod", "u+s", "/usr/local/bin/docker-machine-driver-hyperkit")
-*/
+		return o.runCommand("sudo", "chmod", "u+s", "/usr/local/bin/docker-machine-driver-hyperkit")
+	*/
 	return nil
 }
 
