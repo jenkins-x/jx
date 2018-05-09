eb1cb1add1f96e0316d3f5b34a465f3a6aee72b0
Henryk Konsek <hekonsek@gmail.com>
2018-05-09 10:26:00 +0200

    Added common_commands tests.

---
+++ pkg/jx/cmd/common_commands_test.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"bytes"
+)
+
+func TestExecuteCommand(t *testing.T) {
+	o := CommonOptions{}
+	err := o.runCommand("echo", "foo")
+	assert.Nil(t, err)
+
+}
+
+func TestCommandError(t *testing.T) {
+	o := CommonOptions{}
+	err := o.runCommand("noSuchCommand")
+	assert.NotNil(t, err)
+
+}
+
+func TestVerboseOutput(t *testing.T) {
+	out := new(bytes.Buffer)
+	o := CommonOptions{Verbose: true, Out: out}
+	o.runCommand("echo", "foo")
+	assert.Equal(t, out.String(), "foo\n")
+}
+
+func TestNonVerboseOutput(t *testing.T) {
+	out := new(bytes.Buffer)
+	o := CommonOptions{Out: out}
+	o.runCommand("echo", "foo")
+	assert.Empty(t, out.String())
+}
