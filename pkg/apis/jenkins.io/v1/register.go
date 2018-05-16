53b7ab69822a5dc76a92038dab13953e25dd243f
Keith Zantow <kzantow@gmail.com>
2018-05-16 16:39:20 -0400

    add UserList

--- pkg/apis/jenkins.io/v1/register.go
+++ pkg/apis/jenkins.io/v1/register.go
@@ -48,6 +48,7 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&Release{},
 		&ReleaseList{},
 		&User{},
+		&UserList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
