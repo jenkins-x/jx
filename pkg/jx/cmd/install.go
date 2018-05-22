55ad35e41ec287d4aa29fd092b0a67fa41905924
James Strachan <james.strachan@gmail.com>
2018-05-22 06:41:20 +0100

    remove unnecessary warning

--- pkg/jx/cmd/install.go
+++ pkg/jx/cmd/install.go
@@ -262,8 +262,6 @@ func (options *InstallOptions) Run() error {
 	if exposeController != nil && exposeController.Config.Domain == "" && options.Flags.Domain != "" {
 		exposeController.Config.Domain = options.Flags.Domain
 		log.Success("set exposeController Config Domain " + exposeController.Config.Domain + "\n")
-	} else {
-		log.Error("Did not set exposeController Config Domain\n")
 	}
 	if exposeController != nil {
 		if isOpenShiftProvider(options.Flags.Provider) {
