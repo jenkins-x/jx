413d7de23890489f101ca094391c5b4cb811c12c
James Strachan <james.strachan@gmail.com>
2018-05-23 12:07:36 +0100

    fix:(jx delete preview) allow the preview to be chosen in non-batch mode

--- pkg/jx/cmd/delete_preview.go
+++ pkg/jx/cmd/delete_preview.go
@@ -3,8 +3,10 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/jenkins-x/jx/pkg/kube"
+	"github.com/jenkins-x/jx/pkg/util"
 	"github.com/spf13/cobra"
 
 	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
@@ -40,13 +42,13 @@ func NewCmdDeletePreview(f cmdutil.Factory, out io.Writer, errOut io.Writer) *co
 		},
 	}
 	options.addPreviewOptions(cmd)
+	options.addCommonFlags(cmd)
 	return cmd
 }
 
 // Run implements this command
 func (o *DeletePreviewOptions) Run() error {
-	f := o.Factory
-	kubeClient, currentNs, err := f.CreateClient()
+	kubeClient, currentNs, err := o.KubeClient()
 	if err != nil {
 		return err
 	}
@@ -55,19 +57,51 @@ func (o *DeletePreviewOptions) Run() error {
 		return err
 	}
 
-	err = o.defaultValues(ns)
+	err = o.defaultValues(ns, o.BatchMode)
 	if err != nil {
+		if !o.BatchMode {
+			jxClient, _, err := o.JXClient()
+			if err != nil {
+				return err
+			}
+
+			// lets let the user pick from a list of preview environments to delete
+			names, err := kube.GetFilteredEnvironmentNames(jxClient, ns, kube.IsPreviewEnvironment)
+			if err != nil {
+				return err
+			}
+			selected, err := util.PickNames(names, "Pick preview environments to delete: ")
+			if err != nil {
+				return err
+			}
+			deletePreviews := strings.Join(selected, ", ")
+			if !util.Confirm("You are about to delete the Preview environments: "+deletePreviews, false, "The list of Preview Enviroments to be deleted") {
+				return nil
+			}
+
+			for _, name := range selected {
+				err = o.deletePreview(name)
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		}
 		return err
 	}
 
 	if o.Name == "" {
 		return fmt.Errorf("Could not default the preview environment name!")
 	}
+	return o.deletePreview(o.Name)
+}
 
+func (o *DeletePreviewOptions) deletePreview(name string) error {
+	o.Printf("Deleting preview environment: %s\n", util.ColorInfo(name))
 	deleteOptions := &DeleteEnvOptions{
 		CommonOptions:   o.CommonOptions,
 		DeleteNamespace: true,
 	}
-	deleteOptions.Args = []string{o.Name}
+	deleteOptions.Args = []string{name}
 	return deleteOptions.Run()
 }
