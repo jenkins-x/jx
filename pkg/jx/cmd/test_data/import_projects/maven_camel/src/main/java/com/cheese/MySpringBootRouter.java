fb3a359809a193ce5c55597ebb289a45be6556f2
James Strachan <james.strachan@gmail.com>
2018-06-11 09:11:46 +0100

    fix:(import) use correct health probe for older spring boot versions
    fixes #1004

---
+++ pkg/jx/cmd/test_data/import_projects/maven_camel/src/main/java/com/cheese/MySpringBootRouter.java
@@ -0,0 +1,25 @@
+package com.cheese;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
+ * <p/>
+ * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
+ */
+@Component
+public class MySpringBootRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        from("timer:hello?period={{timer.period}}").routeId("hello")
+            .transform().method("myBean", "saySomething")
+            .filter(simple("${body} contains 'foo'"))
+                .to("log:foo")
+            .end()
+            .to("stream:out");
+    }
+
+}
