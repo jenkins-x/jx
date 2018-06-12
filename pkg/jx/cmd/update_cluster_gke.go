0f67be7c7431221756411c968c87ec64ca24465b
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-06-12 13:25:48 +0100

    feat: added 'jx update cluster gke terraform' command

---
+++ pkg/jx/cmd/update_cluster_gke.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"io"
+
+	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
+	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
+	"github.com/spf13/cobra"
+)
+
+// CreateClusterOptions the flags for running create cluster
+type UpdateClusterGKEOptions struct {
+	UpdateClusterOptions
+}
+
+var (
+	updateClusterGKELong = templates.LongDesc(`
+		This command creates a new kubernetes cluster on GKE, installing required local dependencies and provisions the
+		Jenkins X platform
+
+		You can see a demo of this command here: [http://jenkins-x.io/demos/create_cluster_gke/](http://jenkins-x.io/demos/create_cluster_gke/)
+
+		Google Kubernetes Engine is a managed environment for deploying containerized applications. It brings our latest
+		innovations in developer productivity, resource efficiency, automated operations, and open source flexibility to
+		accelerate your time to market.
+
+		Google has been running production workloads in containers for over 15 years, and we build the best of what we
+		learn into Kubernetes, the industry-leading open source container orchestrator which powers Kubernetes Engine.
+
+`)
+
+	updateClusterGKEExample = templates.Examples(`
+
+		jx update cluster gke
+
+`)
+)
+
+func NewCmdUpdateClusterGKE(f cmdutil.Factory, out io.Writer, errOut io.Writer) *cobra.Command {
+	options := createUpdateClusterGKEOptions(f, out, errOut, GKE)
+
+	cmd := &cobra.Command{
+		Use:     "gke",
+		Short:   "Updates an existing kubernetes cluster on GKE: Runs on Google Cloud",
+		Long:    updateClusterGKELong,
+		Example: updateClusterGKEExample,
+		Run: func(cmd *cobra.Command, args []string) {
+			options.Cmd = cmd
+			options.Args = args
+			err := options.Run()
+			cmdutil.CheckErr(err)
+		},
+	}
+
+	cmd.AddCommand(NewCmdUpdateClusterGKETerraform(f, out, errOut))
+
+	return cmd
+}
+
+func createUpdateClusterGKEOptions(f cmdutil.Factory, out io.Writer, errOut io.Writer, cloudProvider string) UpdateClusterGKEOptions {
+	commonOptions := CommonOptions{
+		Factory: f,
+		Out:     out,
+		Err:     errOut,
+	}
+	options := UpdateClusterGKEOptions{
+		UpdateClusterOptions: UpdateClusterOptions{
+			UpdateOptions: UpdateOptions{
+				CommonOptions: commonOptions,
+			},
+			Provider:       cloudProvider,
+		},
+	}
+	return options
+}
+
+
+func (o *UpdateClusterGKEOptions) Run() error {
+	return o.Cmd.Help()
+}
+
