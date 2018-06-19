cd51003ae4db7b434ea74a9516313c12172ca0ca
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-06-19 11:39:01 +0200

    find the application associated with an issue

--- pkg/jx/cmd/get_issue.go
+++ pkg/jx/cmd/get_issue.go
@@ -2,12 +2,14 @@ package cmd
 
 import (
 	"io"
+	"os/user"
 	"regexp"
 	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 
 	"github.com/jenkins-x/jx/pkg/apis/jenkins.io/v1"
 	"github.com/jenkins-x/jx/pkg/gits"
@@ -81,7 +83,7 @@ func (o *GetIssueOptions) Run() error {
 	}
 
 	table := o.CreateTable()
-	table.AddRow("ISSUE", "STATUS", "ENVIRONMENT")
+	table.AddRow("ISSUE", "STATUS", "APPLICATION", "ENVIRONMENT")
 
 	f := o.Factory
 	client, ns, err := f.CreateJXClient()
@@ -103,6 +105,11 @@ func (o *GetIssueOptions) Run() error {
 		return errors.Wrap(err, "cannot list the environments")
 	}
 
+	kubeClient, _, err := f.CreateClient()
+	if err != nil {
+		return errors.Wrap(err, "failed to create the Kubernetes client")
+	}
+
 	found := false
 	for _, env := range envList.Items {
 		envNs, err := kube.GetEnvironmentNamespace(client, ns, env.Name)
@@ -117,11 +124,20 @@ func (o *GetIssueOptions) Run() error {
 		if rel == nil {
 			continue
 		}
-		table.AddRow(issue.URL, *issue.State, env.Name)
-		found = true
+
+		apps, err := o.getApplications(kubeClient, &env, envNs)
+		if err != nil {
+			continue
+		}
+		for _, app := range apps {
+			if o.match(issue.URL, app) {
+				table.AddRow(issue.URL, *issue.State, app, env.Name)
+				found = true
+			}
+		}
 	}
 	if !found {
-		table.AddRow(issue.URL, *issue.State, "")
+		table.AddRow(issue.URL, *issue.State, "", "")
 	}
 	table.Render()
 	return nil
@@ -187,4 +203,43 @@ func (o *GetIssueOptions) convertIssueIDsToURLs(tracker issues.IssueProvider, is
 		issueURLs = append(issueURLs, issue.URL)
 	}
 	return issueURLs
+}
+
+func (o *GetIssueOptions) getApplications(client kubernetes.Interface, env *v1.Environment, envNs string) ([]string, error) {
+	apps := []string{}
+	deployments, err := kube.GetDeployments(client, envNs)
+	if err != nil {
+		return apps, errors.Wrap(err, "failed to retrieve the application deployments")
+	}
+
+	for k, _ := range deployments {
+		appName := kube.GetAppName(k, envNs)
+		if env.Spec.Kind == v1.EnvironmentKindTypeEdit {
+			if appName == kube.DeploymentExposecontrollerService {
+				continue
+			}
+			currUser, err := user.Current()
+			if err == nil {
+				if currUser.Username != env.Spec.PreviewGitSpec.User.Username {
+					continue
+				}
+			}
+			appName = kube.GetEditAppName(appName)
+		} else if env.Spec.Kind == v1.EnvironmentKindTypePreview {
+			appName = env.Spec.PullRequestURL
+		}
+		apps = append(apps, appName)
+	}
+
+	return apps, nil
+}
+
+func (o *GetIssueOptions) match(issueURL string, appName string) bool {
+	isssueURLParts := strings.Split(issueURL, "/")
+	for _, issueURLPart := range isssueURLParts {
+		if issueURLPart == appName {
+			return true
+		}
+	}
+	return false
 }
