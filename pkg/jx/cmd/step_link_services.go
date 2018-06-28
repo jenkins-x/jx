30e5e475c56996f78b789cb856ebbe993cb384c0
angoothachap <angoothachap@gmail.com>
2018-06-27 20:10:42 -0400

    Writing run

--- pkg/jx/cmd/step_link_services.go
+++ pkg/jx/cmd/step_link_services.go
@@ -3,8 +3,19 @@ package cmd
 import (
 	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
 	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
+	"github.com/jenkins-x/jx/pkg/kube"
+	"github.com/jenkins-x/jx/pkg/util"
 	"github.com/spf13/cobra"
 	"io"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	fromNamespace = "from-namespace"
+	toNamespace   = "to-namespace"
+	includes      = "includes"
+	excludes      = "excludes"
+	blankString   = ""
 )
 
 var (
@@ -22,15 +33,16 @@ var (
 	#Link services from jx-staging namespace to the jx-prod namespace including all but the ones starting with  the characters 'cheese'
 	jx step link services --from-namespace jx-staging --to-namespace jx-prod --includes * --excludes cheese*
 `)
+	currentNamespace = blankString
 )
 
 // StepLinkServicesOptions contains the command line flags
 type StepLinkServicesOptions struct {
 	StepOptions
-	FromNameSpace string
-	ToNameSpace   string
-	Includes      string
-	Excludes      string
+	FromNamespace string
+	ToNamespace   string
+	Includes      []string
+	Excludes      []string
 }
 
 // NewCmdStepLinkServices Creates a new Command object
@@ -59,14 +71,45 @@ func NewCmdStepLinkServices(f cmdutil.Factory, out io.Writer, errOut io.Writer) 
 	}
 	options.addCommonFlags(cmd)
 
-	cmd.Flags().StringVarP(&options.FromNameSpace, "from-namespace", "f", "", "The source namespace from which the linking would happen")
-	cmd.Flags().StringVarP(&options.ToNameSpace, "to-namespace", "t", "", "The destination namespace to which the linking would happen")
-	cmd.Flags().StringVarP(&options.Includes, "includes", "i", "", "What services from source namespace to include in the linking process")
-	cmd.Flags().StringVarP(&options.Excludes, "excludes", "e", "", "What services from the source namespace to exclude from the linking process")
+	cmd.Flags().StringVarP(&options.FromNamespace, fromNamespace, "f", blankString, "The source namespace from which the linking would happen")
+	cmd.Flags().StringVarP(&options.ToNamespace, toNamespace, "t", blankString, "The destination namespace to which the linking would happen")
+	cmd.Flags().StringArrayVarP(&options.Includes, includes, "i", []string{}, "What services from source namespace to include in the linking process")
+	cmd.Flags().StringArrayVarP(&options.Excludes, excludes, "e", []string{}, "What services from the source namespace to exclude from the linking process")
 	return cmd
 }
 
 // Run implements this command
 func (o *StepLinkServicesOptions) Run() error {
-	return o.Cmd.Help()
+	if o.FromNamespace == blankString {
+		return util.MissingOption(fromNamespace)
+	}
+	currentNamespace = o.ToNamespace
+	if currentNamespace == blankString {
+		//Derive current namespace since o.ToNameSpace is blank
+		config, po, err := kube.LoadConfig()
+		if err != nil {
+			return err
+		}
+		currentNamespace = kube.CurrentNamespace(config)
+	}
+	if currentNamespace == blankString {
+		return util.MissingOption(toNamespace)
+	} else {
+		serviceList, err := o.kubeClient.CoreV1().Services(o.FromNamespace).List(metav1.ListOptions{})
+		if err != nil {
+			return err
+		} /*else {
+			for _, service := range serviceList.Items {
+				if util.StringMatchesAny(service.Name, o.Includes, o.Excludes) {
+					ing, err := kube.GetServiceURLFromName(o.kubeClient, service.Name, o.FromNamespace)
+					err = kube.CreateServiceLink(o.kubeClient,  o.FromNamespace,currentNamespace, service.Name, ing)
+
+
+					o.kubeClient.CoreV1().Services(currentNamespace).Create(service.Name)
+					// TODO create a Service resource if one does not exist with `service.Name` in namespace toNs with an external name pointing to this service
+				}
+			}
+		}*/
+	}
+	return nil //TODO change return
 }
