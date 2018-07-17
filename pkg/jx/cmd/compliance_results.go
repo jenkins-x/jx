51d06741c3f3e40df9d7b1caf3a051b0aa18f598
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-07-17 17:58:57 +0200

    fix:(compliance) check the status of the compliance before retrieving the results

--- pkg/jx/cmd/compliance_results.go
+++ pkg/jx/cmd/compliance_results.go
@@ -10,12 +10,13 @@ import (
 
 	"github.com/heptio/sonobuoy/pkg/client"
 	"github.com/heptio/sonobuoy/pkg/client/results"
+	"github.com/heptio/sonobuoy/pkg/plugin/aggregation"
 	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
-	"github.com/jenkins-x/jx/pkg/log"
 	"github.com/jenkins-x/jx/pkg/util"
 	"github.com/onsi/ginkgo/reporters"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 )
 
 var (
@@ -68,45 +69,50 @@ func (o *ComplianceResultsOptions) Run() error {
 		return errors.Wrap(err, "could not create the compliance client")
 	}
 
+	status, err := cc.GetStatus(complianceNamespace)
+	if err != nil {
+		return errors.Wrap(err, "failed to retrieve the compliance status")
+	}
+
+	if status.Status != aggregation.CompleteStatus {
+		return errors.New("Compliance results not ready. Run `jx compliance status` for status.")
+	}
+
 	cfg := &client.RetrieveConfig{
 		Namespace: complianceNamespace,
 	}
 
 	reader, errch := cc.RetrieveResults(cfg)
-
-	// TODO Refactor? Seems like there should be a nicer way to handle this...
-	go func() {
-		for {
-			select {
-			case err := <-errch:
-				if err != nil {
-					log.Fatalf("could not retrieve the compliance results\n %v", err)
-					o.Exit(1)
-				}
-			}
+	eg := &errgroup.Group{}
+	eg.Go(func() error { return <-errch })
+	eg.Go(func() error {
+		resultsReader, err := untarResults(reader)
+		if err != nil {
+			return errors.Wrap(err, "could not extract the compliance results from archive")
 		}
-	}()
 
-	resultsReader, err := untarResults(reader)
-	if err != nil {
-		return errors.Wrap(err, "could not extract the compliance results from archive")
-	}
+		gzr, err := gzip.NewReader(resultsReader)
+		if err != nil {
+			return errors.Wrap(err, "could not create a gzip reader for compliance results ")
+		}
 
-	gzr, err := gzip.NewReader(resultsReader)
-	if err != nil {
-		return errors.Wrap(err, "could not create a gzip reader for compliance results ")
-	}
+		testResults, err := cc.GetTests(gzr, "all")
+		if err != nil {
+			return errors.Wrap(err, "could not get the results of the compliance tests from the archive")
+		}
+		testResults = filterTests(
+			func(tc reporters.JUnitTestCase) bool {
+				return !results.Skipped(tc)
+			}, testResults)
+		sort.Sort(StatusSortedTestCases(testResults))
+		o.printResults(testResults)
+		return nil
+	})
 
-	testResults, err := cc.GetTests(gzr, "all")
+	err = eg.Wait()
 	if err != nil {
-		return errors.Wrap(err, "could not get the results of the compliance tests from the archive")
+		return errors.Wrap(err, "failed to retrieve the results")
 	}
-	testResults = filterTests(
-		func(tc reporters.JUnitTestCase) bool {
-			return !results.Skipped(tc)
-		}, testResults)
-	sort.Sort(StatusSortedTestCases(testResults))
-	o.printResults(testResults)
 	return nil
 }
 
