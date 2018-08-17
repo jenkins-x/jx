f5e54560ffde57ad403b5fcd8d451d7ae1ae375f
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-08-17 16:06:33 +0200

    fix:(update ingress) use 'prod' as default ACME service
    ACME staging service issue a fake certificate which can not be verified.

--- pkg/jx/cmd/upgrade_ingress.go
+++ pkg/jx/cmd/upgrade_ingress.go
@@ -3,16 +3,17 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"io"
+	"strings"
+	"time"
+
 	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
 	"github.com/jenkins-x/jx/pkg/kube"
 	"github.com/jenkins-x/jx/pkg/log"
 	"github.com/jenkins-x/jx/pkg/util"
 	"github.com/spf13/cobra"
 	"gopkg.in/AlecAivazis/survey.v1"
-	"io"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"strings"
-	"time"
 )
 
 var (
@@ -258,7 +259,7 @@ func (o *UpgradeIngressOptions) confirmExposecontrollerConfig() error {
 		o.IngressConfig.TLS = util.Confirm("If your network is publicly available would you like to enable cluster wide TLS?", true, "Enables cert-manager and configures TLS with signed certificates from LetsEncrypt")
 
 		if o.IngressConfig.TLS {
-			clusterIssuer, err := util.PickNameWithDefault([]string{"prod", "staging"}, "Use LetsEncrypt staging or production?  Warning if testing use staging else you may be rate limited:", "staging")
+			clusterIssuer, err := util.PickNameWithDefault([]string{"prod", "staging"}, "Use LetsEncrypt production or staging?  Warning staging issues a fake certificate:", "prod")
 			if err != nil {
 				return err
 			}
