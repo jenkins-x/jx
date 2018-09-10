c8424c132159a9e019f02c2bbc9d1f06b00e6847
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-09-10 16:10:05 +0200

    feat:(login) Login the user using the chrome browser and extract the SSO cookie

--- pkg/jx/cmd/login.go
+++ pkg/jx/cmd/login.go
@@ -2,13 +2,25 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
-	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/runner"
+	"github.com/hpcloud/tail"
+	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+)
 
-	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
+const (
+	ssoCookieName      = "sso-cb-cdx"
+	onboardingEndpoint = "/api/v1/users/loggedin"
 )
 
 type LoginOptions struct {
@@ -55,27 +67,138 @@ func NewCmdLogin(f Factory, out io.Writer, errOut io.Writer) *cobra.Command {
 }
 
 func (o *LoginOptions) Run() error {
-	return o.login()
+	cookie, err := o.Login()
+	if err != nil {
+		return errors.Wrap(err, "loging into the CloudBees application")
+	}
+	if cookie == "" {
+		return errors.New("failed to log into the CloudBees application")
+	}
+
+	resp, err := o.onboardUser(cookie)
+	if err != nil {
+		return errors.Wrap(err, "onboarding user")
+	}
+
+	fmt.Println(*resp)
+	return nil
 }
 
-func (o *LoginOptions) login() error {
+func (o *LoginOptions) Login() (string, error) {
 	url := o.URL
 	if url == "" {
-		return errors.New("please povide the URL of the CloudBees application in '--url' option")
+		return "", errors.New("please povide the URL of the CloudBees application in '--url' option")
 	}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	chrome, err := chromedp.New(ctx)
+	userDataDir, err := ioutil.TempDir("/tmp", "jx-login-chrome-userdata-dir")
 	if err != nil {
-		return errors.Wrap(err, "creating chrome client")
+		return "", errors.Wrap(err, "creating the chrome user data dir")
+	}
+	defer os.RemoveAll(userDataDir)
+
+	netLogFile := filepath.Join(userDataDir, "net-logs.json")
+	options := func(m map[string]interface{}) error {
+		m["start-url"] = o.URL
+		m["user-data-dir"] = userDataDir
+		m["log-net-log"] = netLogFile
+		m["net-log-capture-mode"] = "IncludeCookiesAndCredentials"
+		m["v"] = 1
+		return nil
 	}
 
-	err = chrome.Run(ctx, chromedp.Tasks{
-		chromedp.Navigate(url),
-	})
+	r, err := runner.New(options)
 	if err != nil {
-		return errors.Wrap(err, "open chrome with CloudBees application URL")
+		return "", errors.Wrap(err, "creating chrome runner")
 	}
-	return chrome.Wait()
+
+	err = r.Start(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "starting chrome")
+	}
+
+	t, err := tail.TailFile(netLogFile, tail.Config{
+		Follow: true,
+		Logger: log.New(ioutil.Discard, "", log.LstdFlags)})
+	if err != nil {
+		return "", errors.Wrap(err, "reading the netlog file")
+	}
+	cookie := ""
+	pattern := fmt.Sprintf("%s=", ssoCookieName)
+	for line := range t.Lines {
+		if strings.Contains(line.Text, pattern) {
+			fmt.Println(line.Text)
+			cookie = ExtractSsoCookie(line.Text)
+			break
+		}
+	}
+
+	err = r.Shutdown(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "shutting down Chrome")
+	}
+
+	err = r.Wait()
+	if err != nil {
+		return "", errors.Wrap(err, "waiting for Chrome  to exit")
+	}
+
+	return cookie, nil
+}
+
+func (o *LoginOptions) onboardUser(cookie string) (*string, error) {
+	client := http.Client{}
+	req, err := http.NewRequest("GET", o.onboardingURL(), nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "building onboarding request")
+	}
+	req.Header.Add("Accept", "application/json")
+	ssoCookie := http.Cookie{Name: ssoCookieName, Value: cookie}
+	req.AddCookie(&ssoCookie)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "executing onboarding request")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("user onboarding status code: %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "reading user onboarding information from response body")
+	}
+
+	userInfo := string(body)
+	return &userInfo, nil
+}
+
+func (o *LoginOptions) onboardingURL() string {
+	url := o.URL
+	if strings.HasPrefix(url, "/") {
+		url = strings.TrimPrefix(url, "/")
+	}
+	return url + onboardingEndpoint
+}
+
+func ExtractSsoCookie(text string) string {
+	cookiePattern := fmt.Sprintf("%s=", ssoCookieName)
+	start := strings.Index(text, cookiePattern)
+	if start < 0 {
+		return ""
+	}
+	end := -1
+	cookieStart := start + len(cookiePattern)
+	for i, ch := range text[cookieStart:] {
+		if ch == ';' {
+			end = cookieStart + i
+			break
+		}
+	}
+	if end < 0 {
+		return ""
+	}
+	return text[cookieStart:end]
 }
