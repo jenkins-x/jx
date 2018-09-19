f732568b5de4c9936f62ba832a6f3fde4a759f1c
Mark Nielsen <mark.nielsen@blackboard.com>
2018-09-19 15:33:26 -0700

    feat: add flags to delete cluster and user to jx delete context

--- pkg/jx/cmd/delete_context.go
+++ pkg/jx/cmd/delete_context.go
@@ -34,8 +34,10 @@ var (
 type DeleteContextOptions struct {
 	CreateOptions
 
-	SelectAll    bool
-	SelectFilter string
+	SelectAll      bool
+	SelectFilter   string
+	DeleteAuthInfo bool
+	DeleteCluster  bool
 }
 
 // NewCmdDeleteContext creates a command object for the "delete repo" command
@@ -67,6 +69,9 @@ func NewCmdDeleteContext(f Factory, out io.Writer, errOut io.Writer) *cobra.Comm
 
 	cmd.Flags().BoolVarP(&options.SelectAll, "all", "a", false, "Selects all the matched contexts")
 	cmd.Flags().StringVarP(&options.SelectFilter, "filter", "f", "", "Filter the list of contexts to those containing this text")
+	cmd.Flags().BoolVarP(&options.DeleteAuthInfo, "delete-user", "", false, "Also delete the user config associated to the context")
+	cmd.Flags().BoolVarP(&options.DeleteCluster, "delete-cluster", "", false, "Also delete the cluster config associated to the context")
+	cmd.Flags().BoolVarP(&options.Verbose, "verbose", "", false, "Enable verbose logging")
 	return cmd
 }
 
@@ -123,6 +128,18 @@ func (o *DeleteContextOptions) Run() error {
 
 	newConfig := *config
 	for _, name := range selected {
+		a := newConfig.Contexts[name].AuthInfo
+		if o.DeleteAuthInfo && a != "" {
+			o.Debugf("Deleting user %s for context %s\n", util.ColorInfo(a), util.ColorInfo(name))
+			delete(newConfig.AuthInfos, a)
+		}
+		c := newConfig.Contexts[name].Cluster
+		if o.DeleteCluster && c != "" {
+			o.Debugf("Deleting cluster %s for context %s\n", util.ColorInfo(c), util.ColorInfo(name))
+			delete(newConfig.Clusters, c)
+		}
+
+		o.Debugf("Deleting context %s\n", util.ColorInfo(name))
 		delete(newConfig.Contexts, name)
 	}
 	err = clientcmd.ModifyConfig(po, newConfig, false)
