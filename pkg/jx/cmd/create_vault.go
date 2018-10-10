00f6fb6f235c6bf0ca3b6d97dc3677a7b07200e3
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-10-10 14:39:12 +0200

    feat:(vault) add skeleton command for create vault command

---
+++ pkg/jx/cmd/create_vault.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"io"
+
+	"github.com/spf13/cobra"
+	"gopkg.in/AlecAivazis/survey.v1/terminal"
+
+	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
+)
+
+var (
+	createVaultLong = templates.LongDesc(`
+		Creates a Vault using the vault-operator
+`)
+
+	createVaultExample = templates.Examples(`
+		# Create a new vault 
+		jx create vault
+"
+	`)
+)
+
+// CreateVaultOptions the options for the create vault command
+type CreateVaultOptions struct {
+	CreateOptions
+}
+
+// NewCmdCreateVault  creates a command object for the "create" command
+func NewCmdCreateVault(f Factory, in terminal.FileReader, out terminal.FileWriter, errOut io.Writer) *cobra.Command {
+	options := &CreateVaultOptions{
+		CreateOptions: CreateOptions{
+			CommonOptions: CommonOptions{
+				Factory: f,
+				In:      in,
+				Out:     out,
+				Err:     errOut,
+			},
+		},
+	}
+
+	cmd := &cobra.Command{
+		Use:     "vault",
+		Short:   "Create a new Vault using the vault-opeator",
+		Long:    createVaultLong,
+		Example: createVaultExample,
+		Run: func(cmd *cobra.Command, args []string) {
+			options.Cmd = cmd
+			options.Args = args
+			err := options.Run()
+			CheckErr(err)
+		},
+	}
+
+	options.addCommonFlags(cmd)
+	return cmd
+}
+
+// Run implements the command
+func (o *CreateVaultOptions) Run() error {
+	return nil
+}
