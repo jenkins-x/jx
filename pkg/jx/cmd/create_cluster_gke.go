8f27ebfe1778021be3f498171db860cc77124c3b
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-10-11 10:56:45 +0200

    refactor:(gke) move google project survey in common options

--- pkg/jx/cmd/create_cluster_gke.go
+++ pkg/jx/cmd/create_cluster_gke.go
@@ -4,10 +4,6 @@ import (
 	"io"
 	"strings"
 
-	"fmt"
-
-	"errors"
-
 	os_user "os/user"
 
 	"regexp"
@@ -324,63 +320,4 @@ func (o *CreateClusterGKEOptions) createClusterGKE() error {
 func sanitizeLabel(username string) string {
 	sanitized := strings.ToLower(username)
 	return disallowedLabelCharacters.ReplaceAllString(sanitized, "-")
-}
-
-// asks to chose from existing projects or optionally creates one if none exist
-func (o *CreateClusterGKEOptions) getGoogleProjectId() (string, error) {
-	surveyOpts := survey.WithStdio(o.In, o.Out, o.Err)
-	out, err := o.getCommandOutput("", "gcloud", "projects", "list")
-	if err != nil {
-		return "", err
-	}
-
-	lines := strings.Split(string(out), "\n")
-	var existingProjects []string
-	for _, l := range lines {
-		if strings.Contains(l, CLUSTER_LIST_HEADER) {
-			continue
-		}
-		fields := strings.Fields(l)
-		existingProjects = append(existingProjects, fields[0])
-	}
-
-	var projectId string
-	if len(existingProjects) == 0 {
-		confirm := &survey.Confirm{
-			Message: fmt.Sprintf("No existing Google Projects exist, create one now?"),
-			Default: true,
-		}
-		flag := true
-		err = survey.AskOne(confirm, &flag, nil, surveyOpts)
-		if err != nil {
-			return "", err
-		}
-		if !flag {
-			return "", errors.New("no google project to create cluster in, please manual create one and rerun this wizard")
-		}
-
-		if flag {
-			return "", errors.New("auto creating projects not yet implemented, please manually create one and rerun the wizard")
-		}
-	} else if len(existingProjects) == 1 {
-		projectId = existingProjects[0]
-		log.Infof("Using the only Google Cloud Project %s to create the cluster\n", util.ColorInfo(projectId))
-	} else {
-		prompts := &survey.Select{
-			Message: "Google Cloud Project:",
-			Options: existingProjects,
-			Help:    "Select a Google Project to create the cluster in",
-		}
-
-		err := survey.AskOne(prompts, &projectId, nil, surveyOpts)
-		if err != nil {
-			return "", err
-		}
-	}
-
-	if projectId == "" {
-		return "", errors.New("no Google Cloud Project to create cluster in, please manual create one and rerun this wizard")
-	}
-
-	return projectId, nil
 }
