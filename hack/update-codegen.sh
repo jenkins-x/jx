599dde842f32e20b9053730b8c4fa974e8cdb2df
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-10-18 09:05:39 +0100

    chore: update the codegen script to use go modules when running the code-genrator

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,9 +18,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+# update the code-genrator submodule
+echo "Installing the code-generator"
+git submodule update --remote --merge
 
+export GO111MODULE=on 
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./hack/code-generator 2>/dev/null || echo ../code-generator)}
 OUTDIR=$(dirname ${BASH_SOURCE})/../../../..
 
 echo "Generating code to ${OUTDIR}"
