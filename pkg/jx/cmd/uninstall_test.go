e8b3ccab3133e688454f76dbee4f7035ff6306e2
Steve Arch <sarch@cloudbees.com>
2018-10-25 15:44:37 +0100

    package name should be cmd_test

--- pkg/jx/cmd/uninstall_test.go
+++ pkg/jx/cmd/uninstall_test.go
@@ -1,4 +1,4 @@
-package cmd
+package cmd_test
 
 import (
 	"github.com/jenkins-x/jx/pkg/gits/mocks"
@@ -7,13 +7,9 @@ import (
 	"k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/clientcmd/api"
 
-	//"github.com/jenkins-x/jx/pkg/tests"
-
 	cmd_mocks "github.com/jenkins-x/jx/pkg/jx/cmd/mocks"
 	kuber_mocks "github.com/jenkins-x/jx/pkg/kube/mocks"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	//testclient "k8s.io/client-go/kubernetes/fake"
 
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -21,7 +17,7 @@ import (
 	. "github.com/petergtz/pegomock"
 )
 
-func setup(contextName string) (*cmd_mocks.MockFactory, *kuber_mocks.MockKuber) {
+func setupUninstall(contextName string) (*cmd_mocks.MockFactory, *kuber_mocks.MockKuber) {
 	factory := cmd_mocks.NewMockFactory()
 	kubeMock := kuber_mocks.NewMockKuber()
 	fakeKubeConfig := api.NewConfig()
@@ -31,7 +27,7 @@ func setup(contextName string) (*cmd_mocks.MockFactory, *kuber_mocks.MockKuber) 
 }
 
 func TestUninstallOptions_Run_ContextSpecifiedAsOption_FailsWhenContextNamesDoNotMatch(t *testing.T) {
-	factory, kubeMock := setup("current-context")
+	factory, kubeMock := setupUninstall("current-context")
 
 	o := &cmd.UninstallOptions{
 		CommonOptions: cmd.CommonOptions{
@@ -47,7 +43,7 @@ func TestUninstallOptions_Run_ContextSpecifiedAsOption_FailsWhenContextNamesDoNo
 }
 
 func TestUninstallOptions_Run_ContextSpecifiedAsOption_PassWhenContextNamesMatch(t *testing.T) {
-	factory, kubeMock := setup("correct-context-to-delete")
+	factory, kubeMock := setupUninstall("correct-context-to-delete")
 
 	o := &cmd.UninstallOptions{
 		CommonOptions: cmd.CommonOptions{
@@ -72,7 +68,7 @@ func TestUninstallOptions_Run_ContextSpecifiedAsOption_PassWhenContextNamesMatch
 }
 
 func TestUninstallOptions_Run_ContextSpecifiedAsOption_PassWhenForced(t *testing.T) {
-	factory, kubeMock := setup("correct-context-to-delete")
+	factory, kubeMock := setupUninstall("correct-context-to-delete")
 
 	o := &cmd.UninstallOptions{
 		CommonOptions: cmd.CommonOptions{
@@ -96,7 +92,7 @@ func TestUninstallOptions_Run_ContextSpecifiedAsOption_PassWhenForced(t *testing
 	assert.Error(t, err)
 }
 
-func createNamespace(o *UninstallOptions, ns string) error {
+func createNamespace(o *cmd.UninstallOptions, ns string) error {
 	_, err := o.KubeClientCached.CoreV1().Namespaces().Create(&v1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: ns,
