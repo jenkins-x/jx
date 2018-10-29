93d9ab08aaed13262771eaa3dcc2ee90b796ff82
Steve Arch <sarch@cloudbees.com>
2018-10-29 17:14:46 +0000

    fix(tests): Do not use dirty aws profile from previous test
    Fixes #2077

--- pkg/cloud/amazon/ecr_test.go
+++ pkg/cloud/amazon/ecr_test.go
@@ -11,18 +11,20 @@ import (
 func TestCreateNewSessionWithDefaultRegion(t *testing.T) {
 	// TODO Refactor for encapsulation
 	oldHome, err := switchHome()
+	defer restoreHome(oldHome)
 	os.Setenv("AWS_REGION", "")
 	os.Setenv("AWS_DEFAULT_REGION", "")
+	os.Setenv("AWS_PROFILE", "")
 	sess, err := amazon.NewAwsSessionWithoutOptions()
 	assert.Nil(t, err)
 	assert.Equal(t, "us-west-2", *sess.Config.Region)
-	restoreHome(oldHome)
 }
 
 func TestCreateNewSessionWithRegionFromAwsRegion(t *testing.T) {
 	// TODO Refactor for encapsulation
 	os.Setenv("AWS_REGION", "us-east-1")
 	os.Setenv("AWS_DEFAULT_REGION", "")
+	os.Setenv("AWS_PROFILE", "")
 	sess, err := amazon.NewAwsSessionWithoutOptions()
 	assert.Nil(t, err)
 	assert.Equal(t, "us-east-1", *sess.Config.Region)
