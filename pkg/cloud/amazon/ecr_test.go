e5897b10d039239f92cbab045a475d44ad904302
Steve Arch <sarch@cloudbees.com>
2018-10-29 16:14:53 +0000

    fix(tests): ECR tests picked up user's default region from ~/.aws
    Fixes #2077

--- pkg/cloud/amazon/ecr_test.go
+++ pkg/cloud/amazon/ecr_test.go
@@ -1,7 +1,6 @@
 package amazon_test
 
 import (
-	"github.com/jenkins-x/jx/pkg/tests"
 	"os"
 	"testing"
 
@@ -10,17 +9,17 @@ import (
 )
 
 func TestCreateNewSessionWithDefaultRegion(t *testing.T) {
-	tests.SkipForWindows(t, "Pre-existing test. Reason not investigated")
 	// TODO Refactor for encapsulation
+	oldHome, err := switchHome()
 	os.Setenv("AWS_REGION", "")
 	os.Setenv("AWS_DEFAULT_REGION", "")
 	sess, err := amazon.NewAwsSessionWithoutOptions()
 	assert.Nil(t, err)
 	assert.Equal(t, "us-west-2", *sess.Config.Region)
+	restoreHome(oldHome)
 }
 
 func TestCreateNewSessionWithRegionFromAwsRegion(t *testing.T) {
-	tests.SkipForWindows(t, "Pre-existing test. Reason not investigated")
 	// TODO Refactor for encapsulation
 	os.Setenv("AWS_REGION", "us-east-1")
 	os.Setenv("AWS_DEFAULT_REGION", "")
