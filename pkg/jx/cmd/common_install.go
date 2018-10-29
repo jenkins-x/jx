8681fc6c11b9da0b67767621d0231e44912028ca
Steve Arch <sarch@cloudbees.com>
2018-10-29 17:19:23 +0000

    restored initial behaviour committed in error.

--- pkg/jx/cmd/common_install.go
+++ pkg/jx/cmd/common_install.go
@@ -1364,7 +1364,7 @@ func (o *CommonOptions) installMissingDependencies(providerSpecificDeps []string
 		survey.AskOne(prompt, &install, nil, surveyOpts)
 	}
 
-	return o.doInstallMissingDependencies(deps)
+	return o.doInstallMissingDependencies(install)
 }
 
 // installRequirements installs any requirements for the given provider kind
