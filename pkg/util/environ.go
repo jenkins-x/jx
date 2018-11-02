6c8c5f222a600be61edb6eea06adf2d84de1df8b
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-11-02 13:47:43 +0100

    refactor(auth config): refactor how the user auth is created from environment and add tests

--- pkg/util/environ.go
+++ pkg/util/environ.go
@@ -2,6 +2,7 @@ package util
 
 import "os"
 
+// GetAndCleanEnviron cleans the provided env variables and returns their current value
 func GetAndCleanEnviron(keys []string) (map[string]string, error) {
 	environ := map[string]string{}
 	for _, key := range keys {
@@ -17,6 +18,7 @@ func GetAndCleanEnviron(keys []string) (map[string]string, error) {
 	return environ, nil
 }
 
+// RestoreEnviron sets the in the environment the environment variables provided as input
 func RestoreEnviron(environ map[string]string) error {
 	for key, value := range environ {
 		err := os.Setenv(key, value)
