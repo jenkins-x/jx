b1ed578235ee8c0281a2f36597351d40981a2587
Mark Wynn-Mackenzie <mwynnmackenzie@cloudbees.com>
2018-11-06 17:35:03 +0000

    Refactor for slightly more readable test.

--- pkg/jx/cmd/preview_test.go
+++ pkg/jx/cmd/preview_test.go
@@ -2,6 +2,7 @@ package cmd_test
 
 import (
 	"reflect"
+	"strconv"
 
 	"github.com/jenkins-x/jx/pkg/apis/jenkins.io/v1"
 
@@ -40,30 +41,33 @@ const (
 	prNum          = 1
 	prAuthor       = "the-pr-author"
 	prOwner        = "the-pr-owner"
+	prEmail        = "the-pr-owner@organisation.com"
+	userK8sId      = "the-pr-owner.organisation.com"
 )
 
-//Pegomock 'any' matcher for *string.
-//(since these don't seem to get generated).
-func anyPtrToString() *string {
-	RegisterMatcher(NewAnyMatcher(reflect.TypeOf((*(*string))(nil)).Elem()))
-	var nullValue *string
-	return nullValue
-}
-
-//Pegomock 'any' matcher for *int.
-//(since these don't seem to get generated).
-func anyPtrToInt() *int {
-	RegisterMatcher(NewAnyMatcher(reflect.TypeOf((*(*int))(nil)).Elem()))
-	var nullValue *int
-	return nullValue
-}
-
 func TestRun_CreateNewPreviewEnv(t *testing.T) {
-
-	//TODO: t.Parallel()
+	//todo: validate sourceurl/sourceref in environmentspec.source
+	t.Parallel()
 
 	RegisterMockTestingT(t)
 
+	setupEnvironment()
+	previewOpts, cs := setupMocks()
+
+	err := previewOpts.Run()
+
+	assert.NoError(t, err, "Should not error")
+
+	validatePreviewEnvironment(t, cs)
+	validateUser(t, cs)
+
+	//TODO: assert CRD registrations?
+
+	//TODO: check PR comment.
+
+}
+
+func setupEnvironment() {
 	//Environment variables expected to be found by jx preview when run in cluster:
 	os.Setenv("GITHUB_USERNAME", gitHubUsername)
 	os.Setenv("GITHUB_BEARER_TOKEN", "abc123def")
@@ -72,12 +76,12 @@ func TestRun_CreateNewPreviewEnv(t *testing.T) {
 	os.Setenv("JOB_NAME", "job")
 	os.Setenv("BUILD_NUMBER", "1")
 	os.Setenv(cmd.ORG, "MyOrganisation")
-	//TODO: remove?
 	os.Setenv(cmd.APP_NAME, "MyApp")
 	os.Setenv(cmd.PREVIEW_VERSION, "v0.1.2")
+}
 
+func setupMocks() (*cmd.PreviewOptions, *cs_fake.Clientset) {
 	factory := cmd_mocks.NewMockFactory()
-
 	previewOpts := &cmd.PreviewOptions{
 		PromoteOptions: cmd.PromoteOptions{
 			CommonOptions: cmd.CommonOptions{
@@ -92,7 +96,7 @@ func TestRun_CreateNewPreviewEnv(t *testing.T) {
 		DevNamespace: "jx",
 		Name:         name,
 		SourceURL:    gitHubLink + ".git",
-		PullRequest:  string(prNum),
+		PullRequest:  strconv.Itoa(prNum),
 	}
 
 	nsObj := &k8s_v1.Namespace{
@@ -138,10 +142,9 @@ func TestRun_CreateNewPreviewEnv(t *testing.T) {
 		cmd_matchers.AnyIoWriter(),
 	)).ThenReturn(mockGitProvider, nil)
 	number := prNum
-	//todo: fill in e-mail & validate UserDetailService updates (when set).
 	mockGitPR := &gits.GitPullRequest{
 		Owner:  prOwner,
-		Author: &gits.GitUser{Name: prAuthor},
+		Author: &gits.GitUser{Name: prAuthor, Email: prEmail},
 		Number: &number,
 	}
 	When(mockGitProvider.GetPullRequest(AnyString(), //owner
@@ -153,14 +156,8 @@ func TestRun_CreateNewPreviewEnv(t *testing.T) {
 	When(factory.CreateAuthConfigService(cmd.GitAuthConfigFile)).ThenReturn(mockAuthConfigService, nil)
 	When(factory.IsInCDPIpeline()).ThenReturn(true)
 
-	//env := &jio_v1.Environment{
-	//	ObjectMeta: metav1.ObjectMeta{
-	//		Name: "my-app-name",
-	//	},
-	//}
-
-	//cs := cs_fake.NewSimpleClientset(env)
 	cs := cs_fake.NewSimpleClientset()
+
 	When(factory.CreateJXClient()).ThenReturn(cs, namespace, nil)
 
 	mockHelmer := helm_test.NewMockHelmer()
@@ -176,31 +173,61 @@ func TestRun_CreateNewPreviewEnv(t *testing.T) {
 		AnyStringSlice(),
 		AnyStringSlice())).ThenReturn(nil) //err=nil
 
-	err := previewOpts.Run()
+	return previewOpts, cs
+}
 
-	assert.NoError(t, err, "Should not error")
-
+func validatePreviewEnvironment(t *testing.T, cs *cs_fake.Clientset) {
 	envs := cs.JenkinsV1().Environments(namespace)
+
+	//Validate preview environment:
 	previewEnv, err := envs.Get(name, metav1.GetOptions{})
 	assert.NoError(t, err, "Preview environment should have been created.")
+	assert.NotNil(t, previewEnv)
 	assert.Equal(t, namespace, previewEnv.Namespace)
 	assert.Equal(t, name, previewEnv.Name)
+	//Validate preview environment spec:
 	assert.NotNil(t, previewEnv.Spec)
 	assert.Equal(t, v1.EnvironmentKindTypePreview, previewEnv.Spec.Kind)
 	assert.Equal(t, v1.PromotionStrategyTypeAutomatic, previewEnv.Spec.PromotionStrategy)
-	prURL := gitHubLink + "/pull/" + string(prNum)
+	prURL := gitHubLink + "/pull/" + strconv.Itoa(prNum)
 	assert.Equal(t, prURL, previewEnv.Spec.PullRequestURL)
-	//todo: validate previewgitspec
+	//Validate preview environment git spec:
 	assert.NotNil(t, previewEnv.Spec.PreviewGitSpec)
-	assert.Equal(t, prNum, previewEnv.Spec.PreviewGitSpec.Name)
+	assert.Equal(t, strconv.Itoa(prNum), previewEnv.Spec.PreviewGitSpec.Name)
 	assert.Equal(t, prURL, previewEnv.Spec.PreviewGitSpec.URL)
+	//Validate preview environment git spec owner:
+	assert.NotNil(t, previewEnv.Spec.PreviewGitSpec.User)
+	assert.Equal(t, prAuthor, previewEnv.Spec.PreviewGitSpec.User.Name)
 	//todo: set build status.
+	//Validate build status
 	assert.Equal(t, "", previewEnv.Spec.PreviewGitSpec.BuildStatus)
 	assert.Equal(t, "", previewEnv.Spec.PreviewGitSpec.BuildStatusURL)
-	assert.Equal(t, gitHubUsername, previewEnv.Spec.PreviewGitSpec.User)
+	assert.Equal(t, application, previewEnv.Spec.PreviewGitSpec.ApplicationName)
+}
 
-	//TODO: assert CRD registrations?
+func validateUser(t *testing.T, cs *cs_fake.Clientset) {
+	//Validate UserDetailsService updates:
+	users := cs.JenkinsV1().Users(namespace)
+	user, err := users.Get(userK8sId, metav1.GetOptions{})
+	assert.NoError(t, err, "User should have been created.")
+	assert.NotNil(t, user)
+	//TODO: should this be used Spec instead of User (deprecated).
+	assert.Equal(t, prEmail, user.User.Email)
+	assert.Equal(t, prAuthor, user.User.Name)
+}
 
-	//TODO: check PR comment.
+//Pegomock 'any' matcher for *string.
+//(since these don't seem to get generated).
+func anyPtrToString() *string {
+	RegisterMatcher(NewAnyMatcher(reflect.TypeOf((*(*string))(nil)).Elem()))
+	var nullValue *string
+	return nullValue
+}
 
+//Pegomock 'any' matcher for *int.
+//(since these don't seem to get generated).
+func anyPtrToInt() *int {
+	RegisterMatcher(NewAnyMatcher(reflect.TypeOf((*(*int))(nil)).Elem()))
+	var nullValue *int
+	return nullValue
 }
