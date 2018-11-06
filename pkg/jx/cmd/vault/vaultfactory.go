515263cb67785fd37d43a3b3c4affecfb7bfc404
Steve Arch <sarch@cloudbees.com>
2018-11-06 17:02:37 +0000

    Used helper function for getting token from service account

--- pkg/jx/cmd/vault/vaultfactory.go
+++ pkg/jx/cmd/vault/vaultfactory.go
@@ -57,22 +57,13 @@ func (v *VaultClientFactory) GetConfigData(name string, namespace string) (confi
 	}
 
 	serviceAccount, err := v.getServiceAccountFromVault(vlt)
-	secret, err := v.getSecretFromServiceAccount(serviceAccount, vlt.Namespace)
+	token, err := kube.GetServiceAccountToken(v.kubeClient, namespace, serviceAccount.Name)
 
-	return &api.Config{Address: vlt.URL}, getJWTFromSecret(secret), serviceAccount.Name, nil
+	return &api.Config{Address: vlt.URL}, token, serviceAccount.Name, err
 }
 
 func (v *VaultClientFactory) getServiceAccountFromVault(vault *kube.Vault) (*v1.ServiceAccount, error) {
 	return v.kubeClient.CoreV1().ServiceAccounts(vault.Namespace).Get(vault.AuthServiceAccountName, meta_v1.GetOptions{})
-}
-
-func (v *VaultClientFactory) getSecretFromServiceAccount(sa *v1.ServiceAccount, namespace string) (*v1.Secret, error) {
-	secretName := sa.Secrets[0].Name
-	return v.kubeClient.CoreV1().Secrets(namespace).Get(secretName, meta_v1.GetOptions{})
-}
-
-func getJWTFromSecret(secret *v1.Secret) string {
-	return string(secret.Data["token"])
 }
 
 func getTokenFromVault(role string, jwt string, vaultClient *api.Client) (string, error) {
