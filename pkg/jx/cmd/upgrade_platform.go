b813044d1f96ce80cc85b2063a3402b801a38f5a
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-11-16 10:50:10 +0000

    feat(upgrade) prompt user if kubeProvider is not set

--- pkg/jx/cmd/upgrade_platform.go
+++ pkg/jx/cmd/upgrade_platform.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"github.com/jenkins-x/jx/pkg/apis/jenkins.io/v1"
 	"github.com/jenkins-x/jx/pkg/kube"
+	"gopkg.in/AlecAivazis/survey.v1"
 	"io"
 	"io/ioutil"
 	"strings"
@@ -108,6 +110,38 @@ func (o *UpgradePlatformOptions) Run() error {
 		}
 	}
 
+	settings, err := o.TeamSettings()
+	if err != nil {
+		return err
+	}
+
+	if "" == settings.KubeProvider {
+		log.Warnf("Unable to determine provider from team settings")
+
+		surveyOpts := survey.WithStdio(o.In, o.Out, o.Err)
+
+		provider := ""
+
+		prompt := &survey.Select{
+			Message: "Select the kube provider:",
+			Options: KUBERNETES_PROVIDERS,
+			Default: "",
+		}
+		survey.AskOne(prompt, &provider, nil, surveyOpts)
+
+		err = o.ModifyDevEnvironment(func(env *v1.Environment) error {
+			settings = &env.Spec.TeamSettings
+			settings.KubeProvider = provider
+			return nil
+		})
+		if err != nil {
+			return errors.Wrap(err, "failed to create the API extensions client")
+		}
+	}
+
+	log.Infof("Using provider '%s' from team settings\n", util.ColorInfo(settings.KubeProvider))
+
+
 	wrkDir := ""
 
 	if targetVersion == "" {
@@ -147,12 +181,6 @@ func (o *UpgradePlatformOptions) Run() error {
 		return errors.New("Jenkins X platform helm chart is not installed.")
 	}
 
-	settings, err := o.TeamSettings()
-	if err != nil {
-		return err
-	}
-	log.Infof("Using provider %s from team settings\n", util.ColorInfo(settings.KubeProvider))
-
 	helmConfig := &o.CreateEnvOptions.HelmValuesConfig
 	exposeController := helmConfig.ExposeController
 	if exposeController != nil && exposeController.Config.Domain == "" {
@@ -165,10 +193,6 @@ func (o *UpgradePlatformOptions) Run() error {
 		if err != nil {
 			return errors.Wrap(err, "failed to clone the jx cloud environments repo")
 		}
-	}
-
-	if "" == settings.KubeProvider {
-		return fmt.Errorf("kubeProvider is not set in teamSettings")
 	}
 
 	makefileDir := filepath.Join(wrkDir, fmt.Sprintf("env-%s", strings.ToLower(settings.KubeProvider)))
