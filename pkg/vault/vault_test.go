0bd07584c0dd6d1d726af2794058db7fe6d6d554
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-11-29 16:14:48 +0100

    test(vault): check if the vault role and role binding are created

--- pkg/vault/vault_test.go
+++ pkg/vault/vault_test.go
@@ -53,11 +53,17 @@ func TestCreateVault(t *testing.T) {
 			}
 
 			vault, err := vaultclient.Vault().Vaults(tc.namespace).Get(tc.name, metav1.GetOptions{})
-			assert.NoError(t, err, "should retrive created vault without an error")
+			assert.NoError(t, err, "should retrieve created vault without an error")
 			assert.NotNil(t, vault, "created vault should not be nil")
 			sa, err := client.CoreV1().ServiceAccounts(tc.namespace).Get(tc.name, metav1.GetOptions{})
-			assert.NoError(t, err, "should retrive vault service account without error")
+			assert.NoError(t, err, "should retrieve vault service account without error")
 			assert.NotNil(t, sa, "created vault service account should not be nil")
+			role, err := client.RbacV1().ClusterRoles().Get("vault-auth", metav1.GetOptions{})
+			assert.NoError(t, err, "should retrieve vault cluster role without error")
+			assert.NotNil(t, role, "created vault cluster role should not be nil")
+			rb, err := client.RbacV1().ClusterRoleBindings().Get(tc.name, metav1.GetOptions{})
+			assert.NoError(t, err, "should retrieve vault cluster role binding without error")
+			assert.NotNil(t, rb, "created vault cluster role binding should not be nil")
 		})
 	}
 }
