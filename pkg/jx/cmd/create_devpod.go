1497f2d08c71fc47eb57ecd22fc74e7a4eb0e8ef
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-12-10 15:56:01 +0000

    fix: removed additional 'Needs more' in jx status output

--- pkg/jx/cmd/create_devpod.go
+++ pkg/jx/cmd/create_devpod.go
@@ -674,7 +674,6 @@ func (o *CreateDevPodOptions) Run() error {
 	if create {
 		//  Let install bash-completion to make life better
 		log.Infof("Attempting to install Bash Completion into DevPod\n")
-		// rshExec = append(rshExec, "yum install -q -y bash-completion bash-completion-extra", "mkdir -p ~/.jx", "jx completion bash > ~/.jx/bash", "echo \"source ~/.jx/bash\" >> ~/.bashrc")
 
     rshExec = append(rshExec,
       "if which yum 1> /dev/null; then yum install -q -y bash-completion bash-completion-extra; fi",
