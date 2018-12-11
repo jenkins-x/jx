8406df5b4da82718371855f7a484f5cc11cb7952
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-12-11 10:42:09 +0000

    feat(gke) can now provide oauth scopes when creating gke cluster

--- pkg/jx/cmd/create_cluster_gke.go
+++ pkg/jx/cmd/create_cluster_gke.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"io"
 	"strings"
 
@@ -45,6 +46,7 @@ type CreateClusterGKEFlags struct {
 	Zone            string
 	Namespace       string
 	Labels          string
+	Scopes          []string
 }
 
 const clusterListHeader = "PROJECT_ID"
@@ -109,6 +111,7 @@ func NewCmdCreateClusterGKE(f Factory, in terminal.FileReader, out terminal.File
 	cmd.Flags().StringVarP(&options.Flags.Zone, "zone", "z", "", "The compute zone (e.g. us-central1-a) for the cluster")
 	cmd.Flags().BoolVarP(&options.Flags.SkipLogin, "skip-login", "", false, "Skip Google auth if already logged in via gcloud auth")
 	cmd.Flags().StringVarP(&options.Flags.Labels, "labels", "", "", "The labels to add to the cluster being created such as 'foo=bar,whatnot=123'. Label names must begin with a lowercase character ([a-z]), end with a lowercase alphanumeric ([a-z0-9]) with dashes (-), and lowercase alphanumeric ([a-z0-9]) between.")
+	cmd.Flags().StringArrayVarP(&options.Flags.Scopes, "scope", "", []string{}, "The OAuth scopes to be added to the cluster")
 
 	cmd.AddCommand(NewCmdCreateClusterGKETerraform(f, in, out, errOut))
 
@@ -248,6 +251,10 @@ func (o *CreateClusterGKEOptions) createClusterGKE() error {
 
 	if o.Flags.SubNetwork != "" {
 		args = append(args, "--subnetwork", o.Flags.SubNetwork)
+	}
+
+	if len(o.Flags.Scopes) > 0 {
+		args = append(args, fmt.Sprintf("--scopes=%s", strings.Join(o.Flags.Scopes, ",")))
 	}
 
 	labels := o.Flags.Labels
