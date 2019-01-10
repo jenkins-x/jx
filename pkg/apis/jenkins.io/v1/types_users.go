9d9cc4b82305ee63f79174cb6e70be8c58a19ce1
Pete Muir <pmuir@bleepbleep.org.uk>
2019-01-10 13:25:17 +0000

    fix: custom marshaler to keep Spec and User in sync

--- pkg/apis/jenkins.io/v1/types_users.go
+++ pkg/apis/jenkins.io/v1/types_users.go
@@ -1,6 +1,8 @@
 package v1
 
 import (
+	"encoding/json"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -52,4 +54,71 @@ func (u *User) SubjectKind() string {
 		return "ServiceAccount"
 	}
 	return "User"
+}
+
+// UnmarshalJSON method merges the deprecated User field and the Spec field on User, preferring Spec
+func (u *User) UnmarshalJSON(bs []byte) error {
+	var intermediate map[string]json.RawMessage
+	err := json.Unmarshal(bs, &intermediate)
+	if err != nil {
+		return err
+	}
+	kind, ok := intermediate["kind"]
+	if ok {
+		err = json.Unmarshal(kind, &u.Kind)
+		if err != nil {
+			return err
+		}
+	}
+	apiVersion, ok := intermediate["apiVersion"]
+	if ok {
+		err = json.Unmarshal(apiVersion, &u.APIVersion)
+		if err != nil {
+			return err
+		}
+	}
+	objectMeta, ok := intermediate["metadata"]
+	if ok {
+		err = json.Unmarshal(objectMeta, &u.ObjectMeta)
+		if err != nil {
+			return err
+		}
+	}
+	user, userOk := intermediate["user"]
+	spec, specOk := intermediate["spec"]
+	if specOk && userOk {
+		// If both are there, merge them, with preference given to spec
+		merged := UserDetails{}
+		err = json.Unmarshal(user, &merged)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(spec, &merged)
+		if err != nil {
+			return err
+		}
+		u.Spec = merged
+		u.User = merged
+	} else if !specOk && userOk {
+		// If only user is there, copy it into spec
+		err = json.Unmarshal(user, &u.Spec)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(user, &u.User)
+		if err != nil {
+			return err
+		}
+	} else if specOk {
+		// If only spec is there, copy it into user
+		err = json.Unmarshal(spec, &u.User)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(spec, &u.Spec)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
