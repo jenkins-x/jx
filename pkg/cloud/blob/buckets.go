8cbc2f7fa2d99d4524a9e2ee28a14d05e0acacf6
James Strachan <james.strachan@gmail.com>
2019-01-15 09:17:37 +0000

    chore: first spike at using go-cloud
    to abstract away working with Blobs across cloud providers

---
+++ pkg/cloud/blob/buckets.go
@@ -0,0 +1,81 @@
+package blob
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/Azure/azure-storage-blob-go/azblob"
+	"gocloud.dev/blob"
+	"gocloud.dev/blob/azureblob"
+	"gocloud.dev/blob/gcsblob"
+	"gocloud.dev/blob/s3blob"
+	"gocloud.dev/gcp"
+)
+
+// SetupBucket creates a connection to a particular cloud provider's blob storage.
+func SetupBucket(ctx context.Context, cloud, bucket string) (*blob.Bucket, error) {
+	switch cloud {
+	case "aws":
+		return setupAWS(ctx, bucket)
+	case "gcp":
+		return setupGCP(ctx, bucket)
+	case "azure":
+		return setupAzure(ctx, bucket)
+	default:
+		return nil, fmt.Errorf("invalid cloud provider: %s", cloud)
+	}
+}
+
+// setupGCP creates a connection to Google Cloud Storage (GCS).
+func setupGCP(ctx context.Context, bucket string) (*blob.Bucket, error) {
+	// DefaultCredentials assumes a user has logged in with gcloud.
+	// See here for more information:
+	// https://cloud.google.com/docs/authentication/getting-started
+	creds, err := gcp.DefaultCredentials(ctx)
+	if err != nil {
+		return nil, err
+	}
+	c, err := gcp.NewHTTPClient(gcp.DefaultTransport(), gcp.CredentialsTokenSource(creds))
+	if err != nil {
+		return nil, err
+	}
+	return gcsblob.OpenBucket(ctx, c, bucket, nil)
+}
+
+// setupAWS creates a connection to Simple Cloud Storage Service (S3).
+func setupAWS(ctx context.Context, bucket string) (*blob.Bucket, error) {
+	c := &aws.Config{
+		// Either hard-code the region or use AWS_REGION.
+		Region: aws.String("us-east-2"),
+		// credentials.NewEnvCredentials assumes two environment variables are
+		// present:
+		// 1. AWS_ACCESS_KEY_ID, and
+		// 2. AWS_SECRET_ACCESS_KEY.
+		Credentials: credentials.NewEnvCredentials(),
+	}
+	s := session.Must(session.NewSession(c))
+	return s3blob.OpenBucket(ctx, s, bucket, nil)
+}
+
+// setupAzure creates a connection to Azure Storage Account using shared key
+// authorization. It assumes environment variables AZURE_STORAGE_ACCOUNT_NAME
+// and AZURE_STORAGE_ACCOUNT_KEY are present.
+func setupAzure(ctx context.Context, bucket string) (*blob.Bucket, error) {
+	accountName, err := azureblob.DefaultAccountName()
+	if err != nil {
+		return nil, err
+	}
+	accountKey, err := azureblob.DefaultAccountKey()
+	if err != nil {
+		return nil, err
+	}
+	credential, err := azureblob.NewCredential(accountName, accountKey)
+	if err != nil {
+		return nil, err
+	}
+	p := azureblob.NewPipeline(credential, azblob.PipelineOptions{})
+	return azureblob.OpenBucket(ctx, p, accountName, bucket, nil)
+}
