0da8ef8fc3c25151d311c3d71e607d6b20a960fc
Mark Wynn-Mackenzie <mwynnmackenzie@cloudbees.com>
2019-01-23 14:36:02 +0000

    chore: use full name for User.CreateServiceAccount property.

--- pkg/apis/jenkins.io/v1/types_users.go
+++ pkg/apis/jenkins.io/v1/types_users.go
@@ -37,15 +37,15 @@ type UserList struct {
 
 // UserDetails containers details of a user
 type UserDetails struct {
-	Login             string             `json:"login,omitempty"  protobuf:"bytes,1,opt,name=login"`
-	Name              string             `json:"name,omitempty"  protobuf:"bytes,2,opt,name=name"`
-	Email             string             `json:"email,omitempty"  protobuf:"bytes,3,opt,name=email"`
-	CreationTimestamp *metav1.Time       `json:"creationTimestamp,omitempty" protobuf:"bytes,4,opt,name=creationTimestamp"`
-	URL               string             `json:"url,omitempty"  protobuf:"bytes,5,opt,name=url"`
-	AvatarURL         string             `json:"avatarUrl,omitempty"  protobuf:"bytes,6,opt,name=avatarUrl"`
-	ServiceAccount    string             `json:"serviceAccount,omitempty"  protobuf:"bytes,7,opt,name=serviceAccount"`
-	Accounts          []AccountReference `json:"accountReference,omitempty"  protobuf:"bytes,8,opt,name=accountReference"`
-	CreateServiceAc   bool               `json:"createServiceAc,omitempty"  protobuf:"bytes,9,opt,name=createServiceAc"`
+	Login                string             `json:"login,omitempty"  protobuf:"bytes,1,opt,name=login"`
+	Name                 string             `json:"name,omitempty"  protobuf:"bytes,2,opt,name=name"`
+	Email                string             `json:"email,omitempty"  protobuf:"bytes,3,opt,name=email"`
+	CreationTimestamp    *metav1.Time       `json:"creationTimestamp,omitempty" protobuf:"bytes,4,opt,name=creationTimestamp"`
+	URL                  string             `json:"url,omitempty"  protobuf:"bytes,5,opt,name=url"`
+	AvatarURL            string             `json:"avatarUrl,omitempty"  protobuf:"bytes,6,opt,name=avatarUrl"`
+	ServiceAccount       string             `json:"serviceAccount,omitempty"  protobuf:"bytes,7,opt,name=serviceAccount"`
+	Accounts             []AccountReference `json:"accountReference,omitempty"  protobuf:"bytes,8,opt,name=accountReference"`
+	CreateServiceAccount bool               `json:"createServiceAccount,omitempty"  protobuf:"bytes,9,opt,name=createServiceAccount"`
 }
 
 // AccountReference is a reference to a user account in another system that is attached to this user
