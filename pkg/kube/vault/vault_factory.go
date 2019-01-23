12ca83e8e94d948ee24fa3f86aa490e4ff3b8e8b
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2019-01-23 13:45:17 +0100

    fix(install): wait for vault to be initialized and unsealed before retrieving the auth token

--- pkg/kube/vault/vault_factory.go
+++ pkg/kube/vault/vault_factory.go
@@ -1,6 +1,9 @@
 package vault
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/banzaicloud/bank-vaults/operator/pkg/client/clientset/versioned"
 	"github.com/hashicorp/vault/api"
 	"github.com/jenkins-x/jx/pkg/jx/cmd/common"
@@ -11,7 +14,19 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
-// VaultClientFactory keeps the configuration required to build a new VaultClientFactory
+const (
+
+	// maxRetries controls the maximum number of time retry when 5xx error occurs. Default to 2 (for a total
+	// of three retires)
+	maxRetries = 2
+
+	// maxHealthRetries maximum number of time to retry when checking the health of Vault
+	maxHealthRetries = 24
+
+	// healthRetyTimeout timeout for health which controls the duration of the health retry
+	healthRetyTimeout = 2 * time.Minute
+)
+
 type VaultClientFactory struct {
 	Options          common.OptionsInterface
 	Selector         Selector
@@ -59,9 +74,16 @@ func (v *VaultClientFactory) NewVaultClient(name string, namespace string) (*api
 		return nil, err
 	}
 	vaultClient, err := api.NewClient(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "crating vault client")
+	}
+	err = waitForVault(vaultClient)
+	if err != nil {
+		return nil, errors.Wrap(err, "wait for vault to be initialized and unsealed")
+	}
 	token, err := getTokenFromVault(role, jwt, vaultClient)
 	if err != nil {
-		return nil, errors.Wrapf(err, "getting Vault signin token")
+		return nil, errors.Wrapf(err, "getting Vault authentication token")
 	}
 	vaultClient.SetToken(token)
 	return vaultClient, nil
@@ -80,12 +102,36 @@ func (v *VaultClientFactory) GetConfigData(name string, namespace string) (confi
 
 	serviceAccount, err := v.getServiceAccountFromVault(vlt)
 	token, err := serviceaccount.GetServiceAccountToken(v.kubeClient, namespace, serviceAccount.Name)
+	cfg := &api.Config{
+		Address:    vlt.URL,
+		MaxRetries: maxRetries,
+	}
 
-	return &api.Config{Address: vlt.URL}, token, serviceAccount.Name, err
+	return cfg, token, serviceAccount.Name, err
 }
 
 func (v *VaultClientFactory) getServiceAccountFromVault(vault *Vault) (*v1.ServiceAccount, error) {
 	return v.kubeClient.CoreV1().ServiceAccounts(vault.Namespace).Get(vault.AuthServiceAccountName, meta_v1.GetOptions{})
+}
+
+func waitForVault(vaultClient *api.Client) error {
+	sleepTime := healthRetyTimeout / maxHealthRetries
+	var err error
+	var hr *api.HealthResponse
+	for i := 0; i < maxHealthRetries; i++ {
+		hr, err = vaultClient.Sys().Health()
+		if err == nil && hr != nil && hr.Initialized && !hr.Sealed {
+			return nil
+		}
+		time.Sleep(sleepTime)
+	}
+	if err != nil {
+		return errors.Wrap(err, "reading vault health")
+	}
+	if hr != nil {
+		return fmt.Errorf("vault health: initialized=%t, sealed=%t", hr.Initialized, hr.Sealed)
+	}
+	return errors.New("failed to read vault health")
 }
 
 func getTokenFromVault(role string, jwt string, vaultClient *api.Client) (string, error) {
