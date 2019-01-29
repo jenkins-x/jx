eb94f7dc375736c4b903dc5264a8ffb5fd4e57a4
Henryk Konsek <hekonsek@gmail.com>
2019-01-29 12:42:24 +0100

    Added comments, fixed lint errors.

--- pkg/util/color.go
+++ pkg/util/color.go
@@ -6,9 +6,20 @@ import (
 	"github.com/fatih/color"
 )
 
+// ColorInfo returns a new function that returns info-colorized (green) strings for the
+// given arguments with fmt.Sprint().
 var ColorInfo = color.New(color.FgGreen).SprintFunc()
+
+// ColorStatus returns a new function that returns status-colorized (blue) strings for the
+// given arguments with fmt.Sprint().
 var ColorStatus = color.New(color.FgBlue).SprintFunc()
+
+// ColorWarning returns a new function that returns warning-colorized (yellow) strings for the
+// given arguments with fmt.Sprint().
 var ColorWarning = color.New(color.FgYellow).SprintFunc()
+
+// ColorError returns a new function that returns error-colorized (red) strings for the
+// given arguments with fmt.Sprint().
 var ColorError = color.New(color.FgRed).SprintFunc()
 
 var colorMap = map[string]color.Attribute{
