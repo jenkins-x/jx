53c8f6f92deaf871a83f0491b07a575b53353513
Steve Boardwell <steve.boardwell@datameer.com>
2019-02-01 15:49:25 +0100

    Add urltemplate to helm_values and ingress

--- pkg/config/helm_values_test.go
+++ pkg/config/helm_values_test.go
@@ -1,8 +1,9 @@
 package config_test
 
 import (
-	"github.com/ghodss/yaml"
 	"testing"
+
+	"github.com/ghodss/yaml"
 
 	"io/ioutil"
 
@@ -32,5 +33,31 @@ func TestEnvironmentExposecontrollerHelmValues(t *testing.T) {
 	values.ExposeController.Config.Domain = "jenkinsx.io"
 	values.ExposeController.Config.HTTP = "false"
 	values.ExposeController.Config.TLSAcme = "false"
+	assert.Equal(t, helmValuesFromFile, values, "expected exposecontroller helm values do not match")
+}
+
+func TestEnvironmentExposecontrollerHelmValuesWithUrlTemplate(t *testing.T) {
+	t.Parallel()
+
+	testFile, err := ioutil.ReadFile("helm_values_test_with_urltemplate.yaml")
+	assert.NoError(t, err)
+	helmValuesFromFile := config.HelmValuesConfig{}
+	err = yaml.Unmarshal(testFile, &helmValuesFromFile)
+	assert.NoError(t, err)
+
+	a := make(map[string]string)
+	a["helm.sh/hook"] = "post-install,post-upgrade"
+	a["helm.sh/hook-delete-policy"] = "hook-succeeded"
+
+	values := config.HelmValuesConfig{
+		ExposeController: &config.ExposeController{},
+	}
+
+	values.ExposeController.Annotations = a
+	values.ExposeController.Config.Exposer = "Ingress"
+	values.ExposeController.Config.Domain = "jenkinsx.io"
+	values.ExposeController.Config.HTTP = "false"
+	values.ExposeController.Config.TLSAcme = "false"
+	values.ExposeController.Config.UrlTemplate = "{{.Service}}-{{.Namespace}}.{{.Domain}}"
 	assert.Equal(t, helmValuesFromFile, values, "expected exposecontroller helm values do not match")
 }
