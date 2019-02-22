16ed3e5f3a55c8fbdb58f7cd52b2963e3d43ab11
rawlingsj <rawlingsj80@gmail.com>
2019-02-22 21:58:24 +0000

    tmp use my bdd fork

--- jx/scripts/ci.sh
+++ jx/scripts/ci.sh
@@ -21,13 +21,14 @@ export GINKGO_ARGS="-v"
 export JX_DISABLE_DELETE_APP="true"
 export JX_DISABLE_DELETE_REPO="true"
 
-echo ""
+
 git config --global credential.helper store
 git config --global --add user.name JenkinsXBot
 git config --global --add user.email jenkins-x@googlegroups.com
 
 # lets create a team for this PR and run the BDD tests
 gcloud auth activate-service-account --key-file $GKE_SA
+
 gcloud container clusters get-credentials anthorse --zone europe-west1-b --project jenkinsx-dev
 
 sed 's/\$VERSION/${PREVIEW_IMAGE_TAG}/g' myvalues.yaml.template > myvalues.yaml
@@ -42,4 +43,4 @@ git config --global --add user.email jenkins-x@googlegroups.com
 cp ./build/linux/jx /usr/bin
 
 # lets trigger the BDD tests in a new team and git provider
-./build/linux/jx step bdd -b  --provider=gke --git-provider=ghe --git-provider-url=https://github.beescloud.com --git-username dev1 --git-api-token $GHE_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring
+./build/linux/jx step bdd -b  --provider=gke --git-provider=ghe --git-provider-url=https://github.beescloud.com --git-username dev1 --git-api-token $GHE_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring --test-git-repo https://github.com/rawlingsj/bdd-jx.git
