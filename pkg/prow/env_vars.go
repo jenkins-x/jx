9428bde7e31fb833fc04508b14b5f42051879c91
Pete Muir <pmuir@bleepbleep.org.uk>
2019-02-25 07:42:15 -0500

    fix: properly use PULL_REFS

--- pkg/prow/env_vars.go
+++ pkg/prow/env_vars.go
@@ -3,20 +3,31 @@ package prow
 import (
 	"fmt"
 	"strings"
-
-	"github.com/iancoleman/orderedmap"
 )
 
+type PullRefs struct {
+	BaseBranch string
+	BaseSha    string
+	ToMerge    map[string]string
+}
+
 // ParsePullRefs parses the Prow PULL_REFS env var formatted string and converts to a map of branch:sha
-func ParsePullRefs(pullRefs string) (*orderedmap.OrderedMap, error) {
+func ParsePullRefs(pullRefs string) (*PullRefs, error) {
 	kvs := strings.Split(pullRefs, ",")
-	answer := orderedmap.New()
-	for _, kv := range kvs {
+	answer := PullRefs{
+		ToMerge: make(map[string]string),
+	}
+	for i, kv := range kvs {
 		s := strings.Split(kv, ":")
 		if len(s) != 2 {
 			return nil, fmt.Errorf("incorrect format for branch:sha %s", kv)
 		}
-		answer.Set(s[0], s[1])
+		if i == 0 {
+			answer.BaseBranch = s[0]
+			answer.BaseSha = s[1]
+		} else {
+			answer.ToMerge[s[0]] = s[1]
+		}
 	}
-	return answer, nil
+	return &answer, nil
 }
--- pkg/prow/env_vars_test.go
+++ pkg/prow/env_vars_test.go
@@ -3,8 +3,6 @@ package prow_test
 import (
 	"testing"
 
-	"github.com/iancoleman/orderedmap"
-
 	"github.com/jenkins-x/jx/pkg/prow"
 
 	"github.com/stretchr/testify/assert"
@@ -15,9 +13,14 @@ func TestParsePullRefs(t *testing.T) {
 	shas, err := prow.ParsePullRefs(pullRefs)
 	assert.NoError(t, err)
 
-	expected := orderedmap.New()
-	expected.Set("master", "ef08a6cd194c2687d4bc12df6bb8a86f53c348ba")
-	expected.Set("2739", "5b351f4eae3c4afbb90dd7787f8bf2f8c454723f")
-	expected.Set("2822", "bac2a1f34fd54811fb767f69543f59eb3949b2a5")
+	expected := &prow.PullRefs{
+		BaseBranch: "master",
+		BaseSha:    "ef08a6cd194c2687d4bc12df6bb8a86f53c348ba",
+		ToMerge: map[string]string{
+			"2739": "5b351f4eae3c4afbb90dd7787f8bf2f8c454723f",
+			"2822": "bac2a1f34fd54811fb767f69543f59eb3949b2a5",
+		},
+	}
+
 	assert.Equal(t, expected, shas)
 }
