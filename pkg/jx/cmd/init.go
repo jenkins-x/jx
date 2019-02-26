6356abc45560beb447a2a3ac293a44fe78470807
James Strachan <james.strachan@gmail.com>
2019-02-26 06:47:21 +0000

    chore: fixed whitespace

--- pkg/jx/cmd/init.go
+++ pkg/jx/cmd/init.go
@@ -466,7 +466,7 @@ func (o *InitOptions) initIngress() error {
 			return errors.Wrap(err, "failed to append the myvalues file")
 		}
 		if o.Flags.Provider == cloud.AWS || o.Flags.Provider == cloud.EKS {
-			// For EKS enable both ports for NLBs to be able to use TLS on Nginx ingresses 
+			// For EKS enable both ports for NLBs to be able to use TLS on Nginx ingresses
 			// Fix for https://github.com/jenkins-x/jx/issues/3079
 			enableHTTP := "true"
 			enableHTTPS := "true"
