0f6ae24a90611284c7ef7e7b6e67693697fbefce
David Conde <dconde@cloudbees.com>
2019-03-01 15:44:16 +0000

    chore: Add coverage for version changeset step

---
+++ pkg/jx/cmd/step_get_version_changeset_test.go
@@ -0,0 +1,138 @@
+package cmd_test
+
+import (
+	"github.com/acarl005/stripansi"
+	"github.com/jenkins-x/jx/pkg/gits"
+	"github.com/jenkins-x/jx/pkg/helm"
+	"github.com/jenkins-x/jx/pkg/jx/cmd"
+	"github.com/jenkins-x/jx/pkg/kube/resources/mocks"
+	"github.com/jenkins-x/jx/pkg/version"
+	"github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"k8s.io/apimachinery/pkg/runtime"
+	"os"
+	"testing"
+)
+
+func TestStepGetVersionChangeSetOptionsBranch(t *testing.T) {
+	t.Parallel()
+	testDir, _ := ioutil.TempDir("", "test-version-changesetbranch")
+	defer func() {
+		err := os.RemoveAll(testDir)
+		assert.NoError(t, err)
+	}()
+	repoOwner := uuid.NewV4().String()
+	repoName := uuid.NewV4().String()
+	fakeRepo := gits.NewFakeRepository(repoOwner, repoName)
+	fakeGitProvider := gits.NewFakeProvider(fakeRepo)
+	testBranch := "test-app-version-bump"
+	stableBranch := "master"
+	r, fakeStdout, _ := os.Pipe()
+	options := &cmd.StepGetVersionChangeSetOptions{
+		StepOptions: cmd.StepOptions{
+			CommonOptions: &cmd.CommonOptions{},
+		},
+		VersionsDir:   testDir,
+		TestingBranch: testBranch,
+		StableBranch:  stableBranch,
+	}
+	options.CommonOptions.Out = fakeStdout
+	cmd.ConfigureTestOptionsWithResources(options.CommonOptions,
+		[]runtime.Object{},
+		[]runtime.Object{},
+		gits.NewGitLocal(),
+		fakeGitProvider,
+		helm.NewHelmCLI("helm", helm.V2, "", true),
+		resources_test.NewMockInstaller(),
+	)
+	gitter := options.Git()
+	gitter.Init(testDir)
+	stableVersion := &version.StableVersion{
+		Version: "1.0.0",
+		GitURL:  "fake",
+	}
+	gitter.AddRemote(testDir, "origin", fakeRepo.GitRepo.CloneURL)
+	version.SaveStableVersion(testDir, version.KindChart, "test-app", stableVersion)
+	gitter.Add(testDir, ".")
+	gitter.AddCommit(testDir, "Initial Commit")
+	gitter.Push(testDir)
+	gitter.CreateBranch(testDir, testBranch)
+	gitter.Checkout(testDir, testBranch)
+	stableVersion.Version = "1.0.1"
+	version.SaveStableVersion(testDir, version.KindChart, "test-app", stableVersion)
+	gitter.AddCommit(testDir, "Bump version")
+	gitter.Push(testDir)
+	gitter.Checkout(testDir, stableBranch)
+
+	err := options.Run()
+	assert.NoError(t, err)
+	fakeStdout.Close()
+	outBytes, _ := ioutil.ReadAll(r)
+	r.Close()
+	output := stripansi.Strip(string(outBytes))
+	assert.Contains(t, output, `JX_CHANGED_VERSIONS="charts:test-app:1.0.1"`)
+	assert.Contains(t, output, `JX_STABLE_VERSIONS="charts:test-app:1.0.0"`)
+
+}
+
+func TestStepGetVersionChangeSetOptionsPR(t *testing.T) {
+	t.Parallel()
+	testDir, _ := ioutil.TempDir("", "test-version-changesetpr")
+	defer func() {
+		err := os.RemoveAll(testDir)
+		assert.NoError(t, err)
+	}()
+	repoOwner := uuid.NewV4().String()
+	repoName := uuid.NewV4().String()
+	fakeRepo := gits.NewFakeRepository(repoOwner, repoName)
+	fakeGitProvider := gits.NewFakeProvider(fakeRepo)
+	testBranch := "test-app-version-bump"
+	stableBranch := "master"
+	r, fakeStdout, _ := os.Pipe()
+	options := &cmd.StepGetVersionChangeSetOptions{
+		StepOptions: cmd.StepOptions{
+			CommonOptions: &cmd.CommonOptions{},
+		},
+		VersionsDir:  testDir,
+		PR:           "1",
+		StableBranch: stableBranch,
+	}
+	options.CommonOptions.Out = fakeStdout
+	cmd.ConfigureTestOptionsWithResources(options.CommonOptions,
+		[]runtime.Object{},
+		[]runtime.Object{},
+		gits.NewGitLocal(),
+		fakeGitProvider,
+		helm.NewHelmCLI("helm", helm.V2, "", true),
+		resources_test.NewMockInstaller(),
+	)
+	gitter := options.Git()
+	gitter.Init(testDir)
+	stableVersion := &version.StableVersion{
+		Version: "1.0.0",
+		GitURL:  "fake",
+	}
+	gitter.AddRemote(testDir, "origin", fakeRepo.GitRepo.CloneURL)
+	version.SaveStableVersion(testDir, version.KindChart, "test-app", stableVersion)
+	gitter.Add(testDir, ".")
+	gitter.AddCommit(testDir, "Initial Commit")
+	gitter.Push(testDir)
+	gitter.CreateBranch(testDir, testBranch)
+	gitter.Checkout(testDir, testBranch)
+	stableVersion.Version = "1.0.1"
+	version.SaveStableVersion(testDir, version.KindChart, "test-app", stableVersion)
+	gitter.AddCommit(testDir, "Bump version")
+	gitter.CreateBranch(testDir, "1")
+	gitter.Checkout(testDir, stableBranch)
+
+	err := options.Run()
+	assert.NoError(t, err)
+	fakeStdout.Close()
+	outBytes, _ := ioutil.ReadAll(r)
+	r.Close()
+	output := stripansi.Strip(string(outBytes))
+	assert.Contains(t, output, `JX_CHANGED_VERSIONS="charts:test-app:1.0.1"`)
+	assert.Contains(t, output, `JX_STABLE_VERSIONS="charts:test-app:1.0.0"`)
+
+}
