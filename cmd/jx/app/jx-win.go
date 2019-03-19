25bbebe84f09770119fac58c58c68dd15d2eec21
James Nord <jnord+github@cloudbees.com>
2019-03-19 10:23:21 +0000

    Add documentation about the need to set the console mode.
    for more details see #711

--- cmd/jx/app/jx-win.go
+++ cmd/jx/app/jx-win.go
@@ -12,7 +12,6 @@ import (
 
 // Run runs the command
 func Run() error {
-	// if windows
 	configureTerminalForAnsiEscapes()
 	cmd := cmd.NewJXCommand(clients.NewFactory(), os.Stdin, os.Stdout, os.Stderr, nil)
 	return cmd.Execute()
@@ -20,21 +19,25 @@ func Run() error {
 
 const (
 	// https://docs.microsoft.com/en-us/windows/console/setconsolemode
-	enableProcessedOutput = 0x1
-	enableWrapAtEOLOutput = 0x2
-	enableVirtualTermainalProcessing = 0x4
-
+	enableProcessedOutput           = 0x1
+	enableWrapAtEOLOutput           = 0x2
+	enableVirtualTerminalProcessing = 0x4
 )
+
+
+// configureTerminalForAnsiEscapes enables the windows 10 console to translate ansi escape sequences
+// requires windows 10 1511 or higher and fails gracefully on older versions (and prior releases like windows 7)
+// https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
 func configureTerminalForAnsiEscapes() {
 
-	kernel32  := syscall.NewLazyDLL("kernel32.dll")
+	kernel32 := syscall.NewLazyDLL("kernel32.dll")
 	kern32SetConsoleMode := kernel32.NewProc("SetConsoleMode")
 
 	// stderr
 	handle := syscall.Handle(os.Stderr.Fd())
-	kern32SetConsoleMode.Call(uintptr(handle), enableProcessedOutput|enableWrapAtEOLOutput|enableVirtualTermainalProcessing)
+	kern32SetConsoleMode.Call(uintptr(handle), enableProcessedOutput|enableWrapAtEOLOutput|enableVirtualTerminalProcessing)
 
 	// stdout
 	handle = syscall.Handle(os.Stdout.Fd())
-	kern32SetConsoleMode.Call(uintptr(handle), enableProcessedOutput|enableWrapAtEOLOutput|enableVirtualTermainalProcessing)
+	kern32SetConsoleMode.Call(uintptr(handle), enableProcessedOutput|enableWrapAtEOLOutput|enableVirtualTerminalProcessing)
 }
