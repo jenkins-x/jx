41f84e2773b5bfc801ee4aff3b3240892aa664a2
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-03-26 20:26:40 +0000

    chore: disabled extra linters

--- hack/linter.sh
+++ hack/linter.sh
@@ -13,17 +13,18 @@ golangci-lint run \
 	--no-config \
 	-E goimports \
 	-E gocritic \
-	-E gosec \
 	-E interfacer \
 	-E maligned \
 	-E misspell \
 	-E unconvert \
-	-E unparam \
 	-D errcheck \
     -D ineffassign \
   --skip-dirs vendor \
   --deadline 10m0s
 
 # ? deadcode / unused
-# -E goconst \
-# -E golint \
+
+# -E goconst
+# -E golint
+# -E gosec
+# -E unparam
