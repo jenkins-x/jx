448d6f8f7ad5542362a71aa97e36e7e7170e40df
Pete Muir <pmuir@bleepbleep.org.uk>
2019-03-26 11:50:23 +0000

    fix: don’t try to overwrite user id
    If a user id already exists but we can’t match it, then we create a new user with -<n> appended.
    
    Fixes #3420

--- pkg/users/users_test.go
+++ pkg/users/users_test.go
@@ -62,3 +62,54 @@ func TestResolveUserWithEmptyIdDoesNotCreateEmptyAccountReference(t *testing.T) 
 	assert.NotNil(t, resolved)
 	assert.Len(t, resolved.Spec.Accounts, 0)
 }
+
+func TestExistingUserIdButNotFoundBySelectErrors(t *testing.T) {
+	t.Parallel()
+	o := cmd.CommonOptions{}
+
+	//user1 is the user already existing in the cluster
+	user1 := jenkinsv1.User{
+		ObjectMeta: v1.ObjectMeta{
+			Namespace: "jx",
+			Name:      "test-user",
+		},
+		Spec: jenkinsv1.UserDetails{
+			Name:     "Test",
+			Email:    "test@test.com",
+			Accounts: make([]jenkinsv1.AccountReference, 0),
+		},
+	}
+
+	// user2 is the user we will return as new user when we resolve the users
+	user2 := jenkinsv1.User{
+		ObjectMeta: v1.ObjectMeta{
+			Namespace: "jx",
+			Name:      "test-user",
+		},
+		Spec: jenkinsv1.UserDetails{
+			Name:     "Test",
+			Email:    "test@acme.com",
+			Accounts: make([]jenkinsv1.AccountReference, 0),
+		},
+	}
+
+	cmd.ConfigureTestOptionsWithResources(&o,
+		[]runtime.Object{},
+		[]runtime.Object{&user1},
+		&gits.GitFake{},
+		&gits.FakeProvider{},
+		helm_test.NewMockHelmer(),
+		resources_test.NewMockInstaller(),
+	)
+
+	jxClient, ns, err := o.JXClientAndDevNamespace()
+	assert.NoError(t, err)
+
+	// Resolve returns the new user as the first one doesn't match because the emails are different
+	_, err = users.Resolve("", "testProvider", jxClient, ns, func(id string, users []jenkinsv1.User) (string,
+		[]jenkinsv1.User, *jenkinsv1.User, error) {
+		// A real selector would actually check the emails are different, here we force it!
+		return "test-user", []jenkinsv1.User{}, &user2, nil
+	})
+	assert.Error(t, err)
+}
