da3d27efad54294c58ad9bbed4f9f0d9b0204711
JenkinsXBot <jenkins-x@googlegroups.com>
2019-03-30 07:35:32 +0000

    fix: release pipeline for creating the docs

--- jx/scripts/update-website.sh
+++ jx/scripts/update-website.sh
@@ -5,12 +5,12 @@ set -o nounset
 set -o pipefail
 
 echo "Updating the JX CLI & API reference docs"
-./build/linux/jx create client docs --verbose
+./build/linux/jx create docs --verbose
 git clone https://github.com/jenkins-x/jx-docs.git
 cp -r docs/apidocs/site jx-docs/static/apidocs
 cd static/apidocs; git add *
 cd content/commands; \
-    ../../build/linux/jx create docs; \
+    ../../build/linux/jx docs; \
     git config credential.helper store; \
     git add *; \
     git commit --allow-empty -a -m "updated jx commands & API docs from $VERSION"; \
