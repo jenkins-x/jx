8959b9f6951e9dec33625eff62d469f4dab57e1d
James Strachan <james.strachan@gmail.com>
2019-04-03 12:13:32 +0100

    fix: add helper command to edit the deploy kind for the current app
    lets make `jx edit deploy` work for a team default or to edit the current apps codee

--- pkg/jx/cmd/edit_deploy.go
+++ pkg/jx/cmd/edit_deploy.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/jenkins-x/jx/pkg/apis/jenkins.io/v1"
 	"github.com/jenkins-x/jx/pkg/log"
@@ -11,13 +12,18 @@ import (
 	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
 )
 
+const (
+	deployKindKnative = "knative"
+	deployKindDefault = "default"
+)
+
 var (
 	editDeployKindLong = templates.LongDesc(`
-		Edits the deploy kind to use for your team
+		Edits the deploy kind to use for your project or team
 `)
 
 	editDeployKindExample = templates.Examples(`
-		# Edit the deploy kind and prompts you to pick one of the available kinds
+		# Edit the deploy kind for your current project and prompts you to pick one of the available kinds
 		jx edit deploy
 
         # to switch to use Knative Serve deployments
@@ -25,9 +31,17 @@ var (
 
         # to switch to normal kubernetes deployments
 		jx edit deploy default
+
+		# Edit the default deploy kind for your team
+		jx edit deploy --team
+
+		# Set the default for your team to use knative
+		jx edit deploy --team knative
 	`)
 
-	deployKinds = []string{"knative", "default"}
+	deployKinds = []string{deployKindKnative, deployKindDefault}
+
+	knativeDeployKey = "knativeDeploy:"
 )
 
 // EditDeployKindOptions the options for the create spring command
@@ -35,6 +49,8 @@ type EditDeployKindOptions struct {
 	EditOptions
 
 	Kind string
+	Dir  string
+	Team bool
 }
 
 // NewCmdEditDeployKind creates a command object for the "create" command
@@ -47,7 +63,7 @@ func NewCmdEditDeployKind(commonOpts *CommonOptions) *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:     "deploy",
-		Short:   "Edits the deploy kind to use for your team",
+		Short:   "Edits the deploy kind to use for your project or team",
 		Long:    editDeployKindLong,
 		Example: editDeployKindExample,
 		Run: func(cmd *cobra.Command, args []string) {
@@ -57,42 +73,102 @@ func NewCmdEditDeployKind(commonOpts *CommonOptions) *cobra.Command {
 			CheckErr(err)
 		},
 	}
+	cmd.Flags().BoolVarP(&options.Team, "team", "t", false, "Edits the team default")
+
 	return cmd
 }
 
 // Run implements the command
 func (o *EditDeployKindOptions) Run() error {
+	settings, err := o.TeamSettings()
+	if err != nil {
+		return err
+	}
+	if o.Team {
+		name, err := o.pickDeployKind(settings.DeployKind)
+		if err != nil {
+			return err
+		}
+		callback := func(env *v1.Environment) error {
+			teamSettings := &env.Spec.TeamSettings
+			teamSettings.DeployKind = name
+
+			log.Infof("Setting the team deploy kind to: %s\n", util.ColorInfo(name))
+			return nil
+		}
+		return o.ModifyDevEnvironment(callback)
+	}
+
+	fn := func(text string) (string, error) {
+		defaultName := o.findDefaultDeployKindInValuesYaml(text)
+		name := ""
+
+		name, err := o.pickDeployKind(defaultName)
+		if err != nil {
+			return name, err
+		}
+		return o.setDeployKindInValuesYaml(text, name)
+	}
+	return o.modifyHelmValuesFile(o.Dir, fn)
+}
+
+func (o *EditDeployKindOptions) findDefaultDeployKindInValuesYaml(yamlText string) string {
+	// lets try find the current setting
+	knativeFlag := ""
+	lines := strings.Split(yamlText, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if strings.HasPrefix(line, knativeDeployKey) {
+			knativeFlag = strings.TrimSpace(line[len(knativeDeployKey):])
+			break
+		}
+	}
+	if knativeFlag == "true" {
+		return deployKindKnative
+	}
+	return deployKindDefault
+}
+
+// setDeployKindInValuesYaml sets the `knativeDeployKey` key to true or false based on the deployment kind
+func (o *EditDeployKindOptions) setDeployKindInValuesYaml(yamlText string, deployKind string) (string, error) {
+	var buffer strings.Builder
+
+	lines := strings.Split(yamlText, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if strings.HasPrefix(line, knativeDeployKey) {
+			buffer.WriteString(knativeDeployKey)
+			buffer.WriteString(" ")
+			if deployKind == deployKindKnative {
+				buffer.WriteString("true")
+			} else {
+				buffer.WriteString("false")
+			}
+		} else {
+			buffer.WriteString(line)
+		}
+		buffer.WriteString("\n")
+	}
+	return buffer.String(), nil
+}
+
+func (o *EditDeployKindOptions) pickDeployKind(defaultName string) (string, error) {
 	args := o.Args
-	name := ""
-	if len(args) == 0 {
-		if o.BatchMode {
-			return util.MissingArgument("kind")
-		}
-		settings, err := o.TeamSettings()
-		if err != nil {
-			return err
-		}
-		defaultName := settings.DeployKind
-		if defaultName == "" || util.StringArrayIndex(deployKinds, defaultName) < 0 {
-			defaultName = "default"
-		}
-		name, err = util.PickNameWithDefault(deployKinds, "Pick the deployment kind: ", defaultName, "lets you switch between knative serve based deployments and default kubernetes deployments", o.In, o.Out, o.Err)
-		if err != nil {
-			return err
-		}
-		if name == "" {
-			return fmt.Errorf("no kind chosen")
-		}
-	} else {
-		name = args[0]
+	if len(args) > 0 {
+		return args[0], nil
 	}
-
-	callback := func(env *v1.Environment) error {
-		teamSettings := &env.Spec.TeamSettings
-		teamSettings.DeployKind = name
-
-		log.Infof("Setting the team deploy kind to: %s\n", util.ColorInfo(name))
-		return nil
+	if o.BatchMode {
+		return "", util.MissingArgument("kind")
 	}
-	return o.ModifyDevEnvironment(callback)
+	if defaultName == "" || util.StringArrayIndex(deployKinds, defaultName) < 0 {
+		defaultName = "default"
+	}
+	name, err := util.PickNameWithDefault(deployKinds, "Pick the deployment kind: ", defaultName, "lets you switch between knative serve based deployments and default kubernetes deployments", o.In, o.Out, o.Err)
+	if err != nil {
+		return name, err
+	}
+	if name == "" {
+		return name, fmt.Errorf("no kind chosen")
+	}
+	return name, nil
 }
