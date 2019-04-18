f0e892e6ac34c75a6680ae2fb7c200f9c2c2144a
James Rawlings <rawlingsj80@gmail.com>
2019-04-18 13:56:17 +0100

    fix parsing vault placeholders (#3668)
    * switch upgrade platform to use public chartmuseum repo
    
    * fix parsing vault placeholders

--- pkg/vault/helpers_test.go
+++ pkg/vault/helpers_test.go
@@ -43,3 +43,31 @@ func TestReplaceURIs(t *testing.T) {
   bar: %s
 `, secret), result)
 }
+
+func TestReplaceRealExampleURI(t *testing.T) {
+	pegomock.RegisterMockTestingT(t)
+	vaultClient := vault_test.NewMockClient()
+	path := "secret/gitOps/jenkins-x/environment-tekton-mole-dev/connectors-github-config-clientid-secret"
+	key := "token-passthrough"
+	secret := uuid.New()
+	valuesyaml := fmt.Sprintf(`foo:
+  bar: vault:%s:%s
+`, path, key)
+	valuesFile, err := ioutil.TempFile("", "values.yaml")
+	defer func() {
+		err := util.DeleteFile(valuesFile.Name())
+		assert.NoError(t, err)
+	}()
+	assert.NoError(t, err)
+	err = ioutil.WriteFile(valuesFile.Name(), []byte(valuesyaml), 0600)
+	assert.NoError(t, err)
+	pegomock.When(vaultClient.Read(pegomock.EqString(path))).ThenReturn(map[string]interface{}{
+		key: secret,
+	}, nil)
+	result, err := vault.ReplaceURIs(valuesyaml, vaultClient)
+	assert.NoError(t, err)
+	assert.NoError(t, err)
+	assert.Equal(t, fmt.Sprintf(`foo:
+  bar: %s
+`, secret), result)
+}
