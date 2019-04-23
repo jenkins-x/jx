0fb0f134bb4b595614251de3acb69670f1e510eb
James Strachan <james.strachan@gmail.com>
2019-04-23 12:58:21 +0100

    fix: avoid tiller less failing with empty templates (#3706)
    * fix: lets trim the helm chart a tad
    
    * fix: only use jenkins x pipelines if using prow as well
    
    fixes #3689
    
    * fix: avoid tiller less failing with empty templates
    
    fixes #3613

--- pkg/helm/helm_template.go
+++ pkg/helm/helm_template.go
@@ -664,10 +664,16 @@ func splitObjectsInFiles(file string) ([]string, error) {
 	count := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		if line == resourcesSeparator && buf.Len() > 0 {
+		if line == resourcesSeparator {
 			// ensure that we actually have YAML in the buffer
+			data := buf.Bytes()
+			if isWhitespaceOrComments(data) {
+				buf.Reset()
+				continue
+			}
+
 			m := yaml.MapSlice{}
-			err = yaml.Unmarshal(buf.Bytes(), &m)
+			err = yaml.Unmarshal(data, &m)
 			if err != nil {
 				return make([]string, 0), errors.Wrapf(err, "Failed to parse the following YAML from file '%s':\n%s", file, buf.String())
 			}
@@ -693,7 +699,7 @@ func splitObjectsInFiles(file string) ([]string, error) {
 			}
 		}
 	}
-	if buf.Len() > 0 {
+	if buf.Len() > 0 && !isWhitespaceOrComments(buf.Bytes()) {
 		if count > 0 {
 			objFile, err := writeObjectInFile(&buf, dir, fileName, count)
 			if err != nil {
@@ -706,6 +712,21 @@ func splitObjectsInFiles(file string) ([]string, error) {
 	}
 
 	return result, nil
+}
+
+// isWhitespaceOrComments returns true if the data is empty, whitespace or comments only
+func isWhitespaceOrComments(data []byte) bool {
+	if len(data) == 0 {
+		return true
+	}
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		t := strings.TrimSpace(line)
+		if t != "" && !strings.HasPrefix(t, "#") {
+			return false
+		}
+	}
+	return true
 }
 
 func writeObjectInFile(buf *bytes.Buffer, dir string, fileName string, count int) (string, error) {
