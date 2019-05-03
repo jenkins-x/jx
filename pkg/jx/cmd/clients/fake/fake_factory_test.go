ab10127b563f5a52efee0f6d75ab62ea7410b934
James Strachan <james.strachan@gmail.com>
2019-05-03 12:22:02 +0100

    fix: lets add a FakeFactory for easier testing
    without requiring mocks

---
+++ pkg/jx/cmd/clients/fake/fake_factory_test.go
@@ -0,0 +1,23 @@
+package fake
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestFakeFactory(t *testing.T) {
+	f := NewFakeFactory()
+	client, ns, err := f.CreateKubeClient()
+	require.NoError(t, err, "CreateKubeClient() failed")
+	assert.Equal(t, "jx", ns, "namespace")
+	assert.NotNil(t, client, "client")
+
+	namespaces, err := client.CoreV1().Namespaces().List(metav1.ListOptions{})
+	require.NoError(t, err, "Namespaces().List() failed")
+	require.NotNil(t, namespaces, "namespaces")
+	assert.Equal(t, 0, len(namespaces.Items), "namespaces")
+
+}
