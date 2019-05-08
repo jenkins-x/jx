15205069d7fc3e0e81d9652cd2d1f21dcaffd88d
James Strachan <james.strachan@gmail.com>
2019-05-08 11:04:34 +0100

    chore: revert using a default image type on GKE for tekton (#3883)
    * chore: revert using a default image type on GKE for tekton
    
    as it doesn't seem to work around the tekton issues - so lets roll back to an older tekton instead
    
    * fix: let allow an output file be specified for the json schema file
    
    also lets default to `--pipeline` mode if no other specified
    
    * fix: lets update the json schema on each release
    
    also move the frequently failing API doc generation last so we get the updated CLI reference back

--- jx/scripts/update-website.sh
+++ jx/scripts/update-website.sh
@@ -4,14 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "Updating the JX CLI & API reference docs"
-make generate-docs
+echo "updating the CLI reference"
 git clone https://github.com/jenkins-x/jx-docs.git
-cp -r docs/apidocs/site jx-docs/static/apidocs
-
-pushd jx-docs/static/apidocs
-  git add *
-popd
 
 pushd jx-docs/content/commands
   ../../../build/linux/jx create docs
@@ -20,3 +14,25 @@ pushd jx-docs/content/commands
   git commit --allow-empty -a -m "updated jx commands & API docs from $VERSION"
   git push origin
 popd
+
+
+echo "Updating the JSON Schema"
+pushd jx-docs/content
+  mkdir -p schemas
+  cd schemas
+  ../../../build/linux/jx step syntax schema -o jx-schema.json
+  git add *
+  git commit --allow-empty -a -m "updated jx Json Schema from $VERSION"
+  git push origin
+popd
+
+echo "Updating the JX CLI & API reference docs"
+make generate-docs
+cp -r docs/apidocs/site jx-docs/static/apidocs
+
+pushd jx-docs/static/apidocs
+  git add *
+  git commit --allow-empty -a -m "updated jx API docs from $VERSION"
+  git push origin
+popd
+
