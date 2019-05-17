f9b5fab8fd809eebc71b21183a2722f4c3ee4579
Miguel Garcia <miguel.garcia@gmail.com>
2019-05-17 04:26:07 -0300

    feat(install): added flag to pass ingress-class to exposecontroller (#3162)
    * feat(install): added flag to pass ingress-class to exposecontroller
    
    * chore: fix formatting
    
    Signed-off-by: Hardy Ferentschik <hardy@ferentschik.de>

--- pkg/config/helm_values_test.go
+++ pkg/config/helm_values_test.go
@@ -61,3 +61,29 @@ func TestEnvironmentExposecontrollerHelmValuesWithUrlTemplate(t *testing.T) {
 	values.ExposeController.Config.UrlTemplate = "{{.Service}}-{{.Namespace}}.{{.Domain}}"
 	assert.Equal(t, helmValuesFromFile, values, "expected exposecontroller helm values do not match")
 }
+
+func TestEnvironmentExposecontrollerHelmValuesWithIngressClass(t *testing.T) {
+	t.Parallel()
+
+	testFile, err := ioutil.ReadFile("helm_values_test_with_ingress_class.yaml")
+	assert.NoError(t, err)
+	helmValuesFromFile := config.HelmValuesConfig{}
+	err = yaml.Unmarshal(testFile, &helmValuesFromFile)
+	assert.NoError(t, err)
+
+	a := make(map[string]string)
+	a["helm.sh/hook"] = "post-install,post-upgrade"
+	a["helm.sh/hook-delete-policy"] = "hook-succeeded"
+
+	values := config.HelmValuesConfig{
+		ExposeController: &config.ExposeController{},
+	}
+
+	values.ExposeController.Annotations = a
+	values.ExposeController.Config.Exposer = "Ingress"
+	values.ExposeController.Config.Domain = "jenkinsx.io"
+	values.ExposeController.Config.HTTP = "false"
+	values.ExposeController.Config.TLSAcme = "false"
+	values.ExposeController.Config.IngressClass = "my-ingress-class"
+	assert.Equal(t, helmValuesFromFile, values, "expected exposecontroller helm values do not match")
+}
