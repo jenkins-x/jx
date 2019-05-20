a50a45cd608d3a2f6b09844cba655de7e67b545f
Pete Muir <pmuir@bleepbleep.org.uk>
2019-05-20 15:28:25 +0100

    Chore: try to enable the client update script (#4023)
    Signed-off-by: Pete Muir <pmuir@bleepbleep.org.uk>

--- jx/scripts/update-clients.sh
+++ jx/scripts/update-clients.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 ORG_REPOS=("jenkins-x/jx-ts-client")
 VERSION="$(cat VERSION)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd $DIR
+pushd ../../docs/apidocs/openapi-spec
+SRCDIR=`pwd`
+popd
+popd
+SRC="${SRCDIR}/openapiv2.yaml"
 for org_repo in "${ORG_REPOS[@]}"; do
   OUTDIR="$(jx step git fork-and-clone -b --print-out-dir --dir=$TMPDIR https://github.com/$org_repo)"
   echo "Forked repo to $OUTDIR"
@@ -12,6 +19,8 @@ for org_repo in "${ORG_REPOS[@]}"; do
   git diff --exit-code
   if [ $? -ne 0 ]; then
     jx create pullrequest -b --push=true --body="upgrade $org_repo client to jx $VERSION" --title="upgrade to jx $VERSION" --label="updatebot"
+  else
+    echo "No changes to generated code"
   fi
   popd
 done
