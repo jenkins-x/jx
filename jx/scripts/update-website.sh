eb845023913eecb8079213efb3397a8430570e6a
Pete Muir <pmuir@bleepbleep.org.uk>
2019-05-22 15:47:32 +0100

    fix: rebase needs to be after commit (#4048)
    Signed-off-by: Pete Muir <pmuir@bleepbleep.org.uk>

--- jx/scripts/update-website.sh
+++ jx/scripts/update-website.sh
@@ -10,9 +10,9 @@ git clone https://github.com/jenkins-x/jx-docs.git
 pushd jx-docs/content/commands
   ../../../build/linux/jx create docs
   git config credential.helper store
-  git fetch origin && git rebase origin/master
   git add *
   git commit --allow-empty -a -m "updated jx commands & API docs from $VERSION"
+  git fetch origin && git rebase origin/master
   git push origin
 popd
 
@@ -22,9 +22,9 @@ pushd jx-docs/content
   mkdir -p schemas
   cd schemas
   ../../../build/linux/jx step syntax schema -o jx-schema.json
-  git fetch origin && git rebase origin/master
   git add *
   git commit --allow-empty -a -m "updated jx Json Schema from $VERSION"
+  git fetch origin && git rebase origin/master
   git push origin
 popd
 
@@ -33,9 +33,9 @@ make generate-docs
 cp -r docs/apidocs/site jx-docs/static/apidocs
 
 pushd jx-docs/static/apidocs
-  git fetch origin && git rebase origin/master
   git add *
   git commit --allow-empty -a -m "updated jx API docs from $VERSION"
+  git fetch origin && git rebase origin/master
   git push origin
 popd
 
