083f1223c4201f08c9ac848c9bf0bf6b05615d9d
Pete Muir <pmuir@bleepbleep.org.uk>
2019-05-29 15:52:42 +0100

    fix: try another way to set args for covered binary (#4111)
    Signed-off-by: Pete Muir <pmuir@bleepbleep.org.uk>

--- cmd/jx/app/jx.go
+++ cmd/jx/app/jx.go
@@ -9,11 +9,12 @@ import (
 	"github.com/jenkins-x/jx/pkg/jx/cmd/clients"
 )
 
-// Run runs the command
+// Run runs the command, if args are not nil they will be set on the command
 func Run(args []string) error {
-	if len(args) > 0 {
-		args = args[1:]
-	}
 	cmd := cmd.NewJXCommand(clients.NewFactory(), os.Stdin, os.Stdout, os.Stderr, nil)
+	if args != nil {
+		args = args[1:]
+		cmd.SetArgs(args)
+	}
 	return cmd.Execute()
 }
