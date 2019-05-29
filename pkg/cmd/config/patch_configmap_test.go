2877bd170cd887342b94a26e6f4e263a97517032
Daniel Gozalo <dgozalo@cloudbees.com>
2019-05-29 19:30:04 +0200

    feature: created a step to handle JsonPatch syntax queries into ConfigMaps and literal json/yamls in the Data field
    feature: changed the way the patch is applied
    
    feature: added needed dependency
    
    fix: removed unused file
    
    feature: added a step to apply json patches to config maps and second level embedded items in config maps
    Signed-off-by: Daniel Gozalo <dgozalo@cloudbees.com>
    
    chore: deleted unused code
    
    chore: lint and fmt
    
    chore: lint
    
    fix: used the right version for vault

---
+++ pkg/cmd/config/patch_configmap_test.go
@@ -0,0 +1,109 @@
+package config
+
+import (
+	"github.com/ghodss/yaml"
+	"github.com/jenkins-x/jx/pkg/cmd/clients/fake"
+	"github.com/jenkins-x/jx/pkg/cmd/opts"
+	"github.com/jenkins-x/jx/pkg/cmd/testhelpers"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"k8s.io/api/core/v1"
+	k8sv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestStepModifyConfigMapRootLevel(t *testing.T) {
+	t.Parallel()
+
+	commonOpts := opts.NewCommonOptionsWithFactory(fake.NewFakeFactory())
+	options := &commonOpts
+	testhelpers.ConfigureTestOptions(options, options.Git(), options.Helm())
+
+	kubeClient, ns, err := options.KubeClientAndNamespace()
+	assert.NoError(t, err)
+
+	wd, err := os.Getwd()
+	assert.NoError(t, err)
+
+	testDataPath := filepath.Join(wd, "..", "test_data", "step_config", "config_map1.txt")
+	bytes, err := ioutil.ReadFile(testDataPath)
+	assert.NoErrorf(t, err, "The test data in folder %s should be read", testDataPath)
+
+	configMap := &v1.ConfigMap{}
+	err = yaml.Unmarshal(bytes, configMap)
+	assert.NoErrorf(t, err, "The test data in folder %s should be unmarshaled", testDataPath)
+
+	_, err = kubeClient.CoreV1().ConfigMaps(ns).Create(configMap)
+	assert.NoError(t, err, "the test config map should be created")
+
+	o := &StepModifyConfigMapOptions{
+		StepOptions: opts.StepOptions{
+			CommonOptions: options,
+		},
+
+		JSONPatch:     `{"metadata": {"initializers": {"result": {"status": "newstatus"}}}}`,
+		ConfigMapName: "config",
+		// The fake client only supports strategic
+		Type: "strategic",
+	}
+
+	err = o.Run()
+	assert.NoError(t, err)
+
+	updatedConfig, err := kubeClient.CoreV1().ConfigMaps(ns).Get("config", k8sv1.GetOptions{})
+	assert.NoError(t, err, "there should be a config map called config")
+
+	assert.Equal(t, "newstatus", updatedConfig.Initializers.Result.Status)
+}
+
+func TestStepModifyConfigMapFirstLevelPropertySet(t *testing.T) {
+	t.Parallel()
+
+	commonOpts := opts.NewCommonOptionsWithFactory(fake.NewFakeFactory())
+	options := &commonOpts
+	testhelpers.ConfigureTestOptions(options, options.Git(), options.Helm())
+
+	kubeClient, ns, err := options.KubeClientAndNamespace()
+	assert.NoError(t, err)
+
+	wd, err := os.Getwd()
+	assert.NoError(t, err)
+
+	testDataPath := filepath.Join(wd, "..", "test_data", "step_config", "config_map1.txt")
+	bytes, err := ioutil.ReadFile(testDataPath)
+	assert.NoErrorf(t, err, "The test data in folder %s should be read", testDataPath)
+
+	configMap := &v1.ConfigMap{}
+	err = yaml.Unmarshal(bytes, configMap)
+	assert.NoErrorf(t, err, "The test data in folder %s should be Unmarshal", testDataPath)
+
+	_, err = kubeClient.CoreV1().ConfigMaps(ns).Create(configMap)
+	assert.NoError(t, err, "the test config map should be created")
+
+	o := &StepModifyConfigMapOptions{
+		StepOptions: opts.StepOptions{
+			CommonOptions: options,
+		},
+
+		JSONPatch:          `[{"op": "replace", "path": "/plank", "value" : {"url": "http:"}}]`,
+		ConfigMapName:      "config",
+		FirstLevelProperty: "config.yaml",
+	}
+
+	err = o.Run()
+	assert.NoError(t, err)
+
+	updatedConfig, err := kubeClient.CoreV1().ConfigMaps(ns).Get("config", k8sv1.GetOptions{})
+	assert.NoError(t, err, "there should be a config map called config")
+
+	prowConfig := updatedConfig.Data["config.yaml"]
+	k := make(map[string]interface{})
+	err = yaml.Unmarshal([]byte(prowConfig), &k)
+	assert.NoError(t, err)
+
+	k = k["plank"].(map[string]interface{})
+
+	assert.Equal(t, "http:", k["url"])
+}
