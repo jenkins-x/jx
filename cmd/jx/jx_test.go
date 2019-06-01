d2c9eeb3295ac2bb017f246dd5d947587d8b326f
Pete Muir <pmuir@bleepbleep.org.uk>
2019-06-01 22:40:41 +0100

    feat: switch to a self uploading covered binary
    If called when compiled into coverage mode, this binary will call itself, adding the `test.coverprofile` argument, and, after the subprocess exits, upload the coverage to codecov. It also fixes the output to remove the coverage lines generated by go. This allows us to use it transparently in tests, only setting the `CODECOV_TOKEN` into the environment.
    
    Signed-off-by: Pete Muir <pmuir@bleepbleep.org.uk>

--- cmd/jx/jx_test.go
+++ cmd/jx/jx_test.go
@@ -2,18 +2,51 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"path/filepath"
+	"regexp"
 	"strings"
 	"testing"
+
+	"github.com/pkg/errors"
+
+	"github.com/jenkins-x/jx/cmd/jx/codecov"
+
+	"github.com/jenkins-x/jx/pkg/log"
+
+	"github.com/pborman/uuid"
+
+	"github.com/jenkins-x/jx/pkg/util"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/jenkins-x/jx/cmd/jx/app"
 )
 
+type TestSystemWriter struct {
+	wrapped io.Writer
+}
+
+var coverageOutputRegex = regexp.MustCompile(`(?m:^coverage: ([\d\.]*%) of statements in [\w\.\/]*$)`)
+var passOutputRegex = regexp.MustCompile(`(?m:^PASS$)`)
+
+func (t TestSystemWriter) Write(p []byte) (n int, err error) {
+	s := string(p)
+	s = coverageOutputRegex.ReplaceAllString(s, "")
+	s = passOutputRegex.ReplaceAllString(s, "")
+	_, err = t.wrapped.Write([]byte(s))
+	return len(p), err
+}
+
 func TestSystem(t *testing.T) {
-	enableCoverage := os.Getenv("COVER_JX_BINARY") == "true"
-	if enableCoverage {
+	disableCoverage := os.Getenv("COVER_JX_BINARY") == "false"
+	if !disableCoverage {
+		disableSelfUpload := os.Getenv("COVER_SELF_UPLOAD") == "false"
+		alreadyWrapped := os.Getenv("COVER_WRAPPED") == "true"
+		coverprofileArgFound := false
 		args := make([]string, 0)
 		strippedArgs := make([]string, 0)
 		for i, arg := range os.Args {
@@ -25,14 +58,149 @@ func TestSystem(t *testing.T) {
 			} else {
 				strippedArgs = append(strippedArgs, arg)
 			}
+			if strings.HasPrefix(arg, "-test.coverprofile") {
+				coverprofileArgFound = true
+			}
 		}
-		fmt.Printf("This is a covered JX binary. Run with -test.coverprofile=mycover.out to generate coverage\n")
-		fmt.Printf("Removed arguments %s\n", strings.Join(strippedArgs, ", "))
-		fmt.Printf("Arguments passed to `jx` are %s\n\n", strings.Join(args, ", "))
-		// Purposefully ignore errors from app.Run as we are checking coverage
-		err := app.Run(args)
-		assert.NoError(t, err, "error executing jx")
+		if !alreadyWrapped && (!disableSelfUpload || !coverprofileArgFound) {
+			// We need to wrap our own execution
+			var outFile string
+			var id string
+			if !coverprofileArgFound {
+				reportsDir := os.Getenv("REPORTS_DIR")
+				if reportsDir == "" {
+					reportsDir = filepath.Join("build", "reports")
+				}
+				err := os.MkdirAll(reportsDir, 0700)
+				if err != nil {
+					log.Errorf("Error making reports directory %s %v", reportsDir, err)
+				}
+				id = "jx"
+				for _, arg := range args[1:] {
+					if strings.HasPrefix(arg, "-") {
+						break
+					}
+					id = fmt.Sprintf("%s_%s", id, arg)
+				}
+				args = append(args, "" /* use the zero value of the element type */)
+				copy(args[2:], args[1:])
+				outFile = filepath.Join(reportsDir, fmt.Sprintf("%s.%s.out", id, uuid.New()))
+				args[1] = fmt.Sprintf("-test.coverprofile=%s", outFile)
+			} else if !disableSelfUpload {
+				// TODO support this
+				log.Errorf("Self upload is not supported if -test.coverprofile is specified. Disabling it.")
+				disableSelfUpload = true
+			}
+			cmd := util.Command{
+				Env: map[string]string{
+					"COVER_WRAPPED": "true",
+				},
+				Args: args[1:],
+				Name: os.Args[0],
+				Out: TestSystemWriter{
+					wrapped: os.Stdout,
+				},
+				In:  os.Stdin,
+				Err: os.Stderr,
+			}
+			_, err := cmd.RunWithoutRetry()
+			if !disableSelfUpload {
+				if os.Getenv("CODECOV_TOKEN") == "" {
+					log.Errorf("cannot upload to codecov because CODECOV_TOKEN environment variable is not set")
+				} else {
+					err := uploadToCodecov(outFile, id)
+					if err != nil {
+						log.Errorf("cannot upload to codecov because %v", err)
+					}
+				}
+
+			}
+			if err != nil {
+				log.Error(err.Error())
+				os.Exit(1)
+			}
+			os.Exit(0)
+		} else {
+			// Purposefully ignore errors from app.Run as we are checking coverage
+			err := app.Run(args)
+			// the assert.NoError defers the error reporting until after the coverage is written out
+			assert.NoError(t, err, "error executing jx")
+		}
 	} else {
 		main()
 	}
+}
+
+func uploadToCodecov(outFile string, name string) error {
+	script, err := downloadCodecovUploader()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	err = os.Chmod(script, 0700)
+	if err != nil {
+		return errors.Wrapf(err, "making %s executable", script)
+	}
+	args := []string{
+		"-Z",
+		"-f",
+		outFile,
+		"-n",
+		name,
+	}
+	if codecov.Flag != "" {
+		args = append(args, "-F", codecov.Flag)
+	}
+	if codecov.BuildNumber != "" {
+		args = append(args, "-b", codecov.BuildNumber)
+	}
+	if codecov.PullRequestNumber != "" {
+		args = append(args, "-P", codecov.PullRequestNumber)
+	}
+	if codecov.Tag != "" {
+		args = append(args, "-T", codecov.Tag)
+	}
+	cmd := util.Command{
+		Name: script,
+		Env: map[string]string{
+			"DOCKER_REPO":   codecov.Slug,
+			"SOURCE_COMMIT": codecov.Sha,
+			"SOURCE_BRANCH": codecov.Branch,
+		},
+		Args: args,
+	}
+	out, err := cmd.RunWithoutRetry()
+	if err != nil {
+		log.Errorf("Running %s", cmd.String())
+		log.Errorf(out)
+		return errors.Wrapf(err, "error uploading coverage to codecov.io")
+
+	}
+	return nil
+}
+
+func downloadCodecovUploader() (string, error) {
+	script, err := ioutil.TempFile("", "codecov")
+	defer script.Close()
+	if err != nil {
+		return "", errors.Wrapf(err, "creating tempfile")
+	}
+	err = downloadFile(script, "https://codecov.io/bash")
+	if err != nil {
+		return "", errors.Wrapf(err, "downloading codecov uploader")
+	}
+	return script.Name(), nil
+}
+
+func downloadFile(out *os.File, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
