b311907555f6c82135326eed957b677fcb4a6660
Pete Muir <pmuir@bleepbleep.org.uk>
2019-06-02 21:51:32 +0100

    fix: handle validation service for codecov being down

--- hack/codecov-validate.sh
+++ hack/codecov-validate.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-CODECOV_VALIDATOR=$(curl -s -X POST --data-binary @.codecov.yml https://codecov.io/validate)
+CODECOV_VALIDATOR=$(curl --max-time 10 -s -X POST --data-binary @.codecov.yml https://codecov.io/validate)
 
 if ! echo "${CODECOV_VALIDATOR}" | grep -q "Valid!"; then
-  echo ".codecov.yml not valid"
-  echo ""
-  echo "${CODECOV_VALIDATOR}"
-  exit 1
+  if [[ ${CODECOV_VALIDATOR} -eq "" ]]; then
+    echo "cannot validate .codecov.yaml right now as request timed out"
+    exit 0
+  else
+    echo ".codecov.yml not valid"
+    echo ""
+    echo "${CODECOV_VALIDATOR}"
+    exit 1
+  fi
 else
   echo ".codecov.yml valid"
   exit 0
