4966ae2ecc74d0d2f6b9ad62337940dd3656b03c
Andrew Bayer <andrew.bayer@gmail.com>
2019-06-08 09:22:41 -0400

    fix(tekton): Populate activity with correct step order (#4207)
    `pod.Status.ContainerStatuses` does not appear to have a consistent
    order, unlike init containers. So we shouldn't be using that as the
    basis for the order we add steps to stages in
    `PipelineActivity`. Instead, we should use the `pod.Spec.Containers`
    order.
    
    Fixes #4016
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- pkg/jx/cmd/controller_build_test.go
+++ pkg/jx/cmd/controller_build_test.go
@@ -2,14 +2,19 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/jenkins-x/jx/pkg/jx/cmd/cmd_test_helpers"
-	"github.com/jenkins-x/jx/pkg/jx/cmd/controller"
+	"path"
+	"strings"
 	"testing"
+	"time"
 
 	v1 "github.com/jenkins-x/jx/pkg/apis/jenkins.io/v1"
 	"github.com/jenkins-x/jx/pkg/gits"
+	"github.com/jenkins-x/jx/pkg/jx/cmd/cmd_test_helpers"
+	"github.com/jenkins-x/jx/pkg/jx/cmd/controller"
 	"github.com/jenkins-x/jx/pkg/jx/cmd/opts"
 	"github.com/jenkins-x/jx/pkg/kube"
+	"github.com/jenkins-x/jx/pkg/tekton"
+	"github.com/jenkins-x/jx/pkg/tekton/tekton_helpers_test"
 	"k8s.io/apimachinery/pkg/runtime"
 
 	"github.com/stretchr/testify/assert"
@@ -82,6 +87,51 @@ func TestCompleteBuildSourceInfo(t *testing.T) {
 	assert.Equal(t, act.Spec.PullTitle, "This is the PR title")
 }
 
+func TestUpdateForStage(t *testing.T) {
+	pod := tekton_helpers_test.AssertLoadSinglePod(t, path.Join("test_data", "controller_build", "update_stage_info"))
+	si := &tekton.StageInfo{
+		Name:           "ci",
+		CreatedTime:    *parseTime(t, "2019-06-07T18:14:19-00:00"),
+		FirstStepImage: "gcr.io/abayer-jx-experiment/creds-init:v20190508-91b53326",
+		PodName:        "jenkins-x-jx-pr-4135-integratio-42-ci-kdjkp-pod-d964e6",
+		TaskRun:        "jenkins-x-jx-pr-4135-integratio-42-ci-kdjkp",
+		Task:           "jenkins-x-jx-pr-4135-integratio-ci-42",
+		Parents:        []string{},
+		Pod:            pod,
+	}
+
+	act := &v1.PipelineActivity{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "jenkins-x-jx-pr-4135-42",
+		},
+		Spec: v1.PipelineActivitySpec{
+			GitURL:    "https://github.com/jenkins-x/jx",
+			GitBranch: "PR-4135",
+		},
+	}
+
+	updateForStage(si, act)
+
+	containers, _, _ := kube.GetContainersWithStatusAndIsInit(pod)
+
+	assert.Len(t, act.Spec.Steps, 1, "No steps/stages found on activity")
+	assert.NotNil(t, act.Spec.Steps[0].Stage, "First step on activity is not a stage")
+
+	stage := act.Spec.Steps[0].Stage
+
+	assert.Equal(t, len(containers), len(stage.Steps), "%d containers found in pod, but %d steps found in stage", len(containers), len(stage.Steps))
+
+	for i, c := range containers {
+		step := stage.Steps[i]
+		name := strings.Replace(strings.TrimPrefix(c.Name, "build-step-"), "-", " ", -1)
+		title := strings.Title(name)
+		if title != step.Name {
+			// Using t.Errorf instead of an assert here because we want to see all the misordered names, not just the first one.
+			t.Errorf("For step %d, expected step with name %s, but found %s", i, title, step.Name)
+		}
+	}
+}
+
 func getGitHubSecret() *corev1.Secret {
 	return &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
@@ -136,4 +186,12 @@ func getFakeRepository() *gits.FakeRepository {
 			},
 		},
 	}
+}
+
+func parseTime(t *testing.T, timeString string) *time.Time {
+	parsed, err := time.Parse(time.RFC3339, timeString)
+	if assert.NoError(t, err, "Failed to parse date %s", timeString) {
+		return &parsed
+	}
+	return nil
 }
