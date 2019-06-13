925f1734cc63ab48c7baa5bd8c62f0638a48b6b6
Carlos Sanchez <carlos@apache.org>
2019-06-13 10:33:54 +0200

    fix: too many open files installing istio addon (#4226)
    Each file inside the tarball is kept open while tarball is extracted
    
    Signed-off-by: Carlos Sanchez <carlos@apache.org>

--- pkg/util/downloads.go
+++ pkg/util/downloads.go
@@ -252,23 +252,7 @@ func UnTargz(tarball, target string, onlyFiles []string) error {
 		}
 
 		path := filepath.Join(target, path.Base(header.Name))
-		info := header.FileInfo()
-		if info.IsDir() {
-			if err = os.MkdirAll(path, info.Mode()); err != nil {
-				return err
-			}
-			continue
-		}
-
-		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		_, err = io.Copy(file, tarReader)
-		if err != nil {
-			return err
-		}
+		UnTarFile(header, path, tarReader)
 	}
 	return nil
 }
@@ -299,23 +283,26 @@ func UnTargzAll(tarball, target string) error {
 		}
 
 		path := filepath.Join(target, header.Name)
-		info := header.FileInfo()
-		if info.IsDir() {
-			if err = os.MkdirAll(path, info.Mode()); err != nil {
-				return err
-			}
-			continue
-		}
-
-		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		_, err = io.Copy(file, tarReader)
-		if err != nil {
-			return err
-		}
+		UnTarFile(header, path, tarReader)
 	}
 	return nil
+}
+
+// UnTarFile extracts one file from the tar, or creates a directory
+func UnTarFile(header *tar.Header, path string, tarReader *tar.Reader) error {
+	info := header.FileInfo()
+	if info.IsDir() {
+		if err := os.MkdirAll(path, info.Mode()); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = io.Copy(file, tarReader)
+	return err
 }
