70c6977e7783385238353e45418db08132835465
David Conde <dconde@cloudbees.com>
2019-06-25 14:45:14 +0100

    feat: Add new jx steps to clean up e2e tests
    - jx step e2e gc
    - jx step e2e label mycluster --delete
    - jx step e2e label mycluster --keep
    
    fixes: #3914
    Signed-off-by: David Conde <dconde@cloudbees.com>

---
+++ pkg/cmd/deletecmd/delete_gke.go
@@ -0,0 +1,78 @@
+package deletecmd
+
+import (
+	"errors"
+	"github.com/jenkins-x/jx/pkg/cloud"
+	"github.com/jenkins-x/jx/pkg/cmd/get"
+	"github.com/jenkins-x/jx/pkg/cmd/helper"
+	"github.com/jenkins-x/jx/pkg/log"
+
+	"github.com/jenkins-x/jx/pkg/cmd/opts"
+	"github.com/jenkins-x/jx/pkg/cmd/templates"
+	"github.com/spf13/cobra"
+)
+
+// DeleteGkeOptions Options for deleting a GKE cluster
+type DeleteGkeOptions struct {
+	get.GetOptions
+	Region    string
+	ProjectID string
+}
+
+var (
+	deleteGkeLong = templates.LongDesc(`
+		Deletes GKE cluster resource.
+`)
+
+	deleteGkeExample = templates.Examples(`
+		# Delete GKE cluster
+		jx delete gke
+	`)
+)
+
+// NewCmdDeleteGke command for deleting a GKE cluster
+func NewCmdDeleteGke(commonOpts *opts.CommonOptions) *cobra.Command {
+	options := &DeleteGkeOptions{
+		GetOptions: get.GetOptions{
+			CommonOptions: commonOpts,
+		},
+	}
+	cmd := &cobra.Command{
+		Use:     "gke",
+		Short:   "Deletes GKE cluster.",
+		Long:    deleteGkeLong,
+		Example: deleteGkeExample,
+		Run: func(cmd *cobra.Command, args []string) {
+			options.Cmd = cmd
+			options.Args = args
+			err := options.Run()
+			helper.CheckErr(err)
+		},
+	}
+	cmd.Flags().StringVarP(&options.Region, "region", "", "europe-west1-c", "GKE region to use. Default: europe-west1-c")
+	cmd.Flags().StringVarP(&options.ProjectID, "project-id", "p", "", "Google Project ID to delete cluster from")
+	options.AddGetFlags(cmd)
+	return cmd
+}
+
+// Run implements this command
+func (o *DeleteGkeOptions) Run() error {
+	if len(o.Args) == 0 {
+		return errors.New("cluster name expected")
+	}
+	cluster := o.Args[0]
+	err := o.InstallRequirements(cloud.GKE)
+	if err != nil {
+		return err
+	}
+	args := []string{"container", "clusters", "delete", cluster, "--region=" + o.Region, "--quiet"}
+	if o.ProjectID != "" {
+		args = append(args, "--project="+o.ProjectID)
+	}
+	log.Logger().Info("Deleting cluster ...")
+	err = o.RunCommand("gcloud", args...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
