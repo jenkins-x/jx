1468dbd5434a551c5d803496ee09236f7312be07
James Strachan <james.strachan@gmail.com>
2019-06-26 10:46:39 +0100

    fix: lets add caching of the secreturl client
    also add a setter
    
    Signed-off-by: James Strachan <james.strachan@gmail.com>

--- pkg/cmd/opts/common.go
+++ pkg/cmd/opts/common.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/jenkins-x/jx/pkg/secreturl"
 	"github.com/spf13/pflag"
 
 	"github.com/heptio/sonobuoy/pkg/client"
@@ -138,6 +139,7 @@ type CommonOptions struct {
 	systemVaultClient      vault.Client
 	tektonClient           tektonclient.Interface
 	vaultClient            vault.Client
+	secretURLClient        secreturl.Client
 	vaultOperatorClient    vaultoperatorclient.Interface
 	AdvancedMode           bool
 }
