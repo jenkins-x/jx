5c92e641bbc2d1d0cec7c18523253ed52e53fe0e
James Strachan <james.strachan@gmail.com>
2019-06-28 14:16:15 +0100

    fix: allow 'jx boot' to be ran outside of a JX Boot git clone
    if no pipeline is found lets prompt the user to git clone the source of a git repository and run the 'jx boot' inside that directory instead
    
    Signed-off-by: James Strachan <james.strachan@gmail.com>

--- pkg/cmd/boot/boot.go
+++ pkg/cmd/boot/boot.go
@@ -2,11 +2,14 @@ package boot
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/jenkins-x/jx/pkg/cmd/helper"
 	"github.com/jenkins-x/jx/pkg/cmd/namespace"
 	"github.com/jenkins-x/jx/pkg/cmd/step/create"
 	"github.com/jenkins-x/jx/pkg/config"
+	"github.com/jenkins-x/jx/pkg/gits"
 	"github.com/jenkins-x/jx/pkg/log"
 	"github.com/jenkins-x/jx/pkg/util"
 	"github.com/pkg/errors"
@@ -17,11 +20,16 @@ import (
 	"github.com/jenkins-x/jx/pkg/cmd/templates"
 )
 
+const (
+	defaultBootRepository = "https://github.com/jstrachan/environment-simple-tekton.git"
+)
+
 // BootOptions options for the command
 type BootOptions struct {
 	*opts.CommonOptions
 
-	Dir string
+	Dir    string
+	GitURL string
 }
 
 var (
@@ -62,16 +70,92 @@ func NewCmdBoot(commonOpts *opts.CommonOptions) *cobra.Command {
 		},
 	}
 	cmd.Flags().StringVarP(&options.Dir, "dir", "d", ".", "the directory to look for the Jenkins X Pipeline, requirements and charts")
+	cmd.Flags().StringVarP(&options.GitURL, "git-url", "u", defaultBootRepository, "the Git clone URL for the JX Boot source to boot up")
 	return cmd
 }
 
 // Run runs this command
 func (o *BootOptions) Run() error {
+	info := util.ColorInfo
+
+	projectConfig, pipelineFile, err := config.LoadProjectConfig(o.Dir)
+	if err != nil {
+		return err
+	}
+	exists, err := util.FileExists(pipelineFile)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		log.Logger().Infof("No Jenkins X pipeline file %s found. You are not running this command from inside a Jenkins X Boot git clone\n\n", info(pipelineFile))
+
+		gitURL := o.GitURL
+		if gitURL == "" {
+			return util.MissingOption("git-url")
+		}
+		gitInfo, err := gits.ParseGitURL(gitURL)
+		if err != nil {
+			return errors.Wrapf(err, "failed to parse git URL %s", gitURL)
+		}
+
+		repo := gitInfo.Name
+		cloneDir := filepath.Join(o.Dir, repo)
+
+		if !o.BatchMode {
+			log.Logger().Infof("To continue we will clone: %s\n", info(gitURL))
+			log.Logger().Infof("To the directory: %s\n\n", info(cloneDir))
+
+			help := "A git clone of a Jenkins X Boot source repository is required for 'jx boot'"
+			message := "Do you want to clone the Jenkins X Boot Git repository?"
+			if !util.Confirm(message, true, help, o.In, o.Out, o.Err) {
+				return fmt.Errorf("Please run this command again inside a git clone from a Jenkins X Boot repository")
+			}
+		}
+
+		exists, err = util.FileExists(pipelineFile)
+		if err != nil {
+			return err
+		}
+		if exists {
+			return fmt.Errorf("Cannot clone git repository to %s as the dir already exists. Maybe try 'cd %s' and re-run the 'jx boot' command?", repo, repo)
+		}
+
+		log.Logger().Infof("\ncloning: %s to directory: %s\n", info(gitURL), info(cloneDir))
+
+		err = os.MkdirAll(cloneDir, util.DefaultWritePermissions)
+		if err != nil {
+			return errors.Wrapf(err, "failed to create directory: %s", cloneDir)
+		}
+
+		err = o.Git().Clone(gitURL, cloneDir)
+		if err != nil {
+			return errors.Wrapf(err, "failed to clone git URL %s to directory: %s", gitURL, cloneDir)
+		}
+
+		o.Dir, err = filepath.Abs(cloneDir)
+		if err != nil {
+			return err
+		}
+
+		projectConfig, pipelineFile, err = config.LoadProjectConfig(o.Dir)
+		if err != nil {
+			return err
+		}
+		exists, err = util.FileExists(pipelineFile)
+		if err != nil {
+			return err
+		}
+
+		if !exists {
+			return fmt.Errorf("The cloned repository %s does not include a Jenkins X Pipeline file at %s", gitURL, pipelineFile)
+		}
+	}
+
 	requirements, requirementsFile, err := config.LoadRequirementsConfig(o.Dir)
 	if err != nil {
 		return err
 	}
-	exists, err := util.FileExists(requirementsFile)
+	exists, err = util.FileExists(requirementsFile)
 	if err != nil {
 		return err
 	}
@@ -83,22 +167,13 @@ func (o *BootOptions) Run() error {
 	if err != nil {
 		return err
 	}
-	projectConfig, pipelineFile, err := config.LoadProjectConfig(o.Dir)
-	if err != nil {
-		return err
-	}
-	exists, err = util.FileExists(requirementsFile)
-	if err != nil {
-		return err
-	}
-	if !exists {
-		return fmt.Errorf("No pipeline file %s are you sure you are running this command inside a GitOps clone?", pipelineFile)
-	}
 
 	log.Logger().Infof("booting up Jenkins X\n")
 
 	// now lets really boot
 	_, so := create.NewCmdStepCreateTaskAndOption(o.CommonOptions)
+	so.CloneDir = o.Dir
+	so.CloneDir = o.Dir
 	so.InterpretMode = true
 	so.NoReleasePrepare = true
 	so.AdditionalEnvVars = map[string]string{
@@ -112,7 +187,6 @@ func (o *BootOptions) Run() error {
 	// if we can find the deploy namespace lets switch kubernetes context to it so the user can use `jx` commands immediately
 	ns := FindBootNamespace(projectConfig, requirements)
 	if ns != "" {
-		info := util.ColorInfo
 		no := &namespace.NamespaceOptions{}
 		no.CommonOptions = o.CommonOptions
 		no.Args = []string{ns}
