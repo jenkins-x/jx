ead10bca729e94c9374ebf60891aff732e7416e6
Pete Muir <pmuir@bleepbleep.org.uk>
2019-07-01 10:14:36 +0100

    chore: replace updatebot with jx step create pr

--- jx/scripts/update-bot.sh
+++ jx/scripts/update-bot.sh
@@ -6,8 +6,8 @@ set -o pipefail
 
 # Update other repo's dependencies on jx to use the new version - updates repos as specified at .updatebot.yml
 updatebot push-version --kind brew jx $VERSION
-updatebot push-version --kind docker JX_VERSION $VERSION
-updatebot push-version --kind helm jx $VERSION
-updatebot push-regex -r "\s*release = \"(.*)\"" -v $VERSION config.toml
-updatebot push-regex -r "JX_VERSION=(.*)" -v $VERSION install-jx.sh
-updatebot push-regex -r "\s*jxTag:\s*(.*)" -v $VERSION prow/values.yaml
+../../../build/linux/jx step create pr docker --name JX_VERSION --version $VERSION
+../../../build/linux/jx step create pr chart --name jx --version $VERSION
+../../../build/linux/jx step create pr regex --regex "\s*release = \"(.*)\"" --version $VERSION --files config.toml
+../../../build/linux/jx step create pr regex --regex "JX_VERSION=(.*)" --version $VERSION --files install-jx.sh
+../../../build/linux/jx step create pr regex --regex "\s*jxTag:\s*(.*)" --version $VERSION --files prow/values.yaml
