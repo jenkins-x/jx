e4e19adcb489f407a3cfda301b8c982d9cf7bb97
Hardy Ferentschik <hardy@ferentschik.de>
2019-07-02 15:54:33 +0200

    fix(test): making sure original namespace is reset in step verify preinstall test
    fixes issue #4583

--- pkg/cmd/step/verify/step_verify_preinstall_integration_test.go
+++ pkg/cmd/step/verify/step_verify_preinstall_integration_test.go
@@ -1,6 +1,10 @@
+// +build integration
+
 package verify_test
 
 import (
+	"github.com/jenkins-x/jx/pkg/cmd/clients"
+	"github.com/jenkins-x/jx/pkg/cmd/namespace"
 	"github.com/jenkins-x/jx/pkg/tests"
 	"os"
 	"path/filepath"
@@ -21,18 +25,18 @@ const (
 
 func TestStepVerifyPreInstallTerraformKaniko(t *testing.T) {
 	tests.Retry(t, 5, time.Second*10, func(r *tests.R) {
-		t.Parallel()
-
 		options := createTestStepVerifyPreInstallOptions(filepath.Join("test_data", "preinstall", "terraform_kaniko"))
-		err := options.Run()
 
+		_, origNamespace, err := options.KubeClientAndDevNamespace()
+		assert.NoError(t, err)
+		defer resetNamespace(t, origNamespace)
+
+		err = options.Run()
 		assert.Errorf(r, err, "the command should have failed for terraform and kaniko with a missing kaniko secret")
 	})
 }
 
 func TestStepVerifyPreInstallNoKanikoNoLazyCreate(t *testing.T) {
-	t.Parallel()
-
 	// TODO the fake k8s client always seems to lazily create a namespace on demand so the 'jx step verify preinstall' never fails
 	t.SkipNow()
 
@@ -40,21 +44,26 @@ func TestStepVerifyPreInstallNoKanikoNoLazyCreate(t *testing.T) {
 	// explicitly disable lazy create
 	options.LazyCreateFlag = "false"
 
-	err := options.Run()
+	_, origNamespace, err := options.KubeClientAndDevNamespace()
+	assert.NoError(t, err)
+	defer resetNamespace(t, origNamespace)
+
+	err = options.Run()
 	if err != nil {
 		t.Logf("returned error: %s", err.Error())
 	}
-
 	assert.Errorf(t, err, "the command should have failed due to missing namespace")
 }
 
 func TestStepVerifyPreInstallNoKanikoLazyCreate(t *testing.T) {
-	t.Parallel()
-
 	options := createTestStepVerifyPreInstallOptions(filepath.Join("test_data", "preinstall", "no_kaniko_or_terraform"))
-	// we default to lazy create if not using terraform
-	err := options.Run()
 
+	_, origNamespace, err := options.KubeClientAndDevNamespace()
+	assert.NoError(t, err)
+	defer resetNamespace(t, origNamespace)
+
+	// we default to lazy create if not using terraform
+	err = options.Run()
 	assert.NoErrorf(t, err, "the command should not have failed as we should have lazily created the deploy namespace")
 }
 
@@ -70,4 +79,15 @@ func createTestStepVerifyPreInstallOptions(dir string) *verify.StepVerifyPreInst
 	options.Err = os.Stdout
 	options.Out = os.Stdout
 	return options
+}
+
+func resetNamespace(t *testing.T, ns string) {
+	commonOpts := opts.NewCommonOptionsWithFactory(clients.NewFactory())
+	commonOpts.Out = os.Stdout
+	namespaceOptions := &namespace.NamespaceOptions{
+		CommonOptions: &commonOpts,
+	}
+	namespaceOptions.Args = []string{ns}
+	err := namespaceOptions.Run()
+	assert.NoError(t, err)
 }
