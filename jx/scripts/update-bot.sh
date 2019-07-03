ff2aed416290e59516f4b9a555316a930061b285
Cai Cooper <caicooper82@gmail.com>
2019-07-03 14:32:28 +0000

    feat: Brew package version update using new create pullrequest [command]
    Aliases:
    pullrequest, pr
    Available Commands:
      step create pullrequest chart        Creates a Pull Request on a git repository updating the Chart
      step create pullrequest docker       Creates a Pull Request on a git repository updating the docker file
      step create pullrequest regex        Creates a Pull Request on a git repository, doing an update using the provided regex
      step create pullrequest repositories Creates a Pull Request on a 'jx boot' git repository to mirror all the SourceRepository CRDs into the repositories Chart
      step create pullrequest versions     Creates a Pull Request on the versions git repository for a new version of a chart/package
    
    Locally Available Commands:
      release version                      /home/developer/go-workspace/bin/jx-release-version (from plugin)
    
    Usage:
      jx step create pullrequest [flags] [options]
    Use "jx <command> --help" for more information about a given command.
    Use "jx options" for a list of global command-line options (applies to all commands). command
    
    Signed-off-by: Cai Cooper <caicooper82@gmail.com>

--- jx/scripts/update-bot.sh
+++ jx/scripts/update-bot.sh
@@ -4,6 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+CHECKSUMS="./dist/jx-checksums.txt"
+
+if [ -f $CHECKSUMS ]
+then
+  SHA256=$(grep 'darwin' $CHECKSUMS | cut -d' ' -f1)
+  if [ ! -z $SHA256 ]
+  then
+    ./build/linux/jx step create pr brew --version $VERSION --sha $SHA256 --repo https://github.com/jenkins-x/homebrew-jx.git
+  fi
+fi
+
 ./build/linux/jx step create pr docker --name JX_VERSION --version $VERSION --repo https://github.com/jenkins-x/jenkins-x-builders.git --repo https://github.com/jenkins-x/jenkins-x-serverless.git --repo https://github.com/jenkins-x/dev-env-base.git
 ./build/linux/jx step create pr chart --name jx --version $VERSION  --repo https://github.com/jenkins-x/jenkins-x-platform.git
 ./build/linux/jx step create pr regex --regex "\s*release = \"(.*)\"" --version $VERSION --files config.toml --repo https://github.com/jenkins-x/jx-docs.git
