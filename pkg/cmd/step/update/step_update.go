79f4d8ef75f4e89b495d8759dc79a8b8626ef2c5
Cai Cooper <caicooper82@gmail.com>
2019-07-09 15:37:36 +0000

    wip: adding update release step for github releases
    Signed-off-by: Cai Cooper <caicooper82@gmail.com>

---
+++ pkg/cmd/step/update/step_update.go
@@ -0,0 +1,40 @@
+package update
+
+import (
+	"github.com/jenkins-x/jx/pkg/cmd/helper"
+	"github.com/jenkins-x/jx/pkg/cmd/opts"
+	"github.com/spf13/cobra"
+)
+
+// NewCmdStepUpdate Steps a command object for the "step" command
+func NewCmdStepUpdate(commonOpts *opts.CommonOptions) *cobra.Command {
+	options := &opts.StepUpdateOptions{
+		StepOptions: opts.StepOptions{
+			CommonOptions: commonOpts,
+		},
+	}
+
+	cmd := &cobra.Command{
+		Use:   "update",
+		Short: "update [command]",
+		Run: func(cmd *cobra.Command, args []string) {
+			options.Cmd = cmd
+			options.Args = args
+			err := options.Run()
+			helper.CheckErr(err)
+		},
+	}
+
+	//cmd.AddCommand(pr.NewCmdStepCreatePr(commonOpts))
+	return cmd
+}
+
+//StepUpdateCommand is the options for NewCmdStepUpdate
+type StepUpdateCommand struct {
+	opts.StepUpdateOptions
+}
+
+// Run implements this command
+func (o *StepUpdateCommand) Run() error {
+	return o.Cmd.Help()
+}
