7307f8d5526747e4634a745fe525543cf5e34923
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-07-14 10:52:10 +0100

    chore: fmt
    Signed-off-by: Gareth Evans <gareth@bryncynfelin.co.uk>

--- pkg/cmd/create/install.go
+++ pkg/cmd/create/install.go
@@ -2249,7 +2249,7 @@ func (options *InstallOptions) ConfigureKaniko() error {
 		}
 
 		serviceAccountName := naming.ToValidNameTruncated(fmt.Sprintf("%s-ko", clusterName), 30)
-        log.Logger().Infof("Configuring Kaniko service account %s for project %s", util.ColorInfo(serviceAccountName), util.ColorInfo(projectID))
+		log.Logger().Infof("Configuring Kaniko service account %s for project %s", util.ColorInfo(serviceAccountName), util.ColorInfo(projectID))
 		serviceAccountPath, err := options.GCloud().GetOrCreateServiceAccount(serviceAccountName, projectID, serviceAccountDir, gke.KanikoServiceAccountRoles)
 		if err != nil {
 			return errors.Wrap(err, "creating the service account")
