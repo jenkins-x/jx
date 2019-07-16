aea39e3ac2db518de1d8c01586f5ce6a9b688227
Pete Muir <pmuir@bleepbleep.org.uk>
2019-07-16 09:52:36 +0100

    fix: dedupe and collapse dependency updates
    * Do this for both the markdown and the yaml
    * Add support for collapsing multiple updates for the same version into
      one
    
    Fixes #4715 and #4716

---
+++ pkg/cmd/step/step_changelog_test.go
@@ -0,0 +1,46 @@
+package step_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/ghodss/yaml"
+	v1 "github.com/jenkins-x/jx/pkg/apis/jenkins.io/v1"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/jenkins-x/jx/pkg/cmd/step"
+)
+
+func TestCollapseDependencyUpdates(t *testing.T) {
+	type args struct {
+		data string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "platform",
+			args: args{
+				data: "test_data/changelog/platform.yaml",
+			},
+			want: "test_data/changelog/platform.golden.yaml",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ups := make([]v1.DependencyUpdate, 0)
+			data, err := ioutil.ReadFile(tt.args.data)
+			assert.NoError(t, err)
+			err = yaml.Unmarshal(data, &ups)
+			assert.NoError(t, err)
+			out := step.CollapseDependencyUpdates(ups)
+			dataOut, err := yaml.Marshal(out)
+			assert.NoError(t, err)
+			golden, err := ioutil.ReadFile(tt.want)
+			assert.NoError(t, err)
+			assert.Equal(t, string(golden), string(dataOut))
+		})
+	}
+}
