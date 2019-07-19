e6a216a747e224fe71bcd55255c8994358676432
Jose Armesto <github@armesto.net>
2019-07-19 12:19:21 +0200

    chore: test bdd tests

--- jx/scripts/ci.sh
+++ jx/scripts/ci.sh
@@ -51,7 +51,7 @@ git config --global --add user.name JenkinsXBot
 git config --global --add user.email jenkins-x@googlegroups.com
 
 # lets trigger the BDD tests in a new team and git provider
-jx step bdd -b --provider=gke --git-provider=ghe --git-provider-url=https://github.beescloud.com --git-username dev1 --git-api-token $GHE_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring
+GIT_ORGANISATION="jenkins-x-tests" jx step bdd -b --test-git-repo=https://github.com/fiunchinho/bdd-jx.git --provider=gke --git-provider=ghe --git-provider-url=https://github.beescloud.com --git-username dev1 --git-api-token $GHE_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring
 
 # Reset the namespace back to jx after test for any followup steps
 COVER_JX_BINARY=false jx ns jx
