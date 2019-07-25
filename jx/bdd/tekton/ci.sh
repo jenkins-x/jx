81429623c81e44a8cceda4776e29d6c1c052cd84
Andrew Bayer <andrew.bayer@gmail.com>
2019-07-25 14:23:07 -0400

    chore(tekton): Add BDD test using jenkins-x.yml to Tekton context
    This won't pass until https://github.com/jenkins-x/bdd-jx/pull/54 is
    merged, but I'm going to be out the next few days, so might as well
    get this queued up. =)
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- jx/bdd/tekton/ci.sh
+++ jx/bdd/tekton/ci.sh
@@ -36,5 +36,4 @@ git config --global --add user.name JenkinsXBot
 git config --global --add user.email jenkins-x@googlegroups.com
 
 echo "running the BDD tests with JX_HOME = $JX_HOME"
-jx step bdd --versions-repo https://github.com/jenkins-x/jenkins-x-versions.git --config jx/bdd/tekton/cluster.yaml --gopath /tmp  --git-provider=github --git-username $GH_USERNAME --git-owner $GH_OWNER --git-api-token $GH_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tekton --tests install --tests test-create-spring --tests test-quickstart-golang-http
-
+jx step bdd --versions-repo https://github.com/jenkins-x/jenkins-x-versions.git --config jx/bdd/tekton/cluster.yaml --gopath /tmp  --git-provider=github --git-username $GH_USERNAME --git-owner $GH_OWNER --git-api-token $GH_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tekton --tests install --tests test-create-spring --tests test-quickstart-golang-http --tests test-import-golang-http-from-jenkins-x-yml
