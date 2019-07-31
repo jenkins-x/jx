df40cbdb8b70a36cd646c36f9e267e8960523e0d
Andrew Bayer <andrew.bayer@gmail.com>
2019-07-31 16:16:02 -0400

    chore: Add jenkins-x-builders-ml to update-bot.sh
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- jx/scripts/update-bot.sh
+++ jx/scripts/update-bot.sh
@@ -15,7 +15,7 @@ then
   fi
 fi
 
-./build/linux/jx step create pr docker --name JX_VERSION --version $VERSION --repo https://github.com/jenkins-x/jenkins-x-builders.git --repo https://github.com/jenkins-x/jenkins-x-serverless.git --repo https://github.com/jenkins-x/dev-env-base.git
+./build/linux/jx step create pr docker --name JX_VERSION --version $VERSION --repo https://github.com/jenkins-x/jenkins-x-builders.git --repo https://github.com/jenkins-x/jenkins-x-serverless.git --repo https://github.com/jenkins-x/dev-env-base.git --repo https://github.com/jenkins-x/jenkins-x-builders-ml.git
 ./build/linux/jx step create pr chart --name jx --version $VERSION  --repo https://github.com/jenkins-x/jenkins-x-platform.git
 ./build/linux/jx step create pr regex --regex "\s*release = \"(.*)\"" --version $VERSION --files config.toml --repo https://github.com/jenkins-x/jx-docs.git
 ./build/linux/jx step create pr regex --regex "JX_VERSION=(.*)" --version $VERSION --files install-jx.sh --repo https://github.com/jenkins-x/jx-tutorial.git
