c50f7bfd6e4a5676f0174556d2499e64b0d457b2
Andrew Bayer <andrew.bayer@gmail.com>
2019-08-01 12:31:52 -0400

    fix(tekton): Include logs for builds with just init containers running
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- pkg/tekton/pipelines_test.go
+++ pkg/tekton/pipelines_test.go
@@ -116,3 +116,42 @@ func TestPipelineRunIsNotPendingWaitingSteps(t *testing.T) {
 
 	assert.False(t, tekton.PipelineRunIsNotPending(pr))
 }
+
+func TestPipelineRunIsNotPendingWaitingStepsInPodInitializing(t *testing.T) {
+	taskRunStatusMap := make(map[string]*v1alpha1.PipelineRunTaskRunStatus)
+	taskRunStatusMap["faketaskrun"] = &v1alpha1.PipelineRunTaskRunStatus{
+		Status: &v1alpha1.TaskRunStatus{
+			Steps: []v1alpha1.StepState{{
+				ContainerState: corev1.ContainerState{
+					Waiting: &corev1.ContainerStateWaiting{
+						Reason: "PodInitializing",
+					},
+				},
+			}},
+		},
+	}
+
+	pr := &v1alpha1.PipelineRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "PR1",
+			Namespace: ns,
+			Labels: map[string]string{
+				tekton.LabelRepo:    "fakerepo",
+				tekton.LabelBranch:  "fakebranch",
+				tekton.LabelOwner:   "fakeowner",
+				tekton.LabelContext: "fakecontext",
+			},
+		},
+		Spec: v1alpha1.PipelineRunSpec{
+			Params: []v1alpha1.Param{
+				{Name: "version", Value: "v1"},
+				{Name: "build_id", Value: "1"},
+			},
+		},
+		Status: v1alpha1.PipelineRunStatus{
+			TaskRuns: taskRunStatusMap,
+		},
+	}
+
+	assert.True(t, tekton.PipelineRunIsNotPending(pr))
+}
