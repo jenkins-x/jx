83c43f1c2f0e8c179930cd1b8b04dfd02a71974b
Mark Cox <markcox20@hotmail.com>
2019-08-07 15:29:59 +0000

    fix: test for empty validname
    Signed-off-by: Mark Cox <markcox20@hotmail.com>

--- pkg/kube/naming/names_valid_test.go
+++ pkg/kube/naming/names_valid_test.go
@@ -15,6 +15,7 @@ func TestToValidName(t *testing.T) {
 	assertToValidName(t, "foo-bar-0.1.0", "foo-bar-0-1-0")
 	assertToValidName(t, "---foo-bar-", "foo-bar")
 	assertToValidName(t, "foo/bar_*123", "foo-bar-123")
+	assertToValidName(t, "", "")
 }
 
 func TestToValidNameWithDots(t *testing.T) {
