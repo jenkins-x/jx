cf7adff0ce59ce1ffef49087d0bb485d8d4b86b0
Andrew Bayer <andrew.bayer@gmail.com>
2019-08-20 12:42:54 -0400

    fix: Fix values_tree tests that needed a vault client
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- pkg/helm/values_tree_test.go
+++ pkg/helm/values_tree_test.go
@@ -3,16 +3,19 @@ package helm_test
 import (
 	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 	"testing"
 
 	"github.com/jenkins-x/jx/pkg/config"
 	"github.com/jenkins-x/jx/pkg/helm"
+	"github.com/jenkins-x/jx/pkg/secreturl/localvault"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestValuesTree(t *testing.T) {
 	t.Parallel()
+	vaultClient := localvault.NewFileSystemClient(path.Join("test_data", "local_vault_files"))
 	dir, err := createFiles(map[string]string{
 		"cheese/values.yaml": "foo: bar",
 		"meat/ham/values.yaml": `foo: 
@@ -30,13 +33,14 @@ meat:
 		assert.NoError(t, err)
 	}()
 	assert.NoError(t, err)
-	result, _, err := helm.GenerateValues(config.NewRequirementsConfig(), nil, dir, nil, true, nil)
+	result, _, err := helm.GenerateValues(config.NewRequirementsConfig(), nil, dir, nil, true, vaultClient)
 	assert.NoError(t, err)
 	assert.Equal(t, expectedOutput, string(result))
 }
 
 func TestValuesTreeWithExistingFile(t *testing.T) {
 	t.Parallel()
+	vaultClient := localvault.NewFileSystemClient(path.Join("test_data", "local_vault_files"))
 	dir, err := createFiles(map[string]string{
 		"values.yaml":        "people: pete",
 		"cheese/values.yaml": "foo: bar",
@@ -56,13 +60,14 @@ people: pete
 		assert.NoError(t, err)
 	}()
 	assert.NoError(t, err)
-	result, _, err := helm.GenerateValues(config.NewRequirementsConfig(), nil, dir, nil, true, nil)
+	result, _, err := helm.GenerateValues(config.NewRequirementsConfig(), nil, dir, nil, true, vaultClient)
 	assert.NoError(t, err)
 	assert.Equal(t, expectedOutput, string(result))
 }
 
 func TestValuesTreeWithFileRefs(t *testing.T) {
 	t.Parallel()
+	vaultClient := localvault.NewFileSystemClient(path.Join("test_data", "local_vault_files"))
 	dir, err := createFiles(map[string]string{
 		"milk/values.yaml": `foo:
   bar:
@@ -85,7 +90,7 @@ func TestValuesTreeWithFileRefs(t *testing.T) {
 		assert.NoError(t, err)
 	}()
 	assert.NoError(t, err)
-	result, _, err := helm.GenerateValues(config.NewRequirementsConfig(), nil, dir, nil, true, nil)
+	result, _, err := helm.GenerateValues(config.NewRequirementsConfig(), nil, dir, nil, true, vaultClient)
 	assert.NoError(t, err)
 	assert.Equal(t, expectedOutput, string(result))
 }
