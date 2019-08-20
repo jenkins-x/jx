0ad83fa4a9d7c483945c9008970aeb6b8e8a4968
Mark Nielsen <mark.nielsen@blackboard.com>
2019-08-20 15:28:01 -0700

    fix(init): enable both http and https for NLB ingress
    When using kops or EKS, NLB appears to now support handling
    both HTTP and HTTPS traffic.
    
    Signed-off-by: Mark Nielsen <mark.nielsen@blackboard.com>

--- pkg/cmd/initcmd/init.go
+++ pkg/cmd/initcmd/init.go
@@ -516,20 +516,6 @@ func (o *InitOptions) InitIngress() error {
 			return errors.Wrap(err, "failed to append the myvalues file")
 		}
 		if o.Flags.Provider == cloud.AWS || o.Flags.Provider == cloud.EKS {
-			// For EKS enable both ports for NLBs to be able to use TLS on Nginx ingresses
-			// Fix for https://github.com/jenkins-x/jx/issues/3079
-			enableHTTP := "true"
-			enableHTTPS := "true"
-
-			// For AWS we can only enable one port for NLBs right now?
-			if o.Flags.Provider == cloud.AWS {
-				enableHTTP = "false"
-				enableHTTPS = "true"
-				if o.Flags.Http {
-					enableHTTP = "true"
-					enableHTTPS = "false"
-				}
-			}
 			yamlText := `---
 rbac:
  create: true
@@ -538,8 +524,8 @@ controller:
  service:
    annotations:
      service.beta.kubernetes.io/aws-load-balancer-type: nlb
-   enableHttp: ` + enableHTTP + `
-   enableHttps: ` + enableHTTPS + `
+   enableHttp: true
+   enableHttps: true
 `
 
 			f, err := ioutil.TempFile("", "ing-values-")
