008a6443651caebbcc87afe2e9a7e54b0044fe3f
Hardy Ferentschik <hardy@ferentschik.de>
2019-08-29 09:27:36 +0200

    chore: typo fix in function docs for ShouldForkForPullRequest

--- pkg/gits/interface.go
+++ pkg/gits/interface.go
@@ -160,7 +160,7 @@ type GitProvider interface {
 	// TODO Refactor to remove bespoke types when we implement another provider
 	AcceptInvitation(int64) (*github.Response, error)
 
-	// ShouldForkForPullReques treturns true if we should create a personal fork of this repository
+	// ShouldForkForPullRequest returns true if we should create a personal fork of this repository
 	// before creating a pull request
 	ShouldForkForPullRequest(originalOwner string, repoName string, username string) bool
 
