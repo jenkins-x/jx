1b358d60ec367f1f33a23696f7e7a5b23fda5428
James Strachan <james.strachan@gmail.com>
2019-08-30 17:52:13 +0100

    fix: refactor the loading of quickstarts
    into a common function across `jx create|get quickstarts`
    
    Signed-off-by: James Strachan <james.strachan@gmail.com>
    
    #5283

--- pkg/cmd/get/get_quickstarts.go
+++ pkg/cmd/get/get_quickstarts.go
@@ -2,20 +2,14 @@ package get
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/jenkins-x/jx/pkg/cmd/helper"
-	"github.com/jenkins-x/jx/pkg/log"
-
-	v1 "github.com/jenkins-x/jx/pkg/apis/jenkins.io/v1"
-	"github.com/jenkins-x/jx/pkg/gits"
 	"github.com/jenkins-x/jx/pkg/quickstarts"
 
 	"github.com/spf13/cobra"
 
 	"github.com/jenkins-x/jx/pkg/cmd/opts"
 	"github.com/jenkins-x/jx/pkg/cmd/templates"
-	"github.com/jenkins-x/jx/pkg/kube"
 )
 
 //GetQuickstartsOptions -  the command line options
@@ -24,6 +18,7 @@ type GetQuickstartsOptions struct {
 	GitHubOrganisations []string
 	Filter              quickstarts.QuickstartFilter
 	ShortFormat         bool
+	IgnoreTeam          bool
 }
 
 var (
@@ -74,65 +69,9 @@ func NewCmdGetQuickstarts(commonOpts *opts.CommonOptions) *cobra.Command {
 
 // Run implements this command
 func (o *GetQuickstartsOptions) Run() error {
-	var locations []v1.QuickStartLocation
-	jxClient, ns, err := o.JXClientAndDevNamespace()
+	model, err := o.LoadQuickstartsModel(o.GitHubOrganisations, o.IgnoreTeam)
 	if err != nil {
-		return err
-	}
-
-	locations, err = kube.GetQuickstartLocations(jxClient, ns)
-	if err != nil {
-		return err
-	}
-
-	// lets add any extra github organisations if they are not already configured
-	for _, org := range o.GitHubOrganisations {
-		found := false
-		for _, loc := range locations {
-			if loc.GitURL == gits.GitHubURL && loc.Owner == org {
-				found = true
-				break
-			}
-		}
-		if !found {
-			locations = append(locations, v1.QuickStartLocation{
-				GitURL:   gits.GitHubURL,
-				GitKind:  gits.KindGitHub,
-				Owner:    org,
-				Includes: []string{"*"},
-				Excludes: []string{"WIP-*"},
-			})
-		}
-	}
-
-	gitMap := map[string]map[string]v1.QuickStartLocation{}
-	for _, loc := range locations {
-		m := gitMap[loc.GitURL]
-		if m == nil {
-			m = map[string]v1.QuickStartLocation{}
-			gitMap[loc.GitURL] = m
-		}
-		m[loc.Owner] = loc
-	}
-
-	model := quickstarts.NewQuickstartModel()
-
-	for gitURL, m := range gitMap {
-		for _, location := range m {
-			kind := location.GitKind
-			if kind == "" {
-				kind = gits.KindGitHub
-			}
-			gitProvider, err := o.GitProviderForGitServerURL(gitURL, kind)
-			if err != nil {
-				return err
-			}
-			log.Logger().Debugf("Searching for repositories in Git server %s owner %s includes %s excludes %s as user %s ", gitProvider.ServerURL(), location.Owner, strings.Join(location.Includes, ", "), strings.Join(location.Excludes, ", "), gitProvider.CurrentUsername())
-			err = model.LoadGithubQuickstarts(gitProvider, location.Owner, location.Includes, location.Excludes)
-			if err != nil {
-				log.Logger().Debugf("Quickstart load error: %s", err.Error())
-			}
-		}
+		return fmt.Errorf("failed to load quickstarts: %s", err)
 	}
 
 	//output list of available quickstarts and exit
