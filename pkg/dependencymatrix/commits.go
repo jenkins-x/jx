67b0788c13178b9a644699446cf82a079386905b
Pete Muir <pmuir@bleepbleep.org.uk>
2019-09-06 16:05:20 +0100

    fix: allow .git to appear in dep update messages
    Signed-off-by: Pete Muir <pmuir@bleepbleep.org.uk>

--- pkg/dependencymatrix/commits.go
+++ pkg/dependencymatrix/commits.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	dependencyUpdateRegex = regexp.MustCompile(`^(?m:chore\((?:deps|dependencies)\): (?:bump|update) (.*) from ([\w\.]*) to ([\w\.]*)$)`)
-	slugLinkRegex         = regexp.MustCompile(`^(?:([\w-]*?)?\/?([\w-]+)|(https?):\/\/([\w\.]*)\/([\w-]*)\/([\w-]*))(?::([\w-]*))?$`)
+	slugLinkRegex         = regexp.MustCompile(`^(?:([\w-]*?)?\/?([\w-]+)|(https?):\/\/([\w\.]*)\/([\w-]*)\/([\w-]*)(?:\.git)?)(?::([\w-]*))?$`)
 	//slugLinkRegex = regexp.MustCompile(``)
 	slugRegex = regexp.MustCompile(`^(\w*)?\/(\w*)$`)
 )
