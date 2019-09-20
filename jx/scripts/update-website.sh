5329a546c58a2218d49904e98fc5fca1edf7ee90
Andrew Bayer <andrew.bayer@gmail.com>
2019-09-20 15:54:04 -0400

    fix: Proper relative path for jx binary in update-website.sh
    More parent directories than there used to be!
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- jx/scripts/update-website.sh
+++ jx/scripts/update-website.sh
@@ -8,7 +8,7 @@ echo "updating the CLI reference"
 git clone https://github.com/jenkins-x/jx-docs.git
 
 pushd jx-docs/content/en/docs/reference/commands
-  ../../../build/linux/jx create docs
+  ../../../../../../build/linux/jx create docs
   git config credential.helper store
   git add *
   git commit --allow-empty -a -m "updated jx commands & API docs from $VERSION"
