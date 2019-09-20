302f92f4a9ae2e09d7ac2ff46aecbd18cc7aae2c
James Strachan <james.strachan@gmail.com>
2019-09-20 10:22:55 +0100

    fix: use the new website path for docs creation
    Signed-off-by: James Strachan <james.strachan@gmail.com>

--- jx/scripts/update-website.sh
+++ jx/scripts/update-website.sh
@@ -7,7 +7,7 @@ set -o pipefail
 echo "updating the CLI reference"
 git clone https://github.com/jenkins-x/jx-docs.git
 
-pushd jx-docs/content/commands
+pushd jx-docs/content/en/docs/reference/commands
   ../../../build/linux/jx create docs
   git config credential.helper store
   git add *
