e4abddfe06fc88ed13b68a57e35cfd0401a44604
Hardy Ferentschik <hardy@ferentschik.de>
2019-09-27 12:16:27 +0200

    chore(test): making boot-vault context gitops enabled
    fixes #5638

--- jx/bdd/boot-vault/ci.sh
+++ jx/bdd/boot-vault/ci.sh
@@ -69,4 +69,4 @@ jx step bdd \
     --default-admin-password $JENKINS_PASSWORD \
     --no-delete-app \
     --no-delete-repo \
-    --tests test-quickstart-golang-http
+    --tests test-quickstart-golang-http 
