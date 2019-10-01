c5a3c4f85cff2b99a087786d7857f65768b6dc51
Will Refvem <wbrefvem@gmail.com>
2019-10-01 13:04:02 -0400

    feat(versionstreams): add some more test cases and tweak some existing ones.
    Signed-off-by: Will Refvem <wbrefvem@gmail.com>

--- pkg/versionstream/versionstreamrepo/gitrepo_integration_test.go
+++ pkg/versionstream/versionstreamrepo/gitrepo_integration_test.go
@@ -8,14 +8,62 @@ import (
 	"github.com/jenkins-x/jx/pkg/gits"
 	"github.com/jenkins-x/jx/pkg/versionstream/versionstreamrepo"
 	"github.com/stretchr/testify/assert"
+
+	v1 "github.com/jenkins-x/jx/pkg/apis/jenkins.io/v1"
 )
 
 const (
-	RepoURL    = "https://github.com/jenkins-x/jenkins-x-versions"
-	VersionRef = "v1.0.90"
+	RepoURL    = "https://github.com/jenkins-x/jenkins-x-versions-test.git"
+	VersionRef = "v1.0.103"
 	BranchRef  = "master"
 	HEAD       = "HEAD"
+	CommitRef  = "ea16897b7b12f08708c307de49ff21b2a197517c"
 )
+
+func TestCloneJXVersionsRepoWithNoURL(t *testing.T) {
+	gitter := gits.NewGitCLI()
+	dir, versionRef, err := versionstreamrepo.CloneJXVersionsRepo(
+		"",
+		VersionRef,
+		nil,
+		gitter,
+		true,
+		false,
+		nil,
+		nil,
+		nil,
+	)
+
+	// Pull the latest tag so that we know the correct expected verion ref.
+	tag, _, err := gitter.Describe(dir, false, VersionRef, "")
+
+	assert.NoError(t, err)
+	assert.NotNil(t, dir)
+	assert.NotNil(t, versionRef)
+	assert.Equal(t, tag, versionRef)
+}
+
+func TestCloneJXVersionsRepoWithTeamSettings(t *testing.T) {
+	settings := &v1.TeamSettings{
+		VersionStreamURL: RepoURL,
+		VersionStreamRef: VersionRef,
+	}
+	dir, versionRef, err := versionstreamrepo.CloneJXVersionsRepo(
+		"",
+		"",
+		settings,
+		gits.NewGitCLI(),
+		true,
+		false,
+		nil,
+		nil,
+		nil,
+	)
+	assert.NoError(t, err)
+	assert.NotNil(t, dir)
+	assert.NotNil(t, versionRef)
+	assert.Equal(t, VersionRef, versionRef)
+}
 
 func TestCloneJXVersionsRepoWithATag(t *testing.T) {
 	dir, versionRef, err := versionstreamrepo.CloneJXVersionsRepo(
@@ -50,12 +98,13 @@ func TestCloneJXVersionsRepoWithABranch(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, dir)
 	assert.NotNil(t, versionRef)
+	assert.Equal(t, VersionRef, versionRef)
 }
 
 func TestCloneJXVersionsRepoWithACommit(t *testing.T) {
 	dir, versionRef, err := versionstreamrepo.CloneJXVersionsRepo(
 		RepoURL,
-		HEAD,
+		CommitRef,
 		nil,
 		gits.NewGitCLI(),
 		true,
@@ -67,4 +116,5 @@ func TestCloneJXVersionsRepoWithACommit(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, dir)
 	assert.NotNil(t, versionRef)
+	assert.Equal(t, VersionRef, versionRef)
 }
