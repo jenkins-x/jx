15c929248ffc48c0767e43cdf044b20525f36b6a
Sajid Moinuddin <sajid.moinuddin@kayosports.com.au>
2019-10-05 08:26:32 +1000

    Jenkins-x builder

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,38 @@
+pipeline {
+  agent {
+    label "jenkins-maven"
+  }
+  environment {
+    CHART_REPOSITORY= 'https://artifactory.cluster.foxsports-gitops-prod.com.au/artifactory/helm' // Please do not edit this line! Managed by customize.sh
+    ORG = 'fsa-streamotion' 
+    APP_NAME = 'fsa-streamotion-jx'
+  }
+
+  stages {
+    stage('Build Release') {
+      steps {
+        container('maven') {
+              // ensure we're not on a detached head
+              sh "git config --global credential.helper store"
+              sh "jx step git credentials"
+
+              sh "echo \$(jx-release-version) > VERSION"
+              sh "jx step tag --version \$(cat VERSION)"
+
+              sh "export VERSION=`cat VERSION` && skaffold build -f skaffold.yaml"
+
+              script {
+                def buildVersion =  readFile "${env.WORKSPACE}/VERSION"
+                currentBuild.description = "$buildVersion"
+                currentBuild.displayName = "$buildVersion"
+              }          
+        }
+      }
+    }
+  }
+  post {
+        always {
+          cleanWs()
+        }
+  }
+}
