ebe3d9ae13064e4d732f09d9d5d8f0bf72ee7914
Sajid Moinuddin <sajid.moinuddin@kayosports.com.au>
2019-10-05 10:35:13 +1000

    also publish to dockerhub

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,48 +8,10 @@ pipeline {
     APP_NAME = 'fsa-streamotion-jx'
   }
 
-  stages {
-    stage('Build Release') {
-      steps {
-        container('maven') {
-              // ensure we're not on a detached head
-              sh "git config --global credential.helper store"
-              sh "jx step git credentials"
 
-              sh "echo \$(jx-release-version) > VERSION"
-              sh "jx step tag --version \$(cat VERSION)"
 
-              sh "export VERSION=`cat VERSION` && skaffold build -f skaffold.yaml"
 
-              script {
-                def buildVersion =  readFile "${env.WORKSPACE}/VERSION"
-                currentBuild.description = "$buildVersion"
-                currentBuild.displayName = "$buildVersion"
-              }          
-        }
-    }}
   
-
-    stage('Build Release') {
-        agent {
-          label "dockerhub-maven"
-        }
-
-        steps {
-          container('maven') {
-                sh "export VERSION=`cat VERSION` && skaffold build -f skaffold-dockerhub.yaml"
-
-                script {
-                  def buildVersion =  readFile "${env.WORKSPACE}/VERSION"
-                  currentBuild.description = "$buildVersion"
-                  currentBuild.displayName = "$buildVersion"
-                }          
-          }
-        }
-      }
-  }
-
-  }
   post {
         always {
           cleanWs()
