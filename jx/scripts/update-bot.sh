74cd175fcfe9100e5acc393f230ff4c2a1e98ca5
Cai Cooper <caicooper82@gmail.com>
2019-10-08 20:18:54 +0000

    chore: refactored update script into pipeline
    Signed-off-by: Cai Cooper <caicooper82@gmail.com>

--- jx/scripts/update-bot.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-
-./build/linux/jx step create pr docker --name JX_VERSION --version $VERSION --repo https://github.com/jenkins-x/jenkins-x-builders.git --repo https://github.com/jenkins-x/jenkins-x-serverless.git --repo https://github.com/jenkins-x/jenkins-x-builders-ml.git
-./build/linux/jx step create pr go --name github.com/jenkins-x/jx --version $VERSION --build "make mod" --repo https://github.com/jenkins-x/lighthouse.git
-./build/linux/jx step create pr go --name github.com/jenkins-x/jx --version $VERSION --build "make build" --repo https://github.com/cloudbees/jxui-backend.git
