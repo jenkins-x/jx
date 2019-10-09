7b4d9d90edd175bf8f74ab20e588daad49eac1b5
Andrew Bayer <andrew.bayer@gmail.com>
2019-10-09 11:51:02 -0400

    fix: Add reqs to TeamSettings in `jx step verify env`
    `jx step verify preinstall` is too early - let's do it in `jx step
    verify env` instead.
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- pkg/cmd/step/verify/step_verify_preinstall_test.go
+++ pkg/cmd/step/verify/step_verify_preinstall_test.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path"
 	"testing"
 	"time"
 
 	"github.com/acarl005/stripansi"
-	v1 "github.com/jenkins-x/jx/pkg/apis/jenkins.io/v1"
 	"github.com/jenkins-x/jx/pkg/cmd/clients/fake"
 	"github.com/jenkins-x/jx/pkg/cmd/opts"
 	"github.com/jenkins-x/jx/pkg/cmd/opts/step"
@@ -17,10 +15,8 @@ import (
 	"github.com/jenkins-x/jx/pkg/config"
 	"github.com/jenkins-x/jx/pkg/log"
 	"github.com/jenkins-x/jx/pkg/tests"
-	"github.com/jenkins-x/jx/pkg/util"
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/yaml"
 )
 
 var timeout = 1 * time.Second
@@ -219,97 +215,6 @@ func Test_abort_private_repos_with_github_provider(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.Equal(t, "cannot continue without completed git requirements", err.Error())
-}
-
-func TestStepVerifyPreInstallOptions_VerifyRequirementsInTeamSettings(t *testing.T) {
-	commonOpts := opts.NewCommonOptionsWithFactory(fake.NewFakeFactory())
-	options := &commonOpts
-	testhelpers.ConfigureTestOptions(options, options.Git(), options.Helm())
-
-	testOptions := &StepVerifyPreInstallOptions{
-		StepVerifyOptions: StepVerifyOptions{
-			StepOptions: step.StepOptions{
-				CommonOptions: options,
-			},
-		},
-	}
-
-	requirementsYamlFile := path.Join("test_data", "preinstall", "no_tls", "jx-requirements.yml")
-	exists, err := util.FileExists(requirementsYamlFile)
-	assert.NoError(t, err)
-	assert.True(t, exists)
-
-	bytes, err := ioutil.ReadFile(requirementsYamlFile)
-	assert.NoError(t, err)
-	requirements := &config.RequirementsConfig{}
-	err = yaml.Unmarshal(bytes, requirements)
-	assert.NoError(t, err)
-
-	err = testOptions.VerifyRequirementsInTeamSettings("jx", requirements)
-	assert.NoError(t, err, "there shouldn't be any error adding the requirements to TeamSettings")
-
-	teamSettings, err := testOptions.TeamSettings()
-	assert.NoError(t, err)
-
-	requirementsCm := teamSettings.BootRequirements
-	assert.NotEqual(t, "", requirementsCm, "the BootRequirements field should be present and not empty")
-
-	mapRequirements, err := config.GetRequirementsConfigFromTeamSettings(teamSettings)
-	assert.NoError(t, err)
-
-	assert.Equal(t, requirements, mapRequirements)
-}
-
-func TestStepVerifyPreInstallOptions_VerifyRequirementsConfigMapWithModification(t *testing.T) {
-	commonOpts := opts.NewCommonOptionsWithFactory(fake.NewFakeFactory())
-	options := &commonOpts
-	testhelpers.ConfigureTestOptions(options, options.Git(), options.Helm())
-
-	requirementsYamlFile := path.Join("test_data", "preinstall", "no_tls", "jx-requirements.yml")
-	exists, err := util.FileExists(requirementsYamlFile)
-	assert.NoError(t, err)
-	assert.True(t, exists)
-
-	bytes, err := ioutil.ReadFile(requirementsYamlFile)
-	assert.NoError(t, err)
-	requirements := &config.RequirementsConfig{}
-	err = yaml.Unmarshal(bytes, requirements)
-	assert.NoError(t, err)
-
-	err = options.ModifyDevEnvironment(func(env *v1.Environment) error {
-		env.Spec.TeamSettings.BootRequirements = string(bytes)
-		return nil
-	})
-	assert.NoError(t, err)
-
-	// We make a modification to the requirements and we should see it when we retrieve the ConfigMap later
-	requirements.Storage.Logs = config.StorageEntryConfig{
-		Enabled: true,
-		URL:     "gs://randombucket",
-	}
-
-	testOptions := &StepVerifyPreInstallOptions{
-		StepVerifyOptions: StepVerifyOptions{
-			StepOptions: step.StepOptions{
-				CommonOptions: options,
-			},
-		},
-	}
-
-	err = testOptions.VerifyRequirementsInTeamSettings("jx", requirements)
-	assert.NoError(t, err, "there shouldn't be any error creating the ConfigMap")
-
-	teamSettings, err := testOptions.TeamSettings()
-	assert.NoError(t, err)
-
-	requirementsCm := teamSettings.BootRequirements
-	assert.NotEqual(t, "", requirementsCm, "the BootRequirements field should be present and not empty")
-
-	mapRequirements, err := config.GetRequirementsConfigFromTeamSettings(teamSettings)
-	assert.NoError(t, err)
-
-	assert.Equal(t, requirements.Storage.Logs, mapRequirements.Storage.Logs, "the change done before calling"+
-		"VerifyRequirementsInTeamSettings should be present in the retrieved configuration")
 }
 
 func TestStepVerifyPreInstallOptions_UpdateVersionStreamRefInDevEnvironment(t *testing.T) {
