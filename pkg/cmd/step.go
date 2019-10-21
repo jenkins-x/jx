008a81e85724e0e44aebd0324686f2e851d23e19
warrenbailey <warren@warrenbailey.net>
2019-10-21 08:21:37 +0100

    chore: remove github app command
    Signed-off-by: warrenbailey <warren@warrenbailey.net>

--- pkg/cmd/step.go
+++ pkg/cmd/step.go
@@ -16,7 +16,6 @@ import (
 	"github.com/jenkins-x/jx/pkg/cmd/step/expose"
 	"github.com/jenkins-x/jx/pkg/cmd/step/get"
 	"github.com/jenkins-x/jx/pkg/cmd/step/git"
-	"github.com/jenkins-x/jx/pkg/cmd/step/github"
 	"github.com/jenkins-x/jx/pkg/cmd/step/helm"
 	"github.com/jenkins-x/jx/pkg/cmd/step/nexus"
 	"github.com/jenkins-x/jx/pkg/cmd/step/post"
@@ -87,7 +86,6 @@ func NewCmdStep(commonOpts *opts.CommonOptions) *cobra.Command {
 	cmd.AddCommand(update.NewCmdStepUpdate(commonOpts))
 	cmd.AddCommand(report.NewCmdStepReport(commonOpts))
 	cmd.AddCommand(step.NewCmdStepOverrideRequirements(commonOpts))
-	cmd.AddCommand(github.NewCmdStepGithub(commonOpts))
 
 	return cmd
 }
