7ae01e5e215861023bbd64d2d523c3e72903a512
James Rawlings <rawlingsj80@gmail.com>
2019-10-22 22:09:30 +0100

    feat: lets add a specific wait-for-chart step so that pipelines can wait until a helm index has been updated to contain the newly released chart
    Signed-off-by: James Rawlings <rawlingsj80@gmail.com>

---
+++ pkg/cmd/step/step_wait_for_chart_test.go
@@ -0,0 +1,51 @@
+package step
+
+import (
+	"testing"
+
+	"github.com/jenkins-x/jx/pkg/cmd/opts"
+
+	"github.com/jenkins-x/jx/pkg/cmd/opts/step"
+
+	cmd_test "github.com/jenkins-x/jx/pkg/cmd/clients/mocks"
+	helm_test "github.com/jenkins-x/jx/pkg/helm/mocks"
+	"github.com/jenkins-x/jx/pkg/kube"
+	uuid "github.com/satori/go.uuid"
+	"k8s.io/helm/pkg/proto/hapi/chart"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStepWaitForChart(t *testing.T) {
+	nameUUID, err := uuid.NewV4()
+	assert.NoError(t, err)
+
+	name := nameUUID.String()
+	version := "0.0.1"
+
+	helmer := helm_test.NewMockHelmer()
+	helm_test.StubFetchChart(name, "", kube.DefaultChartMuseumURL, &chart.Chart{
+		Metadata: &chart.Metadata{
+			Name:    name,
+			Version: version,
+		},
+	}, helmer)
+
+	assert.NoError(t, err)
+
+	mockFactory := cmd_test.NewMockFactory()
+
+	commonOpts := opts.NewCommonOptionsWithFactory(mockFactory)
+	commonOpts.SetHelm(helmer)
+	options := &WaitForChartOptions{
+		ChartName:    name,
+		ChartVersion: version,
+		ChartRepo:    kube.DefaultChartMuseumURL,
+		StepOptions: &step.StepOptions{
+			CommonOptions: &commonOpts,
+		},
+	}
+
+	err = options.Run()
+	assert.NoError(t, err)
+}
