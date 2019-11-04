32a2b12db3b4c0ff1e8d49441d41cecfa772da44
James Rawlings <rawlingsj80@gmail.com>
2019-11-04 19:03:08 +0000

    fix: downstream boot config repo builds as teh GitHubApp.Enablede field is mandatory
    Signed-off-by: James Rawlings <rawlingsj80@gmail.com>

--- pkg/config/install_requirements.go
+++ pkg/config/install_requirements.go
@@ -402,7 +402,7 @@ type AutoUpdateConfig struct {
 // GithubAppConfig contains github app config
 type GithubAppConfig struct {
 	// Enabled this determines whether this install should use the jenkins x github app for access tokens
-	Enabled bool `json:"enabled,omitempty"`
+	Enabled bool `json:"enabled"`
 	// Schedule cron of the github app token refresher
 	Schedule string `json:"schedule,omitempty"`
 	// URL contains a URL to the github app
