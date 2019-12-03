ebfa3c6047b53586667c3b4d0672571a2ace88d7
Andrew Bayer <andrew.bayer@gmail.com>
2019-12-03 10:13:03 +0000

    fix: Apply labels as part of pull req creation, not just after
    Analagous to #6244 - gets us away from the nil pointer issue and cuts
    down on API calls a bit.
    
    fixes #6210
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- pkg/gits/operations/pull_request_op.go
+++ pkg/gits/operations/pull_request_op.go
@@ -88,13 +88,10 @@ func (o *PullRequestOperation) CreatePullRequest(kind string, update ChangeFiles
 		filter := &gits.PullRequestFilter{
 			Labels: labels,
 		}
+		details.Labels = labels
 		result, err = gits.PushRepoAndCreatePullRequest(dir, upstreamInfo, forkInfo, o.Base, details, filter, !o.SkipCommit, commitMessage, true, o.DryRun, o.Git(), provider)
 		if err != nil {
 			return nil, errors.Wrapf(err, "failed to create PR for base %s and head branch %s from temp dir %s", o.Base, details.BranchName, dir)
-		}
-		err = gits.AddLabelsToPullRequest(result, labels)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to add labels %+v to PR %s", labels, result.PullRequest.URL)
 		}
 	}
 	return result, nil
