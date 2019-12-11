d771cc9beac3f9c3f89dbdb74fa2ccf14e0aeccc
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-12-11 18:44:18 +0000

    feat: default values to env vars so command can be run in a pipeline
    Signed-off-by: Gareth Evans <gareth@bryncynfelin.co.uk>

--- pkg/cmd/step/pr/step_pr_comment.go
+++ pkg/cmd/step/pr/step_pr_comment.go
@@ -1,6 +1,8 @@
 package pr
 
 import (
+	"os"
+
 	"github.com/jenkins-x/jx/pkg/cmd/helper"
 	"github.com/jenkins-x/jx/pkg/cmd/opts/step"
 	"github.com/spf13/cobra"
@@ -60,14 +62,26 @@ func NewCmdStepPRComment(commonOpts *opts.CommonOptions) *cobra.Command {
 // Run implements this command
 func (o *StepPRCommentOptions) Run() error {
 	if o.Flags.PR == "" {
+		o.Flags.PR = os.Getenv("PULL_NUMBER")
+	}
+	if o.Flags.PR == "" {
 		return fmt.Errorf("no Pull Request number provided")
+	}
+
+	if o.Flags.Owner == "" {
+		o.Flags.Owner = os.Getenv("REPO_OWNER")
 	}
 	if o.Flags.Owner == "" {
 		return fmt.Errorf("no Git owner provided")
 	}
+
+	if o.Flags.Repository == "" {
+		o.Flags.Repository = os.Getenv("REPO_NAME")
+	}
 	if o.Flags.Repository == "" {
 		return fmt.Errorf("no Git repository provided")
 	}
+
 	if o.Flags.Comment == "" {
 		return fmt.Errorf("no comment provided")
 	}
