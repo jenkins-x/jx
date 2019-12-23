a2a463f2912f84873e69926eb31970685290e2df
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2019-12-23 11:12:23 +0100

    refactor: handle the errors when generating the documentation of the commands
    Signed-off-by: Cosmin Cojocar <cosmin.cojocar@gmx.ch>

--- pkg/cmd/create/create_docs.go
+++ pkg/cmd/create/create_docs.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/jenkins-x/jx/pkg/cmd/create/options"
+	"github.com/pkg/errors"
 
 	"github.com/jenkins-x/jx/pkg/cmd/helper"
 	"github.com/jenkins-x/jx/pkg/util"
@@ -79,13 +80,17 @@ func NewCmdCreateDocs(commonOpts *opts.CommonOptions) *cobra.Command {
 func (o *CreateDocsOptions) Run(jxCommand *cobra.Command) error {
 	dir := o.Dir
 
-	exists, _ := util.FileExists(dir)
+	exists, err := util.FileExists(dir)
+	if err != nil {
+		return errors.Wrapf(err, "checking whether the file %q exists", dir)
+	}
 	if !exists {
 		err := os.Mkdir(dir, util.DefaultWritePermissions)
 		if err != nil {
 			return fmt.Errorf("failed to create %s: %s", dir, err)
 		}
 	}
+
 	now := time.Now().Format(time.RFC3339)
 	prepender := func(filename string) string {
 		name := filepath.Base(filename)
@@ -93,15 +98,9 @@ func (o *CreateDocsOptions) Run(jxCommand *cobra.Command) error {
 		url := "/commands/" + strings.ToLower(base) + "/"
 		return fmt.Sprintf(gendocFrontmatterTemplate, now, strings.Replace(base, "_", " ", -1), base, url)
 	}
-
 	linkHandler := func(name string) string {
 		base := strings.TrimSuffix(name, path.Ext(name))
 		return "/commands/" + strings.ToLower(base) + "/"
 	}
-
-	//jww.FEEDBACK.Println("Generating Hugo command-line documentation in", gendocdir, "...")
-	doc.GenMarkdownTreeCustom(jxCommand, dir, prepender, linkHandler)
-	//jww.FEEDBACK.Println("Done.")
-
-	return nil
+	return doc.GenMarkdownTreeCustom(jxCommand, dir, prepender, linkHandler)
 }
