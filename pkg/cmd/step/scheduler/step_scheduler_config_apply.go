fdedced4b016a6bd0fc44d9620f10ea1e3f50bc2
James Strachan <james.strachan@gmail.com>
2020-01-23 14:14:34 +0000

    fix: lets always use the dev namespace for applying schedules
    also add extra validation check to ensure team settings are created
    
    Signed-off-by: James Strachan <james.strachan@gmail.com>

--- pkg/cmd/step/scheduler/step_scheduler_config_apply.go
+++ pkg/cmd/step/scheduler/step_scheduler_config_apply.go
@@ -1,6 +1,8 @@
 package scheduler
 
 import (
+	"fmt"
+
 	"github.com/jenkins-x/jx/pkg/cmd/helper"
 	"github.com/jenkins-x/jx/pkg/cmd/opts"
 	"github.com/jenkins-x/jx/pkg/cmd/opts/step"
@@ -62,13 +64,16 @@ func (o *StepSchedulerConfigApplyOptions) Run() error {
 	gitOps, devEnv := o.GetDevEnv()
 	switch o.Agent {
 	case "prow":
-		jxClient, ns, err := o.JXClient()
+		jxClient, ns, err := o.JXClientAndDevNamespace()
 		if err != nil {
 			return errors.WithStack(err)
 		}
 		teamSettings, err := o.TeamSettings()
 		if err != nil {
 			return err
+		}
+		if teamSettings == nil {
+			return fmt.Errorf("no TeamSettings for namespace %s", ns)
 		}
 		cfg, plugs, err := pipelinescheduler.GenerateProw(gitOps, true, jxClient, ns, teamSettings.DefaultScheduler.Name, devEnv, nil)
 		if err != nil {
