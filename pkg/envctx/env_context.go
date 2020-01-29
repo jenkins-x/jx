573680399003846411446ea4d6984f1def062b18
James Strachan <james.strachan@gmail.com>
2020-01-29 10:28:02 +0000

    fix: add helper class for working with environment contexts
    * wrapping up team settings / version resolver / requirements APIs in a simpler facade
    * add a helper method with tests for resolving a chart name with optional prefix and/or repository to a canonical set of data we can use in version streams / apps framework
    
    Signed-off-by: James Strachan <james.strachan@gmail.com>

---
+++ pkg/envctx/env_context.go
@@ -0,0 +1,87 @@
+package envctx
+
+import (
+	"strings"
+
+	v1 "github.com/jenkins-x/jx/pkg/apis/jenkins.io/v1"
+	"github.com/jenkins-x/jx/pkg/config"
+	"github.com/jenkins-x/jx/pkg/kube"
+	"github.com/jenkins-x/jx/pkg/versionstream"
+)
+
+// EnvironmentContext contains the common interfaces and structs needed for working with a development environment
+type EnvironmentContext struct {
+	// GitOps whether we are using gitops to manage this environment
+	GitOps bool
+
+	// Requirements the installation requirements
+	Requirements *config.RequirementsConfig
+
+	// DevEnv the development environment
+	DevEnv *v1.Environment
+
+	// VersionResolver the resolver of versions in the version stream
+	VersionResolver *versionstream.VersionResolver
+}
+
+// TeamSettings returns the team settings for the current environment
+func (c *EnvironmentContext) TeamSettings() *v1.TeamSettings {
+	if c.DevEnv == nil {
+		return nil
+	}
+	return &c.DevEnv.Spec.TeamSettings
+}
+
+// ChartDetails returns the chart details for the given chart name and repository URL
+type ChartDetails struct {
+	Name       string
+	Prefix     string
+	LocalName  string
+	Repository string
+}
+
+// ResolveChartRepository resolves the chart details from a full or local name and an optional repository URL.
+// this function can handle an empty repository but the chart name "foo/bar" and resolve the prefix "foo" to a repository
+// URL - or taking chart name "bar" and a repository URL and defaulting the prefix to "foo/bar"
+func (c *EnvironmentContext) ChartDetails(chartName string, repo string) (*ChartDetails, error) {
+	prefix := ""
+	localName := chartName
+	name := chartName
+	paths := strings.SplitN(name, "/", 2)
+	if len(paths) == 2 {
+		localName = paths[1]
+		prefix = paths[0]
+
+		prefixes, err := c.VersionResolver.GetRepositoryPrefixes()
+		if err != nil {
+			return nil, err
+		}
+		urls := prefixes.URLsForPrefix(prefix)
+		if len(urls) > 0 {
+			repo = urls[0]
+		}
+	}
+	teamSettings := c.TeamSettings()
+	if repo == "" && teamSettings != nil {
+		repo = teamSettings.AppsRepository
+	}
+	if repo == "" {
+		repo = kube.DefaultChartMuseumURL
+	}
+	if prefix == "" {
+		prefixes, err := c.VersionResolver.GetRepositoryPrefixes()
+		if err != nil {
+			return nil, err
+		}
+		prefix = prefixes.PrefixForURL(repo)
+	}
+	if prefix != "" && name == localName {
+		name = prefix + "/" + name
+	}
+	return &ChartDetails{
+		Name:       name,
+		Prefix:     prefix,
+		LocalName:  localName,
+		Repository: repo,
+	}, nil
+}
---
+++ pkg/envctx/env_context_test.go
@@ -0,0 +1,81 @@
+package envctx_test
+
+import (
+	"path"
+	"testing"
+
+	"github.com/jenkins-x/jx/pkg/envctx"
+	"github.com/jenkins-x/jx/pkg/versionstream"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEnvironmentContextResolveChartDetails(t *testing.T) {
+	t.Parallel()
+
+	versionsDir := path.Join("test_data", "jenkins-x-versions")
+	assert.DirExists(t, versionsDir)
+
+	ec := &envctx.EnvironmentContext{
+		VersionResolver: &versionstream.VersionResolver{
+			VersionsDir: versionsDir,
+		},
+	}
+
+	type testData struct {
+		Test       string
+		Name       string
+		Repository string
+		Expected   envctx.ChartDetails
+	}
+	tests := []testData{
+		{
+			Test:       "findRepositoryFromPrefix",
+			Name:       "jenkins-x/lighthouse",
+			Repository: "",
+			Expected: envctx.ChartDetails{
+				Name:       "jenkins-x/lighthouse",
+				Prefix:     "jenkins-x",
+				LocalName:  "lighthouse",
+				Repository: "https://storage.googleapis.com/chartmuseum.jenkins-x.io",
+			},
+		},
+		{
+			Test:       "findPrefixFromRepositoryURL",
+			Name:       "lighthouse",
+			Repository: "https://storage.googleapis.com/chartmuseum.jenkins-x.io",
+			Expected: envctx.ChartDetails{
+				Name:       "jenkins-x/lighthouse",
+				Prefix:     "jenkins-x",
+				LocalName:  "lighthouse",
+				Repository: "https://storage.googleapis.com/chartmuseum.jenkins-x.io",
+			},
+		},
+		{
+			Test: "findPrefixFromAliasRepositoryURL",
+			Name: "lighthouse",
+			// lets try use an alias
+			Repository: "http://chartmuseum.jenkins-x.io",
+			Expected: envctx.ChartDetails{
+				Name:       "jenkins-x/lighthouse",
+				Prefix:     "jenkins-x",
+				LocalName:  "lighthouse",
+				Repository: "http://chartmuseum.jenkins-x.io",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		name := test.Name
+		repo := test.Repository
+		expected := test.Expected
+		actual, err := ec.ChartDetails(name, repo)
+		require.NoError(t, err, "failed to find chart details for %s and %s", name, repo)
+
+		assert.Equal(t, expected.Name, actual.Name, "chartDetails.Name for test %s", test.Test)
+		assert.Equal(t, expected.LocalName, actual.LocalName, "chartDetails.LocalName for test %s", test.Test)
+		assert.Equal(t, expected.Prefix, actual.Prefix, "chartDetails.Prefix for test %s", test.Test)
+		assert.Equal(t, expected.Repository, actual.Repository, "chartDetails.Repository for test %s", test.Test)
+
+	}
+}
