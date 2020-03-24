6be2f4209ca95dcc8f0ffb9a5cecad19f3690026
mrageh <madam@cloudbees.com>
2020-03-24 12:22:45 +0000

    chore: remove create jhipster command
    This command was deprecated and it's time to remove it from JX.
    
    fixes #6934

--- pkg/cmd/create/create_project.go
+++ pkg/cmd/create/create_project.go
@@ -33,9 +33,9 @@ var (
 	}
 
 	createProjectLong = templates.LongDesc(`
-		Create a new Project by importing code, using a Quickstart or custom wizard for Spring or Camel.
+		Create a new Project by importing code, using a Quickstart or custom wizard for Spring.
 
-` + helper.SeeAlsoText("jx create quickstart", "jx create spring", "jx create jhipster", "jx import"))
+` + helper.SeeAlsoText("jx create quickstart", "jx create spring", "jx import"))
 
 	createProjectExample = templates.Examples(`
 		# Create a project
