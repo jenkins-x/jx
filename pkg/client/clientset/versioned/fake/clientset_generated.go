597b8522c413580acbc0645fa8c1b446e2bd3d35
Andrew Bayer <andrew.bayer@gmail.com>
2020-06-10 16:47:33 -0400

    fix: Use k8s 1.15.12 for client gen
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- pkg/client/clientset/versioned/fake/clientset_generated.go
+++ pkg/client/clientset/versioned/fake/clientset_generated.go
@@ -25,7 +25,7 @@ func NewSimpleClientset(objects ...runtime.Object) *Clientset {
 		}
 	}
 
-	cs := &Clientset{}
+	cs := &Clientset{tracker: o}
 	cs.discovery = &fakediscovery.FakeDiscovery{Fake: &cs.Fake}
 	cs.AddReactor("*", "*", testing.ObjectReaction(o))
 	cs.AddWatchReactor("*", func(action testing.Action) (handled bool, ret watch.Interface, err error) {
@@ -47,20 +47,20 @@ func NewSimpleClientset(objects ...runtime.Object) *Clientset {
 type Clientset struct {
 	testing.Fake
 	discovery *fakediscovery.FakeDiscovery
+	tracker   testing.ObjectTracker
 }
 
 func (c *Clientset) Discovery() discovery.DiscoveryInterface {
 	return c.discovery
 }
 
+func (c *Clientset) Tracker() testing.ObjectTracker {
+	return c.tracker
+}
+
 var _ clientset.Interface = &Clientset{}
 
 // JenkinsV1 retrieves the JenkinsV1Client
 func (c *Clientset) JenkinsV1() jenkinsv1.JenkinsV1Interface {
-	return &fakejenkinsv1.FakeJenkinsV1{Fake: &c.Fake}
-}
-
-// Jenkins retrieves the JenkinsV1Client
-func (c *Clientset) Jenkins() jenkinsv1.JenkinsV1Interface {
 	return &fakejenkinsv1.FakeJenkinsV1{Fake: &c.Fake}
 }
