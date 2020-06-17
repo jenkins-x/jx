b785a5973481b18e105cda3f11acd3d93a42a1e9
Cai Cooper <caicooper82@gmail.com>
2020-06-17 13:17:31 +0100

    feat: attempt to prevent using provided requirements for a provisioned jenkins-x cluster

--- pkg/cmd/boot/boot.go
+++ pkg/cmd/boot/boot.go
@@ -184,6 +184,13 @@ func (o *BootOptions) Run() error {
 		return err
 	}
 
+	// check if the user has passed in a requirements file to a cluster which has already been provisioned.
+	if o.RequirementsFile != "" {
+		if err := o.checkIfProvidedRequirementsArePossiblyStale(); err != nil {
+			return errors.Wrapf(err, "loading from provided requirements file")
+		}
+	}
+
 	if err := o.overrideRequirements(gitURL); err != nil {
 		return errors.Wrap(err, "overwriting the default requirements")
 	}
@@ -494,6 +501,17 @@ func existingBootClone(dir string) (bool, error) {
 		return false, err
 	}
 	return requirementsExist && pipelineExists, nil
+}
+
+func (o *BootOptions) checkIfProvidedRequirementsArePossiblyStale() error {
+	_, devEnv := o.GetDevEnv()
+	if devEnv != nil {
+		log.Logger().Warnf("It seems you're passing a requirements file to cluster which has already been provisioned.")
+		log.Logger().Warnf("We recommend you update the %s file at %s, using the updates provided within your local %s file.",
+			config.RequirementsConfigFileName, devEnv.Spec.Source, o.RequirementsFile)
+		return errors.New("attempting to boot cluster using a requirements file which is possibly stale")
+	}
+	return nil
 }
 
 func (o *BootOptions) overrideRequirements(defaultBootConfigURL string) error {
