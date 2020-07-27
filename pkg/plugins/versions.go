e527dfba2c24ce30d7214be38d06092e98786076
James Strachan <james.strachan@gmail.com>
2020-07-27 12:48:24 +0100

    fix: promote and gitops version

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -12,7 +12,7 @@ const (
 	ApplicationVersion = "0.0.6"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.0.136"
+	GitOpsVersion = "0.0.140"
 
 	// PipelineVersion the version of the jx pipeline plugin
 	PipelineVersion = "0.0.8"
