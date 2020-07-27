e5e53a31cd252079491d8d6116be128d72114aca
James Strachan <james.strachan@gmail.com>
2020-07-27 11:11:40 +0100

    fix: add better promote

--- promote.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "promoting the new version ${VERSION} to downstream repositories"
-
-jx step create pr regex --regex 'version: (.*)' --version ${VERSION} --files docker/gcr.io/jenkinsxio-labs-private/jx-cli.yml --repo https://github.com/jenkins-x/jxr-versions.git
