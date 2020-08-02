6d6c9f08ed8b77d310c6863ee3b5a1c535694889
ryanvanoss <ryanvanoss@users.noreply.github.com>
2020-08-02 07:24:45 -0700

    chore: update docs to use correct Version Stream URL
    Signed-off-by: ryanvanoss <ryanvanoss@users.noreply.github.com>

--- pkg/versionstream/resolver.go
+++ pkg/versionstream/resolver.go
@@ -42,7 +42,7 @@ func (v *VersionResolver) ResolveGitVersion(gitURL string) (string, error) {
 	if answer == "" {
 		path := GitURLToName(gitURL)
 		log.Logger().Warnf("could not find a stable version for git repository: %s in %s", gitURL, v.VersionsDir)
-		log.Logger().Warn("for background see: https://jenkins-x.io/docs/concepts/version-stream/")
+		log.Logger().Warn("for background see: https://jenkins-x.io/about/concepts/version-stream/")
 		log.Logger().Infof("please lock this version down via the command: %s", util.ColorInfo(fmt.Sprintf("jx step create pr versions -k git -n %s -v 1.2.3", path)))
 	}
 	return answer, nil
