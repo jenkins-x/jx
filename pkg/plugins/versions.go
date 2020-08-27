ae527c9dc3dd3e18250af0536bfc3f2005d8672e
James Strachan <james.strachan@gmail.com>
2020-08-27 15:36:30 +0100

    fix: ugprade secret

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -30,7 +30,7 @@ const (
 	PromoteVersion = "0.0.80"
 
 	// SecretVersion the version of the jx secret plugin
-	SecretVersion = "0.0.96"
+	SecretVersion = "0.0.98"
 
 	// TestVersion the version of the jx test plugin
 	TestVersion = "0.0.18"
