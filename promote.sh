818b44f9dcfa8d64d95d1a9ebfd9070e54a2db9a
James Strachan <james.strachan@gmail.com>
2020-08-27 15:36:22 +0100

    fix: polish promote

--- promote.sh
+++ promote.sh
@@ -2,17 +2,4 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
-git clone https://github.com/jenkins-x/jx3-gitops-template.git
-cd jx3-gitops-template
-
-sed -i -e "s/jx-cli:.*/jx-cli:${VERSION}/" .jx/git-operator/job.yaml
-
-git commit -a -m "fix: upgrade jx-cli"
-git push
-
-./promote.sh
-
-cd ..
-
-echo "running the promote binary"
 ./promote/main
