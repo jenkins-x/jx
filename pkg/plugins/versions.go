cfd1e224c516aa2632d3cc194e58ab8e0c3592a9
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2020-09-08 09:50:55 +0000

    chore(deps): bump https://github.com/jenkins-x/jx-secret from 0.0.130 to 0.0.132
    Command run was `jx step create pr regex --regex \s+SecretVersion = "(?P<version>.*)" --version 0.0.132 --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git`

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -30,7 +30,7 @@ const (
 	PromoteVersion = "0.0.86"
 
 	// SecretVersion the version of the jx secret plugin
-	SecretVersion = "0.0.130"
+	SecretVersion = "0.0.132"
 
 	// TestVersion the version of the jx test plugin
 	TestVersion = "0.0.18"
