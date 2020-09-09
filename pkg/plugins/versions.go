43659edbf03db527164bf551feb52dc41e1d7926
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2020-09-09 16:31:19 +0000

    chore(deps): bump https://github.com/jenkins-x/jx-preview from 0.0.46 to 0.0.48
    Command run was `jx step create pr regex --regex \s+PreviewVersion = "(?P<version>.*)" --version 0.0.48 --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git`

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -21,7 +21,7 @@ const (
 	PipelineVersion = "0.0.14"
 
 	// PreviewVersion the version of the jx preview plugin
-	PreviewVersion = "0.0.46"
+	PreviewVersion = "0.0.48"
 
 	// ProjectVersion the version of the jx project plugin
 	ProjectVersion = "0.0.75"
