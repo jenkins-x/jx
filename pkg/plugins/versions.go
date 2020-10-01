e703f2993f3a9e27779baa26f0a31abfd185819a
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2020-10-01 19:17:45 +0000

    chore(deps): bump https://github.com/jenkins-x/jx-gitops from 0.0.358 to 0.0.360
    Command run was `jx step create pr regex --regex \s+GitOpsVersion = "(?P<version>.*)" --version 0.0.360 --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git`

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -12,7 +12,7 @@ const (
 	ApplicationVersion = "0.0.10"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.0.358"
+	GitOpsVersion = "0.0.360"
 
 	// JenkinsVersion the version of the jx jenkins plugin
 	JenkinsVersion = "0.0.22"
