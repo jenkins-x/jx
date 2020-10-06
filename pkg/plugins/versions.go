c0d7d138cca55c41051e5328b371f347f6238428
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2020-10-06 17:54:27 +0000

    chore(deps): bump https://github.com/jenkins-x/jx-pipeline from 0.0.34 to 0.0.36
    Command run was `jx step create pr regex --regex \s+PipelineVersion = "(?P<version>.*)" --version 0.0.36 --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git`

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -18,7 +18,7 @@ const (
 	JenkinsVersion = "0.0.22"
 
 	// PipelineVersion the version of the jx pipeline plugin
-	PipelineVersion = "0.0.34"
+	PipelineVersion = "0.0.36"
 
 	// PreviewVersion the version of the jx preview plugin
 	PreviewVersion = "0.0.86"
