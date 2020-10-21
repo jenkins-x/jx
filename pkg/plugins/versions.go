20a93669870b82a4d3462b0cf5dee3bf436a727f
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2020-10-21 15:37:16 +0000

    chore(deps): bump https://github.com/jenkins-x-plugins/jx-health.git from 0.0.54 to 0.0.55
    Command run was `jx step create pr regex --regex \s+HealthVersion = "(?P<version>.*)" --version 0.0.55 --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git`

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -15,7 +15,7 @@ const (
 	GitOpsVersion = "0.0.398"
 
 	// HealthVersion the version of the jx health plugin
-	HealthVersion = "0.0.54"
+	HealthVersion = "0.0.55"
 
 	// JenkinsVersion the version of the jx jenkins plugin
 	JenkinsVersion = "0.0.29"
