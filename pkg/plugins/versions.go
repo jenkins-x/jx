ab988880e187c9e8976371127130270835904b0a
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2020-10-26 11:21:57 +0000

    chore(deps): bump https://github.com/jenkins-x/jx-admin.git from 0.0.131 to 0.0.133
    Command run was `jx step create pr regex --regex \s+AdminVersion = "(?P<version>.*)" --version 0.0.133 --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git`

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -6,7 +6,7 @@ import (
 
 const (
 	// AdminVersion the version of the jx admin plugin
-	AdminVersion = "0.0.131"
+	AdminVersion = "0.0.133"
 
 	// ApplicationVersion the version of the jx application plugin
 	ApplicationVersion = "0.0.17"
