0f7afecce657939835b8ed186543a2762eabe4b9
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2020-11-13 12:31:20 +0000

    chore(deps): bump https://github.com/jenkins-x/jx-project.git from 0.0.150 to 0.0.151
    Command run was `jx step create pr regex --regex \s+ProjectVersion = "(?P<version>.*)" --version 0.0.151 --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git`

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -33,7 +33,7 @@ const (
 	PreviewVersion = "0.0.119"
 
 	// ProjectVersion the version of the jx project plugin
-	ProjectVersion = "0.0.150"
+	ProjectVersion = "0.0.151"
 
 	// PromoteVersion the version of the jx promote plugin
 	PromoteVersion = "0.0.143"
