29defb70e5e1e6edd60a960d38c00a6c8533297a
James Strachan <james.strachan@gmail.com>
2020-12-04 06:33:04 +0000

    fix: upgrade jx-gitops

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -13,7 +13,7 @@ const (
 	ApplicationVersion = "0.0.19"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.0.446"
+	GitOpsVersion = "0.0.448"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.57"
