041537675126fce60e1bd567e8f17f90784f7c88
root <jenkins-x@googlegroups.com>
2020-12-15 14:46:03 +0000

    fix: upgrade to version 0.0.484

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -13,7 +13,7 @@ const (
 	ApplicationVersion = "0.0.19"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.0.483"
+	GitOpsVersion = "0.0.484"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.59"
