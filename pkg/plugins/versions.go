c7c5b16458d2d18c66077d3203fba2df3a38a80d
root <jenkins-x@googlegroups.com>
2021-01-07 19:06:59 +0000

    fix: upgrade to version 0.0.511

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -13,7 +13,7 @@ const (
 	ApplicationVersion = "0.0.19"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.0.510"
+	GitOpsVersion = "0.0.511"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.59"
