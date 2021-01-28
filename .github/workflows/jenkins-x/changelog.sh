bc1142de5e816b4579b081f0bb05918c0a85b324
James Strachan <james.strachan@gmail.com>
2021-01-28 14:26:03 +0000

    fix: enable GHA release

---
+++ .github/workflows/jenkins-x/changelog.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+source .jx/variables.sh
+
+if [ -d "charts/$REPO_NAME" ]; then
+sed -i -e "s/^version:.*/version: $VERSION/" ./charts/$REPO_NAME/Chart.yaml
+sed -i -e "s/repository:.*/repository: $DOCKER_REGISTRY\/$DOCKER_REGISTRY_ORG\/$APP_NAME/" ./charts/$REPO_NAME/values.yaml
+sed -i -e "s/tag:.*/tag: $VERSION/" ./charts/$REPO_NAME/values.yaml;
+else echo no charts; fi
+
+jx changelog create --verbose --header-file=hack/changelog-header.md --version=$VERSION --rev=$PULL_BASE_SHA --output-markdown=changelog.md --update-release=false
