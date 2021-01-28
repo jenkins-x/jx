36a609866fac14760a1a0cad09c91a0ffd2b381e
James Strachan <james.strachan@gmail.com>
2021-01-28 16:02:46 +0000

    chore: fix git user

--- .github/workflows/jenkins-x/release-chart.sh
+++ .github/workflows/jenkins-x/release-chart.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 if [ -d "charts/$REPO_NAME" ]; then source .jx/variables.sh
+echo setting the git user.name
 git config user.name jenkins-x-bot-test
+git config --global --add user.name jenkins-x-bot-test
 jx gitops helm release
 fi
