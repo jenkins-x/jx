01ceed0d5aa644e330c4c565f360f0850f16166c
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2021-01-29 08:56:58 +0000

    chore: try new chart image

--- .github/workflows/jenkins-x/release-chart.sh
+++ .github/workflows/jenkins-x/release-chart.sh
@@ -5,6 +5,8 @@ echo current git configuration
 git config --global --get user.name
 git config --global --get user.email
 
+echo "setting git user"
+
 git config --global user.name jenkins-x-bot-test
 git config --global user.email "jenkins-x@googlegroups.com"
 
