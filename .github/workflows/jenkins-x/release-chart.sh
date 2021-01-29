e47b037c03b1b7f86e43a89138c0475443981f3e
James Strachan <james.strachan@gmail.com>
2021-01-29 07:09:08 +0000

    chore: avoid HOME dir in boot

--- .github/workflows/jenkins-x/release-chart.sh
+++ .github/workflows/jenkins-x/release-chart.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-if [ -d "charts/$REPO_NAME" ]; then source .jx/variables.sh
+echo "HOME is $HOME"
 echo setting the git user.name
-git config user.name jenkins-x-bot-test
 git config --global --add user.name jenkins-x-bot-test
+git config user.name jenkins-x-bot-test
 jx gitops helm release
-fi
