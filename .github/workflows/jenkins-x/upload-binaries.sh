09a6c20dd328860fbf61bb70087282c8c281aecf
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2021-01-29 09:45:15 +0000

    chore: fix changelog

--- .github/workflows/jenkins-x/upload-binaries.sh
+++ .github/workflows/jenkins-x/upload-binaries.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo "HOME is $HOME"
+echo current git configuration
+
+git config --global --get user.name
+git config --global --get user.email
+
+echo "setting git user"
+
+git config --global user.name jenkins-x-bot-test
+git config --global user.email "jenkins-x@googlegroups.com"
+
 git add * || true
 git commit -a -m "chore: release $VERSION" --allow-empty
 git tag -fa v$VERSION -m "Release version $VERSION"
@@ -10,4 +21,5 @@ export BUILDDATE=$(date)
 export REV=$(git rev-parse HEAD)
 export GOVERSION="1.15"
 export ROOTPACKAGE="github.com/$REPOSITORY"
+
 goreleaser release
