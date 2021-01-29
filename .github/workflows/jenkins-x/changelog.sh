84b61496f9addd460111b373959efd2cde976528
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2021-01-29 08:39:51 +0000

    chore: fix changelog

--- .github/workflows/jenkins-x/changelog.sh
+++ .github/workflows/jenkins-x/changelog.sh
@@ -7,7 +7,11 @@ sed -i -e "s/tag:.*/tag: $VERSION/" ./charts/$REPO_NAME/values.yaml;
 
 jx changelog create --verbose --header-file=hack/changelog-header.md --version=$VERSION --rev=$PULL_BASE_SHA --output-markdown=changelog.md --update-release=false
 
+git config --global user.name "jenkins-x-bot-test"
+git config --global user.email "jenkins-x@googlegroups.com"
+
 git add * || true
 git commit -a -m "chore: release $VERSION" --allow-empty
+
 #git tag -fa v$VERSION -m "Release version $VERSION"
 #git push origin v$VERSION
