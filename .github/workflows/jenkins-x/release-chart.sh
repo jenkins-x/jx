b620958bfd1778057ef55d61d0bbb0fffea3bec5
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2021-01-29 07:52:08 +0000

    chore: fix chart release

--- .github/workflows/jenkins-x/release-chart.sh
+++ .github/workflows/jenkins-x/release-chart.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
 echo "HOME is $HOME"
 echo current git configuration
+
 git config --global --get user.name
-git config --global --get user.emao;
+git config --global --get user.email
 
 git config --global user.name jenkins-x-bot-test
 git config --global user.email "jenkins-x@googlegroups.com"
-
-jx gitops git setup 
 
 jx gitops helm release
