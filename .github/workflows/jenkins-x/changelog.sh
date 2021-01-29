e44f6507cc01afa942465b5977941d9bf99accda
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2021-01-29 09:45:15 +0000

    chore: fix changelog

--- .github/workflows/jenkins-x/changelog.sh
+++ .github/workflows/jenkins-x/changelog.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env sh
-source .jx/variables.sh
 
-sed -i -e "s/^version:.*/version: $VERSION/" ./charts/$REPO_NAME/Chart.yaml
-sed -i -e "s/repository:.*/repository: $DOCKER_REGISTRY\/$DOCKER_REGISTRY_ORG\/$APP_NAME/" ./charts/$REPO_NAME/values.yaml
-sed -i -e "s/tag:.*/tag: $VERSION/" ./charts/$REPO_NAME/values.yaml;
+echo "REPO_NAME = $PULL_BASE_SHA"
+
+export PULL_BASE_SHA=$(git rev-parse HEAD)
+
+if [ -d "charts/$REPO_NAME" ]; then
+  sed -i -e "s/^version:.*/version: $VERSION/" ./charts/$REPO_NAME/Chart.yaml
+  sed -i -e "s/tag:.*/tag: $VERSION/" ./charts/$REPO_NAME/values.yaml;
+
+  # sed -i -e "s/repository:.*/repository: $DOCKER_REGISTRY\/$DOCKER_REGISTRY_ORG\/$APP_NAME/" ./charts/$REPO_NAME/values.yaml
+else
+  echo no charts
+fi
 
 jx changelog create --verbose --header-file=hack/changelog-header.md --version=$VERSION --rev=$PULL_BASE_SHA --output-markdown=changelog.md --update-release=false
-
-echo "HOME is $HOME"
-echo current git configuration
-
-git config --global --get user.name
-git config --global --get user.email
-
-echo "setting git user"
-
-git config --global user.name jenkins-x-bot-test
-git config --global user.email "jenkins-x@googlegroups.com"
-
-git add * || true
-git commit -a -m "chore: release $VERSION" --allow-empty
-
-#git tag -fa v$VERSION -m "Release version $VERSION"
-#git push origin v$VERSION
