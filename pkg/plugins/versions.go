7662200183578fa78ce5007e001b5b1d84406c0f
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2021-02-01 09:28:17 +0000

    fix: upgrade to version 0.0.167
    from: https://github.com/jenkins-x/jx-admin

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	// AdminVersion the version of the jx admin plugin
-	AdminVersion = "0.0.164"
+	AdminVersion = "0.0.167"
 
 	// ApplicationVersion the version of the jx application plugin
 	ApplicationVersion = "0.0.25"
