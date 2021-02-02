7c9788d6eaef8f2599a6cd3fdfc740a7860330a7
root <jenkins-x@googlegroups.com>
2021-02-02 08:05:25 +0000

    fix: upgrade to version 0.0.543
    from: https://github.com/jenkins-x/jx-gitops.git

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -13,7 +13,7 @@ const (
 	ApplicationVersion = "0.0.25"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.0.542"
+	GitOpsVersion = "0.0.543"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.66"
