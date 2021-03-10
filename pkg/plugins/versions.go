7993c4eff844b0094909007c944d69e248734f6e
root <jenkins-x@googlegroups.com>
2021-03-10 15:43:34 +0000

    chore: upgrade jenkins-x/jx-cli to version 0.2.18
    from: https://github.com/jenkins-x/jx-gitops.git

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -13,7 +13,7 @@ const (
 	ApplicationVersion = "0.0.29"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.2.17"
+	GitOpsVersion = "0.2.18"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.72"
