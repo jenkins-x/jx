72b83504fc290a59f13b00e3a032b78c2d1c85c1
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2021-03-26 16:14:03 +0000

    fix: upgrade to version 0.0.245
    from: https://github.com/jenkins-x/jx-promote

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -37,7 +37,7 @@ const (
 	ProjectVersion = "0.0.195"
 
 	// PromoteVersion the version of the jx promote plugin
-	PromoteVersion = "0.0.244"
+	PromoteVersion = "0.0.245"
 
 	// SecretVersion the version of the jx secret plugin
 	SecretVersion = "0.1.10"
