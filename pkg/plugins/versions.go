668d4859cbc0eb2ff7cf54b65bafe260bb91513e
root <jenkins-x@googlegroups.com>
2021-05-05 06:25:55 +0000

    chore(deps): upgrade jenkins-x/jx to version 0.2.75
    from: https://github.com/jenkins-x-plugins/jx-gitops.git

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -13,7 +13,7 @@ const (
 	ApplicationVersion = "0.0.35"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.2.74"
+	GitOpsVersion = "0.2.75"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.74"
