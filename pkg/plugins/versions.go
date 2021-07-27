42e307d3f2d7ab96115c5abad5f86070b1a01fb1
root <jenkins-x@googlegroups.com>
2021-07-27 19:27:47 +0000

    chore(deps): upgrade jenkins-x/jx to version 0.3.6
    from: https://github.com/jenkins-x-plugins/jx-gitops.git

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -13,7 +13,7 @@ const (
 	ApplicationVersion = "0.0.35"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.3.5"
+	GitOpsVersion = "0.3.6"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.75"
