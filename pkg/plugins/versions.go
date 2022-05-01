4a33dc977412db816ae2817ffd15d7bef6d5aa0f
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2022-05-01 21:25:22 +0000

    chore(deps): upgrade jenkins-x/jx3-pipeline-catalog to version 0.1.4
    from: https://github.com/jenkins-x-plugins/jx-admin

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	// AdminVersion the version of the jx admin plugin
-	AdminVersion = "0.1.1"
+	AdminVersion = "0.1.4"
 
 	// ApplicationVersion the version of the jx application plugin
 	ApplicationVersion = "0.0.41"
