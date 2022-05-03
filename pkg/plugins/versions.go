6e5c6ac66c747e2121d6dfbe0f55f39dc84dad00
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2022-05-03 13:54:27 +0000

    chore(deps): upgrade jenkins-x/jx3-pipeline-catalog to version 0.2.9
    from: https://github.com/jenkins-x-plugins/jx-promote

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -34,7 +34,7 @@ const (
 	ProjectVersion = "0.2.39"
 
 	// PromoteVersion the version of the jx promote plugin
-	PromoteVersion = "0.2.8"
+	PromoteVersion = "0.2.9"
 
 	// SecretVersion the version of the jx secret plugin
 	SecretVersion = "0.1.68"
