6e2166692fe721a77a44c603ff29a37d76f3f567
Mårten Svantesson <marten.svantesson@ticket.se>
2022-06-28 14:26:38 +0200

    fix: Release issue

--- .github/workflows/jenkins-x/upload-binaries.sh
+++ .github/workflows/jenkins-x/upload-binaries.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# See https://github.com/actions/checkout/issues/766
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+git config --global --get user.name
+git config --global --get user.email
+
+echo "setting git user"
+
+git config --global user.name jenkins-x-bot-test
+git config --global user.email "jenkins-x@googlegroups.com"
+
 export BRANCH=$(git rev-parse --abbrev-ref HEAD)
 export BUILDDATE=$(date)
 export REV=$(git rev-parse HEAD)
