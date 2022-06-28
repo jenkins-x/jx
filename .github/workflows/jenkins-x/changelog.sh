eb7a39ff73a39a168d6462117333aafb40fbff1f
Mårten Svantesson <marten.svantesson@ticket.se>
2022-06-28 16:17:50 +0200

    fix: Release

--- .github/workflows/jenkins-x/changelog.sh
+++ .github/workflows/jenkins-x/changelog.sh
@@ -4,4 +4,4 @@ echo "REPO_NAME = $PULL_BASE_SHA"
 
 export PULL_BASE_SHA=$(git rev-parse HEAD)
 
-jx changelog create --verbose --header-file=hack/changelog-header.md --version=v$VERSION --rev=$PULL_BASE_SHA
+jx changelog create --verbose --header-file=hack/changelog-header.md --version=v$VERSION --rev=$PULL_BASE_SHA --prerelease
