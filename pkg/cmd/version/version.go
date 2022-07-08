12a842d9e09384617e00030c1468b6a9efb38c46
osamamagdy <osamamagdy174@gmail.com>
2022-07-08 07:55:18 -0700

    feat: add build information to jx version
    Signed-off-by: osamamagdy <osamamagdy174@gmail.com>

--- pkg/cmd/version/version.go
+++ pkg/cmd/version/version.go
@@ -1,34 +1,62 @@
 package version
 
 import (
-	"github.com/jenkins-x/jx-helpers/v3/pkg/cobras/helper"
-	"github.com/jenkins-x/jx-helpers/v3/pkg/termcolor"
-	"github.com/jenkins-x/jx-logging/v3/pkg/log"
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 // Build information. Populated at build-time.
+type buildInfo struct {
+	Version      string
+	Revision     string
+	Branch       string
+	GitTreeState string
+	BuildDate    string
+	GoVersion    string
+}
+
+// Build information. Populated at build-time.
 var (
-	Version   string
-	Revision  string
-	Branch    string
-	BuildUser string
-	BuildDate string
-	GoVersion string
+	Version      string
+	Revision     string
+	BuildDate    string
+	GoVersion    string
+	Branch       string
+	GitTreeState string
 )
 
 const (
 
 	// TestVersion used in test cases for the current version if no
 	// version can be found - such as if the version property is not properly
-	// included in the go test flags
-	TestVersion = "1.0.0-SNAPSHOT"
+	// included in the go test flags.
+	TestVersion = "3.2.238"
+
+	// TestRevision can be used in tests if no revision is passed in the test flags
+	TestRevision = "04b628f48"
+
+	// TestBranch can be used in tests if no tree state is passed in the test flags
+	TestBranch = "main"
+
+	// TestTreeState can be used in tests if no tree state is passed in the test flags
+	TestTreeState = "clean"
+
+	// TestBuildDate can be used in tests if no build date is passed in the test flags
+	TestBuildDate = "2022-05-31T14:51:38Z"
+
+	// TestGoVersion can be used in tests if no version is passed in the test flags
+	TestGoVersion = "1.17.8"
 )
 
 // ShowOptions the options for viewing running PRs
 type Options struct {
 	Verbose bool
 	Quiet   bool
+	Short   bool
+	Out     io.Writer
 }
 
 // NewCmdVersion creates a command object for the "version" command
@@ -39,28 +67,87 @@ func NewCmdVersion() (*cobra.Command, *Options) {
 		Use:   "version",
 		Short: "Displays the version of this command",
 		Run: func(cmd *cobra.Command, args []string) {
-			err := o.Run()
-			helper.CheckErr(err)
+			o.run()
 		},
 	}
 	cmd.Flags().BoolVarP(&o.Quiet, "quiet", "q", false, "uses the quiet format of just outputting the version number only")
+	cmd.Flags().BoolVarP(&o.Short, "short", "s", false, "uses the short format of just outputting the version number only")
 	return cmd, o
 }
 
 // Run implements the command
-func (o *Options) Run() error {
-	v := GetVersion()
-	if o.Quiet {
-		log.Logger().Infof(v)
-		return nil
+func (o *Options) run() {
+	v := getBuildInfo()
+	if o.Out == nil {
+		o.Out = os.Stdout
 	}
-	log.Logger().Infof("version: %s", termcolor.ColorInfo(v))
-	return nil
+	if o.Quiet {
+		fmt.Fprintln(o.Out, "The --quit, -q flag is being deprecated from JX on Oct 2022\nUse --short, -s instead")
+		fmt.Fprintf(o.Out, "%s\n", v.Version)
+		return
+	}
+	if o.Short {
+		fmt.Fprintf(o.Out, "%s\n", v.Version)
+		return
+	}
+	fmt.Fprintf(o.Out, "version: %s\n", v.Version)
+	fmt.Fprintf(o.Out, "shaCommit: %s\n", v.Revision)
+	fmt.Fprintf(o.Out, "buildDate: %s\n", v.BuildDate)
+	fmt.Fprintf(o.Out, "goVersion: %s\n", v.GoVersion)
+	fmt.Fprintf(o.Out, "branch: %s\n", v.Branch)
+	fmt.Fprintf(o.Out, "gitTreeState: %s\n", v.GitTreeState)
+
 }
 
-func GetVersion() string {
+func getBuildInfo() buildInfo {
+	return buildInfo{
+		Version:      getVersion(),
+		Revision:     getCommitSha(),
+		Branch:       getBranch(),
+		GitTreeState: getTreeState(),
+		BuildDate:    getBuildDate(),
+		GoVersion:    getGoVersion(),
+	}
+}
+
+func getVersion() string {
 	if Version != "" {
 		return Version
 	}
 	return TestVersion
+}
+
+func getGoVersion() string {
+	if GoVersion != "" {
+		return GoVersion
+	}
+	return TestGoVersion
+}
+
+func getCommitSha() string {
+	if Revision != "" {
+		return Revision
+	}
+	return TestRevision
+}
+
+func getBuildDate() string {
+	if BuildDate != "" {
+		return BuildDate
+	}
+	return TestBuildDate
+}
+
+func getBranch() string {
+	if Branch != "" {
+		return Branch
+	}
+	return TestBranch
+}
+
+func getTreeState() string {
+	if GitTreeState != "" {
+		return GitTreeState
+	}
+	return TestTreeState
 }
--- pkg/cmd/version/version_test.go
+++ pkg/cmd/version/version_test.go
@@ -1,14 +1,67 @@
 package version_test
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
 
 	"github.com/jenkins-x/jx/pkg/cmd/version"
 	"github.com/stretchr/testify/assert"
 )
 
+// define a strtuct for test cases for the version command
+type TestCase struct {
+	args        []string
+	err         error
+	description string
+	out         string
+}
+
+var testCases = []TestCase{
+	{
+		args:        nil,
+		err:         nil,
+		description: "This is to test without flags",
+		out:         "version: 3.2.238\nshaCommit: 04b628f48\nbuildDate: 2022-05-31T14:51:38Z\ngoVersion: 1.17.8\nbranch: main\ngitTreeState: clean\n",
+	},
+	{
+		args:        []string{"-q"},
+		err:         nil,
+		description: "This is to test -q flag",
+		out:         "The --quit, -q flag is being deprecated from JX on Oct 2022\nUse --short, -s instead\n3.2.238\n",
+	},
+	{
+		args:        []string{"--quiet"},
+		err:         nil,
+		description: "This is to test --quiet flag",
+		out:         "The --quit, -q flag is being deprecated from JX on Oct 2022\nUse --short, -s instead\n3.2.238\n",
+	},
+	{
+		args:        []string{"-s"},
+		err:         nil,
+		description: "This is to test -s flag",
+		out:         "3.2.238\n",
+	},
+	{
+		args:        []string{"--short"},
+		err:         nil,
+		description: "This is to test --short flag",
+		out:         "3.2.238\n",
+	},
+}
+
 func TestNewCmdVersion(t *testing.T) {
-	cmd, _ := version.NewCmdVersion()
-	err := cmd.Execute()
-	assert.NoError(t, err)
+	for _, testCase := range testCases {
+		var buf bytes.Buffer
+		cmd, options := version.NewCmdVersion()
+		options.Out = &buf
+		cmd.SetArgs(testCase.args)
+		err := cmd.Execute()
+
+		fmt.Println(testCase.description)
+		assert.NoError(t, err)
+		if testCase.err == nil {
+			assert.Equal(t, testCase.out, buf.String())
+		}
+	}
 }
