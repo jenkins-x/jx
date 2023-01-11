ff5eb0f1543e20ce2e6421ec35339e70563f10fc
root <jenkins-x@googlegroups.com>
2023-01-11 22:31:47 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-gitops to version 0.12.0
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ### Linux
    
    ```shell
    curl -L https://github.com/jenkins-x-plugins/jx-gitops/releases/download/v0.12.0/jx-gitops-linux-amd64.tar.gz | tar xzv
    sudo mv jx-gitops /usr/local/bin
    ```
    
    ### macOS
    
    ```shell
    curl -L  https://github.com/jenkins-x-plugins/jx-gitops/releases/download/v0.12.0/jx-gitops-darwin-amd64.tar.gz | tar xzv
    sudo mv jx-gitops /usr/local/bin
    ```
    
    ## Changes in version 0.12.0
    
    ### New Features
    
    * jx-variables: allow overriding APP_NAME via --app cli arg (joshuasimon-taulia)
    
    ### Chores
    
    * regen docs (joshuasimon-taulia)

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.9.3"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.11.2"
+	GitOpsVersion = "0.12.0"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.77"
