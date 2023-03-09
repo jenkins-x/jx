da9daf95e4327d76944a60d5fabae39e3f00b151
root <jenkins-x@googlegroups.com>
2023-03-09 15:34:18 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-gitops to version 0.12.1
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ## Changes in version 0.12.1
    
    ### Bug Fixes
    
    * correct ingress details on custom namespaces (Andy Nelson)

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.0"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.12.0"
+	GitOpsVersion = "0.12.1"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.77"
