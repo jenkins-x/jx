1b8cc40d97b2dfba87b9b56fe28c518c5d4f7d66
root <jenkins-x@googlegroups.com>
2023-03-11 15:02:53 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-gitops to version 0.12.4
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ## Changes in version 0.12.4
    
    ### Bug Fixes
    
    * field to distinguish repository groups (Mårten Svantesson)
    
    ### Chores
    
    * deps: bump github.com/emicklei/go-restful (dependabot[bot])

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.1"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.12.2"
+	GitOpsVersion = "0.12.4"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.77"
