d7d5ef07fc91ee1538e6bd12edb029aef296320f
root <jenkins-x@googlegroups.com>
2024-10-24 21:58:08 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-gitops to version 0.22.4
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ## Changes in version 0.22.4
    
    ### Bug Fixes
    
    * increase memory request for linting (Mårten Svantesson)
    
    ### Chores
    
    * upgrade jx-logging (Mårten Svantesson)

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.6"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.22.3"
+	GitOpsVersion = "0.22.4"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.77"
