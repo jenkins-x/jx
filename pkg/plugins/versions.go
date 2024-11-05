e64deff9794e0c57cab4944764b419ee172be935
root <jenkins-x@googlegroups.com>
2024-11-05 16:23:44 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-gitops to version 0.23.2
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ## Changes in version 0.23.2
    
    ### Chores
    
    * change deprecated goreleaser config (GavinPJK)

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.10"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.22.7"
+	GitOpsVersion = "0.23.2"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.77"
