48b448a12a7d5034c606ba3e61fe85453cb58523
root <jenkins-x@googlegroups.com>
2024-11-07 16:44:25 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-gitops to version 0.23.4
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ## Changes in version 0.23.4
    
    ### Chores
    
    * set correct sparse checkout order (GavinPJK)
    * remove leading slash (GavinPJK)
    * remove redundant check (GavinPJK)
    * Check out /jx-requirements.yml sparsely (GavinPJK)

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.10"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.23.3"
+	GitOpsVersion = "0.23.4"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.77"
