9ac16f03b3075b44558f48e62bcfa11964cbd308
root <jenkins-x@googlegroups.com>
2024-11-11 15:06:09 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-gitops to version 0.23.7
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ## Changes in version 0.23.7
    
    ### Bug Fixes
    
    * handle quoted namespace in resource (Mårten Svantesson)

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.10"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.23.6"
+	GitOpsVersion = "0.23.7"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.77"
