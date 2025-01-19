dbed7dedf6f5e9ec16f7d5e5d92c6d7c3f54eb40
root <jenkins-x@googlegroups.com>
2025-01-19 00:33:20 +0000

    chore(deps): upgrade to version 0.23.12
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ## Changes in version 0.23.12
    
    ### Bug Fixes
    
    * upgrade lighthouse-client (Mårten Svantesson)
    
    ### Chores
    
    * deps: upgrade go dependencies (jenkins-x-bot)

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.11"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.23.9"
+	GitOpsVersion = "0.23.12"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.77"
