ed6523218d9010fb4a02074d001eadbd41ea8e85
root <jenkins-x@googlegroups.com>
2025-02-04 17:40:12 +0000

    chore(deps): upgrade to version 0.24.2
    from: https://github.com/jenkins-x-plugins/jx-gitops.git

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.12"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "0.23.14"
+	GitOpsVersion = "0.24.2"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.77"
