38ab96f01e6032759a85a002ac5d7aa108367b03
root <jenkins-x@googlegroups.com>
2025-07-30 15:30:36 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-gitops to version 1.0.26
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ## Changes in version 1.0.26
    
    ### Chores
    
    * deps: bump github.com/go-viper/mapstructure/v2 (dependabot[bot])

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.14"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "1.0.25"
+	GitOpsVersion = "1.0.26"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.81"
