e07e2e8064e8b34196db768b3c14584f85065b16
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2025-07-30 14:44:48 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-preview to version 0.5.11
    from: https://github.com/jenkins-x-plugins/jx-preview

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -25,7 +25,7 @@ const (
 	PipelineVersion = "0.7.30"
 
 	// PreviewVersion the version of the jx preview plugin
-	PreviewVersion = "0.5.10"
+	PreviewVersion = "0.5.11"
 
 	// ProjectVersion the version of the jx project plugin
 	ProjectVersion = "0.2.78"
