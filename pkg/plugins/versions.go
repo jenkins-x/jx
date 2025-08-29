c10aa902b36bb0bd61d121c88a8f9ba4fbc46bb6
root <jenkins-x@googlegroups.com>
2025-08-29 09:24:04 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-gitops to version 1.0.31
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ## Changes in version 1.0.31
    
    ### Chores
    
    * deps: bump helm.sh/helm/v3 from 3.18.4 to 3.18.5 (dependabot[bot])

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.18"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "1.0.30"
+	GitOpsVersion = "1.0.31"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.81"
