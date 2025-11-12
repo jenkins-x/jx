a5b484d2eb10c3c177665c556f1a124d2ac6f607
root <jenkins-x@googlegroups.com>
2025-11-12 12:09:11 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-health to version 0.0.82
    from: https://github.com/jenkins-x-plugins/jx-health.git
    
    -----
    # jenkins-x-plugins/jx-health
    
    ## Changes in version 0.0.82
    
    ### Chores
    
    * deps: upgrade go dependencies (root)

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -19,7 +19,7 @@ const (
 	GitOpsVersion = "1.0.33"
 
 	// HealthVersion the version of the jx health plugin
-	HealthVersion = "0.0.81"
+	HealthVersion = "0.0.82"
 
 	// PipelineVersion the version of the jx pipeline plugin
 	PipelineVersion = "0.7.31"
