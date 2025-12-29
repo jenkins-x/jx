f9e79aa04aff8069ecd3616b5d84d1477742e258
root <jenkins-x@googlegroups.com>
2025-12-29 17:18:24 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-gitops to version 1.0.37
    from: https://github.com/jenkins-x-plugins/jx-gitops.git
    
    -----
    # jenkins-x-plugins/jx-gitops
    
    ## Changes in version 1.0.37
    
    ### Bug Fixes
    
    * properly sanitize PR label names for environment variables (Maximilien Raulic)
    
    -----
    -------------------------|------------------------------|---------------------------------------|
    | `env.staging`                | `PR_LABEL_ENV.STAGING`       | `PR_LABEL_ENV_STAGING`                |
    | `priority: high`             | `PR_LABEL_PRIORITY: HIGH`    | `PR_LABEL_PRIORITY_HIGH`              |
    | `some:/random-label`         | `PR_LABEL_SOME:/RANDOM_LABEL` | `PR_LABEL_SOME_RANDOM_LABEL`        |
    | `🐛 bug`                     | `PR_LABEL_🐛 BUG`            | `PR_LABEL_BUG`                        |
    
    ---
    
    ```
    Fixes issues where pipelines fail with `not a valid identifier` errors when sourcing `.jx/variables.sh` with PR labels containing special characters.
    ```

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -16,7 +16,7 @@ const (
 	ChangelogVersion = "0.10.19"
 
 	// GitOpsVersion the version of the jx gitops plugin
-	GitOpsVersion = "1.0.36"
+	GitOpsVersion = "1.0.37"
 
 	// HealthVersion the version of the jx health plugin
 	HealthVersion = "0.0.83"
