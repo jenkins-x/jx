a31bdcdca531013356e719ef2ba7551b813067a1
Mårten Svantesson <Marten.Svantesson@ticket.se>
2026-06-24 00:20:59 +0200

    Revert "fix: ensure current directory is the working directory"
    This reverts commit 57b3223564cb7a4381e5e33dd57787c83a0af189.
    
    Signed-off-by: Mårten Svantesson <Marten.Svantesson@ticket.se>

--- .github/workflows/jenkins-x/updatebot.sh
+++ .github/workflows/jenkins-x/updatebot.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env sh
 
-if [ -n "$GITHUB_WORKSPACE" ] && [ -d "$GITHUB_WORKSPACE" ]; then
-  cd "$GITHUB_WORKSPACE"
-fi
-
 echo "$CHANGELOG" > changelog.md
 jx-updatebot pr --git-credentials --add-changelog changelog.md
