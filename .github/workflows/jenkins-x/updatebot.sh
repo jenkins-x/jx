57b3223564cb7a4381e5e33dd57787c83a0af189
Mårten Svantesson <Marten.Svantesson@ticket.se>
2026-06-23 22:32:20 +0200

    fix: ensure current directory is the working directory
    part of fix for jenkins-x/jx#8965
    
    Signed-off-by: Mårten Svantesson <Marten.Svantesson@ticket.se>

--- .github/workflows/jenkins-x/updatebot.sh
+++ .github/workflows/jenkins-x/updatebot.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
+if [ -n "$GITHUB_WORKSPACE" ] && [ -d "$GITHUB_WORKSPACE" ]; then
+  cd "$GITHUB_WORKSPACE"
+fi
+
 echo "$CHANGELOG" > changelog.md
 jx-updatebot pr --git-credentials --add-changelog changelog.md
