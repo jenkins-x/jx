d3fb50f95307e121e74eb7cbfd13b9ab5bfc992c
root <jenkins-x@googlegroups.com>
2026-06-24 15:59:20 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-pipeline to version 0.7.40
    from: https://github.com/jenkins-x-plugins/jx-pipeline.git
    
    -----
    # jenkins-x-plugins/jx-pipeline
    
    ## Changes in version 0.7.40
    
    ### Bug Fixes
    
    * update security policy (Mårten Svantesson)

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -22,7 +22,7 @@ const (
 	HealthVersion = "0.0.85"
 
 	// PipelineVersion the version of the jx pipeline plugin
-	PipelineVersion = "0.7.38"
+	PipelineVersion = "0.7.40"
 
 	// PreviewVersion the version of the jx preview plugin
 	PreviewVersion = "0.7.11"
