f7643211a511270c2349fa9e5ba0fce5323d4fba
jenkins-x-bot-test <jenkins-x@googlegroups.com>
2026-08-10 09:02:05 +0000

    chore(deps): upgrade jenkins-x-plugins/jx-admin to version 0.3.34
    from: https://github.com/jenkins-x-plugins/jx-admin
    
    -----
    # jenkins-x-plugins/jx-admin
    
    ## Changes in version 0.3.34
    
    ### Chores
    
    * deps: upgrade jenkins-x-plugins/jx-release-version to version 2.10.7 (root)
    
    -----
    
    # jenkins-x-plugins/jx-release-version
    
    ## Changes in version 2.10.7
    
    ### Chores
    
    * deps: bump github.com/go-git/go-git/v5 from 5.19.1 to 5.19.2 (dependabot[bot])
    * upgrade image to 2.10.6 (jenkins-x-bot-test)

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	// AdminVersion the version of the jx admin plugin
-	AdminVersion = "0.3.33"
+	AdminVersion = "0.3.34"
 
 	// ApplicationVersion the version of the jx application plugin
 	ApplicationVersion = "0.3.14"
